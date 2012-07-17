d2cee9cfa3ddc0514544afd6eac04ee89420832e
Donna Calhoun <donna.calhoun@gmail.com>
2012-07-17 19:37:17 +0200

    Added AMR stuff

---
+++ src/amr_forestclaw.cpp
@@ -0,0 +1,17 @@
+#include <ClawPatch.H>
+#include "forestclaw.h"
+
+void amrsetup(pfclaw_domain_t *domain)
+{
+    for(int i = 0; i < domain->num_blocks; i++)
+    {
+        pfclaw_block_t *block = domain->blocks + i;
+        for(int j = 0; j < block->num_patches; j++)
+        {
+            pfclaw_patch_t *patch = block->patches + j;
+            ClawPatch *cp = new ClawPatch();
+            patch->userdata = cp;
+
+    }
+
+}
