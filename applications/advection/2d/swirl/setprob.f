8abc534e17f034b438dd674b45214010209e2e09
Donna Calhoun <donna.calhoun@gmail.com>
2012-07-21 14:49:18 +0200

    Minor changes to example Makefile and to file setprob.f

--- applications/advection/2d/swirl/setprob.f
+++ applications/advection/2d/swirl/setprob.f
@@ -6,11 +6,11 @@
       common /compi/ pi
       common /comvt/ tperiod,pi2
 
-      open(unit=7,file='setprob.data',status='old',form='formatted')
+      open(unit=7,file='setprob.data')
       read(7,*) tperiod
       close(7)
 
-      pi = 4.d0 * datan(1.d0)
+      pi = 4.d0*atan(1.d0)
       pi2 = 2.d0*pi
 
 
