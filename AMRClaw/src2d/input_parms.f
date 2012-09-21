b8415de8aa9aee10c8600ecb10fb7a3b407c736e
Donna Calhoun <donna.calhoun@gmail.com>
2012-09-21 14:15:14 +0100

    Seperate out routine that reads the input file from the rest of the code.

---
+++ AMRClaw/src2d/input_parms.f
@@ -0,0 +1,75 @@
+      subroutine inputparms(mx_leaf,my_leaf,initial_dt, tfinal,
+     &      max_cfl, desired_cfl,nout, src_term, verbose, mcapa,
+     &      maux,meqn,mwaves, maxmwaves,mthlim,mbc,mthbc,order,
+     &      minlevel,maxlevel,icycle,imanifold)
+      implicit none
+
+      integer mx_leaf, my_leaf
+      double precision initial_dt, tfinal, max_cfl, desired_cfl
+      integer nout, src_term, mcapa, maux, meqn, mwaves
+      integer maxmwaves,mbc, verbose
+      integer mthbc(4),mthlim(maxmwaves), order(2)
+      integer maxlevel, minlevel, icycle, imanifold
+      logical subcycle, manifold
+
+      integer mw, m
+
+
+      open(55,file='claw2ez.data')
+
+      read(55,*) mx_leaf
+      read(55,*) my_leaf
+
+c     timestepping variables
+      read(55,*) nout
+      read(55,*) tfinal
+
+      read(55,*) initial_dt
+      read(55,*) max_cfl
+      read(55,*) desired_cfl
+
+      read(55,*) (order(m),m=1,2)
+      read(55,*) verbose
+      read(55,*) src_term
+      read(55,*) mcapa
+      read(55,*) maux
+
+      read(55,*) meqn
+      read(55,*) mwaves
+
+      if (mwaves .gt. maxmwaves) then
+         write(6,*) 'ERROR : (claw_utils.f) mwaves > maxmwaves'
+         write(6,*) 'mwaves = ', mwaves
+         write(6,*) 'maxmwaves = ', maxmwaves
+         stop
+      endif
+
+      read(55,*) (mthlim(mw), mw=1,mwaves)
+
+      read(55,*) mbc
+      read(55,*) mthbc(1)
+      read(55,*) mthbc(2)
+      read(55,*) mthbc(3)
+      read(55,*) mthbc(4)
+
+      read(55,*) manifold
+      if (manifold) then
+         imanifold = 1
+      else
+         imanifold = 0
+      endif
+
+      read(55,*) minlevel
+      read(55,*) maxlevel
+
+      read(55,*) subcycle
+      if (subcycle) then
+         icycle = 1
+      else
+         icycle = 0
+      endif
+
+
+
+      close(55)
+      end
