71f7bf1c12b0632ce04efa1b76f6af60a4d0eb87
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2012-12-16 12:02:30 +0100

    Start torus C example, doesn't link yet

---
+++ applications/advection/2d/torus/torus.c
@@ -0,0 +1,86 @@
+/*
+Copyright (c) 2012 Carsten Burstedde, Donna Calhoun
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* This needs to go away.  The p4est namespace should not be used directly. */
+#include <p4est.h>
+
+#include <fclaw2d_capi.h>
+#include <fclaw2d_convenience.h>
+#include <fclaw2d_single_step.h>
+#include <fclaw2d_waveprop.h>
+
+int
+main (int argc, char **argv)
+{
+  int			mpiret;
+  int			lp;
+  MPI_Comm		mpicomm;
+  sc_options_t          *options;
+  fclaw2d_domain_t	*domain;
+  amr_options_t         *gparms;
+
+  mpiret = MPI_Init (&argc, &argv);
+  SC_CHECK_MPI (mpiret);
+  mpicomm = MPI_COMM_WORLD;
+  lp = SC_LP_PRODUCTION;
+
+  sc_init (mpicomm, 0, 0, NULL, lp);
+  p4est_init (NULL, lp);
+
+  /* the option values live in amr_options_t * gparms; see amr_options.h */
+  options = sc_options_new (argv[0]);
+  /* set default options and read default option file */
+  gparms = amr_options_new (options);
+  /* parse command line; these values take precedence */
+  amr_options_parse (options, gparms, argc, argv, lp);
+
+  /* ---------------------------------------------------------------
+     Domain geometry
+     --------------------------------------------------------------- */
+  domain = fclaw2d_domain_new_unitsquare (mpicomm, gparms->minlevel);
+
+  fclaw2d_domain_list_levels(domain, lp);
+  fclaw2d_domain_list_neighbors(domain, lp);
+
+  /* ---------------------------------------------------------------
+     Set domain data.
+     --------------------------------------------------------------- */
+  fclaw2d_allocate_domain_data (domain, gparms,
+                                &fclaw2d_single_step,
+                                &fclaw2d_waveprop_update);
+
+  amrinit(&domain);
+  amrrun(&domain);
+  amrreset(&domain);
+
+  sc_options_destroy (options);         /* this could be moved up */
+  amr_options_destroy (gparms);
+  sc_finalize ();
+
+  mpiret = MPI_Finalize ();
+  SC_CHECK_MPI (mpiret);
+
+  return 0;
+}
