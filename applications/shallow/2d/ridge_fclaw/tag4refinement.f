dd367cf93b64f34f75eec878362c38b3d87882fa
Donna Calhoun <donna.calhoun@gmail.com>
2013-06-24 16:49:20 +0200

    fix argument list bug

--- applications/shallow/2d/ridge_fclaw/tag4refinement.f
+++ applications/shallow/2d/ridge_fclaw/tag4refinement.f
@@ -16,11 +16,16 @@
       tag_patch = 0
       do i = 1,mx
          do j = 1,my
-            eta = q(i,j,1) + aux(i,j,19)
-            if (abs(eta) .gt. tol1) then
+            if (initflag .eq. 1) then
+               tag_patch = 0
+            else
                tag_patch = 1
-               return
             endif
+c            eta = q(i,j,1) + aux(i,j,19)
+c            if (abs(eta) .gt. tol1) then
+c               tag_patch = 1
+c               return
+c            endif
          enddo
       enddo
 
@@ -28,19 +33,17 @@
 
 c     # We tag for coarsening if this coarsened patch isn't tagged for refinement
       subroutine ridge_tag4coarsening(mx,my,mbc,meqn,
-     &      xlower,ylower,dx,dy,qcoarsened, maux,aux,tag_patch)
+     &      xlower,ylower,dx,dy,qcoarsened, maux,aux,initflag,tag_patch)
       implicit none
 
-      integer mx,my, mbc, meqn, tag_patch, maux
+      integer mx,my, mbc, meqn, tag_patch, maux, initflag
       double precision xlower, ylower, dx, dy
       double precision qcoarsened(1-mbc:mx+mbc,1-mbc:my+mbc,meqn)
       double precision aux(1-mbc:mx+mbc,1-mbc:my+mbc,maux)
 
-      integer i,j, mq
-      double precision qmin, qmax
+      integer i,j
 
       double precision ampl, tol1, tol2, eta
-
       common /cprob/ ampl, tol1, tol2
 
 
@@ -49,11 +52,6 @@ c     # that we can't check ghost cells here.  Also, we may make the
 c     # coarsening criteria different from the refinement criteria.
 c     # Also, we don't check for an init_flag, since it is unlikely that
 c     # we would coarsen an initial grid.
-
-      double precision xc,yc,s,eta
-      double precision ampl, tol1, tol2, bmount
-
-      common /cprob/ ampl, tol1, tol2
 
       tag_patch = 0
       do i = 1,mx
