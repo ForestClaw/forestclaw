ff1f3a7b7642ef275169911a4003f474019328ca
Donna Calhoun <donna.calhoun@gmail.com>
2013-06-26 17:56:52 +0200

    Provided headers for tagging/coarsening routines

--- applications/advection/2d/hemisphere/hemisphere_user.cpp
+++ applications/advection/2d/hemisphere/hemisphere_user.cpp
@@ -238,7 +238,7 @@ fclaw_bool hemisphere_patch_tag4refinement(fclaw2d_domain_t *domain,
     double* q = cp->q();
 
     int tag_patch;  // == 0 or 1
-    tag4refinement_(mx,my,mbc,meqn,xlower,ylower,dx,dy,q,initflag,tag_patch);
+    hemisphere_tag4refinement_(mx,my,mbc,meqn,xlower,ylower,dx,dy,q,initflag,tag_patch);
     return tag_patch == 1;
 }
 
@@ -268,7 +268,7 @@ fclaw_bool hemisphere_patch_tag4coarsening(fclaw2d_domain_t *domain,
     double* qcoarse = cp->q();
 
     int tag_patch;  // == 0 or 1
-    tag4coarsening_(mx,my,mbc,meqn,xlower,ylower,dx,dy,qcoarse,tag_patch);
+    hemisphere_tag4coarsening_(mx,my,mbc,meqn,xlower,ylower,dx,dy,qcoarse,tag_patch);
     return tag_patch == 0;
 }
 
