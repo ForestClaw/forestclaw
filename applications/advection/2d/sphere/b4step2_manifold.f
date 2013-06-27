cb270920ec654ee133fc19089add11f979668ec3
Donna Calhoun <donna.calhoun@gmail.com>
2013-06-27 17:48:19 +0200

    fixing merge conflicts

--- applications/advection/2d/sphere/b4step2_manifold.f
+++ applications/advection/2d/sphere/b4step2_manifold.f
@@ -8,10 +8,6 @@
       double precision q(1-mbc:maxmx+mbc,1-mbc:maxmy+mbc, meqn)
       double precision aux(1-mbc:maxmx+mbc,1-mbc:maxmy+mbc, maux)
 
-      double precision xp(-mbc:mx+mbc+1,-mbc:my+mbc+1)
-      double precision yp(-mbc:mx+mbc+1,-mbc:my+mbc+1)
-      double precision zp(-mbc:mx+mbc+1,-mbc:my+mbc+1)
-
       double precision xd(-mbc:mx+mbc+2,-mbc:my+mbc+2)
       double precision yd(-mbc:mx+mbc+2,-mbc:my+mbc+2)
       double precision zd(-mbc:mx+mbc+2,-mbc:my+mbc+2)
