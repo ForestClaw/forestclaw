4177b0c2f67dca4b214235bb8ef5e97d82f36221
Donna Calhoun <donna.calhoun@gmail.com>
2013-07-03 08:39:04 +0200

    Allowed for more meqn > 1

--- applications/advection/2d/no_solver/initialize.f
+++ applications/advection/2d/no_solver/initialize.f
@@ -10,24 +10,18 @@
 
       common /com_init/ ichoice
 
-      if (meqn .gt. 2) then
-         write(6,*) 'initialize (initialize.f) : meqn > 2'
-         stop
-      endif
+      ichoice = 2
 
-      ichoice = 1
+      do mq = 1,meqn
+         do i = 1-mbc,mx+mbc
+            xlow = xlower + (i-1)*dx
+            do j = 1-mbc,my+mbc
+               ylow = ylower + (j-1)*dy
 
-      do i = 1-mbc,mx+mbc
-         xlow = xlower + (i-1)*dx
-         do j = 1-mbc,my+mbc
-            ylow = ylower + (j-1)*dy
+               call cellave2(xlow,ylow,dx,dy,wl)
 
-            call cellave2(xlow,ylow,dx,dy,wl)
-
-            q(i,j,1) = wl
-            if (meqn > 1) then
-               q(i,j,2) = 1-q(i,j,1)
-            endif
+               q(i,j,mq) = wl
+            enddo
          enddo
       enddo
 
@@ -58,6 +52,7 @@ c     # Large inner circle
       endif
 
       if (ichoice .eq. 1) then
+         fdisc = -fdisc
          return
       endif
 
