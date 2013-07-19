766d8351162a9edd545a3d8587fba507c4ca0a30
Donna Calhoun <donna.calhoun@gmail.com>
2013-07-19 14:39:32 -0700

    Allow for meqn > 1

--- applications/advection/2d/swirl/qinit.f
+++ applications/advection/2d/swirl/qinit.f
@@ -15,23 +15,22 @@ c     #     0.1  otherwise
        double precision q(1-mbc:maxmx+mbc, 1-mbc:maxmy+mbc, meqn)
        double precision aux(1-mbc:maxmx+mbc, 1-mbc:maxmy+mbc, maux)
 
-       integer i, j
+       integer i, j, mq
        double precision xi, yj, s
 
-       do i = 1-mbc,mx+mbc
-          xi = xlower + (i-0.5d0)*dx
-          do j = 1-mbc,my+mbc
-             yj = ylower + (j-0.5d0)*dy
+       do mq = 1,meqn
+          do i = 1-mbc,mx+mbc
+             xi = xlower + (i-0.5d0)*dx
+             do j = 1-mbc,my+mbc
+                yj = ylower + (j-0.5d0)*dy
 
-             s = 0.5d0
-             if (xi .lt. s) then
-                q(i,j,1) = 0.d0
-             else
-                q(i,j,1) = 1.d0
-             endif
-             if (meqn > 1) then
-                q(i,j,2) = 1-q(i,j,1)
-             endif
+                s = 0.5d0
+                if (xi .lt. s) then
+                   q(i,j,mq) = 0.d0
+                else
+                   q(i,j,mq) = 1.d0
+                endif
+             enddo
           enddo
        enddo
 
