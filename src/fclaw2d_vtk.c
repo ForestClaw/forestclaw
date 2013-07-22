762430377b3c64ba4a241f52609d5ac615734a2c
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2013-07-22 12:18:41 +0200

    Don't use VTK vector annotation

--- src/fclaw2d_vtk.c
+++ src/fclaw2d_vtk.c
@@ -105,6 +105,7 @@ fclaw2d_vtk_write_header (fclaw2d_domain_t * domain, fclaw2d_vtk_state_t * s)
                                 (long long) s->offset_types) < 0;
     retval = retval || fprintf (file, "    </DataArray>\n") < 0;
     retval = retval || fprintf (file, "   </Cells>\n") < 0;
+#if 0
     if (s->meqn == 1)
     {
         retval = retval || fprintf (file, "   <CellData Scalars=\"mpirank,"
@@ -116,6 +117,10 @@ fclaw2d_vtk_write_header (fclaw2d_domain_t * domain, fclaw2d_vtk_state_t * s)
                                     "blockno,patchno\" Vectors=\"meqn\">\n") <
             0;
     }
+#else
+    retval = retval || fprintf (file, "   <CellData Scalars=\"mpirank,"
+                                "blockno,patchno\">\n") < 0;
+#endif
     retval = retval || fprintf (file, "    <DataArray type=\"Int32\" "
                                 "Name=\"mpirank\" format=\"appended\" "
                                 "offset=\"%lld\">\n",
