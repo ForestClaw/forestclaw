b8148161abf534e0f4c81f0bf6f93fffdaa27eae
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2013-07-31 21:56:54 +0200

    Add two plot scripts

---
+++ docs/stampede/manyclaw/scripta.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+MINL=1
+MAXL=5
+
+NODES="1 2 4"
+
+
+for NODE in $NODES ; do
+	# This defines the processor count
+	echo "# nodes $NODE"
+        perl -e "print 16 * $NODE"
+
+for LEVEL in `seq $MINL $MAXL` ; do
+	# This defines the problem size
+	#echo "# level $LEVEL"
+
+        grep "Max/P" "mx32_omp_16_level_${LEVEL}_mpi_${NODE}.out" | \
+		cut -d ' ' -f 5,6 | awk "{ printf (\" %g\", \$2 / \$1 / (4**$LEVEL)); }"
+done
+	echo
+done
