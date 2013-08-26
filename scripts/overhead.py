35761f00ae4301233a5a4ebddda945f82dd99f74
Donna Calhoun <donna.calhoun@gmail.com>
2013-08-26 12:10:17 -0600

    Added python script to compute regridding/exchange overhead associated with simulation run

---
+++ scripts/overhead.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys
+
+if sys.argv.__len__() < 3:
+    print "Usage :  Enter time needed for each portion of the code"
+    print "    % overhead <advance> <exchange> <regrid>"
+    sys.exit();
+
+a = float(sys.argv[1])
+e = float(sys.argv[2])
+r = float(sys.argv[3])
+
+o = r + e
+
+print " "
+print "%40s %6.1f" % ("Over head as percentage of ADVANCE",100.0*o/a)
+print "%40s %6.1f" % ("Over head as percentage of total",100.0*o/(a + o))
+print " "
+
+print "Grid advances take %2.1f times as long as exchange and regridding" % (a/o)
+print " "
