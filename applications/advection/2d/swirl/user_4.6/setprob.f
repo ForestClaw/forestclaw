4ae33d0b1bab61800411e17cbd7c632c588ceaee
Donna <donna.calhoun@gmail.com>
2014-08-07 21:35:17 +0100

    Removed call to set_maptype

--- applications/advection/2d/swirl/user_4.6/setprob.f
+++ applications/advection/2d/swirl/user_4.6/setprob.f
@@ -6,12 +6,7 @@
       common /compi/ pi
       common /comvt/ tperiod,pi2
 
-c      open(unit=7,file='setprob.data')
-c      read(7,*) tperiod
-c      close(7)
       tperiod = 4.d0
-
-      call set_maptype_cart()
 
       pi = 4.d0*atan(1.d0)
       pi2 = 2.d0*pi
