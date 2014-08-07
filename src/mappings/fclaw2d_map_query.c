3bb9389ff1d85f0624510efdb83e842aab662b18
Donna <donna.calhoun@gmail.com>
2014-08-07 22:32:56 +0100

    Added file for query routines

---
+++ src/mappings/fclaw2d_map_query.c
@@ -0,0 +1,96 @@
+/*
+Copyright (c) 2012 Carsten Burstedde, Donna Calhoun
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <fclaw2d_map_query.h>
+
+int FCLAW2D_MAP_IS_PILLOWDISK(fclaw2d_map_context_t** pcont)
+{
+    int iresult;
+    fclaw2d_map_context_t *cont = *pcont;
+    int id = FCLAW2D_MAP_QUERY_IS_PILLOWDISK;
+    // fclaw2d_map_query_(&cont,&id,&iresult);
+    iresult = cont->query(cont,id);
+    return iresult != 0;
+}
+
+int  FCLAW2D_MAP_IS_PILLOWSPHERE(fclaw2d_map_context_t** pcont)
+{
+    int iresult;
+    fclaw2d_map_context_t *cont = *pcont;
+    int id = FCLAW2D_MAP_QUERY_IS_PILLOWSPHERE;
+    iresult = cont->query(cont,id);
+    return iresult != 0;
+}
+
+int FCLAW2D_MAP_IS_CUBEDSPHERE(fclaw2d_map_context_t** pcont)
+{
+    int iresult;
+    fclaw2d_map_context_t *cont = *pcont;
+    int id = FCLAW2D_MAP_QUERY_IS_CUBEDSPHERE;
+    iresult = cont->query(cont,id);
+    return iresult != 0;
+}
+
+int FCLAW2D_MAP_IS_FLAT(fclaw2d_map_context_t** pcont)
+{
+    int iresult;
+    fclaw2d_map_context_t *cont = *pcont;
+    int id = FCLAW2D_MAP_QUERY_IS_FLAT;
+    iresult = cont->query(cont,id);
+    return iresult != 0;
+}
+
+int FCLAW2D_MAP_IS_SPHERE(fclaw2d_map_context_t** pcont)
+{
+    int iresult1, iresult2;
+    fclaw2d_map_context_t *cont = *pcont;
+    int id1 = FCLAW2D_MAP_QUERY_IS_PILLOWSPHERE;
+    iresult1 = cont->query(cont,id1);
+    int id2 = FCLAW2D_MAP_QUERY_IS_CUBEDSPHERE;
+    iresult2 = cont->query(cont,id2);
+
+    return iresult1 != 0 || iresult2 != 0;
+}
+
+int FCLAW2D_MAP_IS_CART(fclaw2d_map_context_t** pcont)
+{
+    int iresult;
+    fclaw2d_map_context_t *cont = *pcont;
+    int id = FCLAW2D_MAP_QUERY_IS_CART;
+    iresult = cont->query(cont,id);
+    return iresult != 0;
+}
+
+int FCLAW2D_MAP_IS_DISK(fclaw2d_map_context_t** pcont)
+{
+    int iresult1, iresult2;
+    fclaw2d_map_context_t *cont = *pcont;
+    int id1 = FCLAW2D_MAP_QUERY_IS_PILLOWDISK;
+    iresult1 = cont->query(cont,id1);
+    int id2 = FCLAW2D_MAP_QUERY_IS_SQUAREDDISK;
+    iresult2 = cont->query(cont,id2);
+
+    return iresult1 != 0 || iresult2 != 0;
+}
