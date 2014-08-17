7359f2df4d6806a82b1ea4438cbcbbb179796cc1
Donna <donna.calhoun@gmail.com>
2014-08-17 09:09:43 +0100

    Added squareddisk queries

--- src/fclaw2d_map_query.c
+++ src/fclaw2d_map_query.c
@@ -53,6 +53,15 @@ int FCLAW2D_MAP_IS_PILLOWDISK(fclaw2d_map_context_t** pcont)
     return iresult != 0;
 }
 
+int FCLAW2D_MAP_IS_SQUAREDDISK(fclaw2d_map_context_t** pcont)
+{
+    int iresult;
+    fclaw2d_map_context_t *cont = *pcont;
+    int id = FCLAW2D_MAP_QUERY_IS_SQUAREDDISK;
+    iresult = cont->query(cont,id);
+    return iresult != 0;
+}
+
 int  FCLAW2D_MAP_IS_PILLOWSPHERE(fclaw2d_map_context_t** pcont)
 {
     int iresult;
