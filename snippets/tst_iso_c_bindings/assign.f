f274c1099d8c82f6f38e0c1ee3092ea7686620f8
Donna <donna.calhoun@gmail.com>
2014-09-17 14:34:10 -0600

    Try out c_double

--- snippets/tst_iso_c_bindings/assign.f
+++ snippets/tst_iso_c_bindings/assign.f
@@ -2,24 +2,24 @@
       implicit none
       interface
          subroutine mult(s,n,x) bind(c,name="mult")
-         use iso_c_binding, only : c_ptr
+         use iso_c_binding, only : c_ptr, c_double
          implicit none
 
          type(c_ptr) s
          integer n
-         double precision x(n)
+         real(c_double) x(n)   !! double precision works too
          end subroutine
       end interface
       end module
 
 
       subroutine assign(s,n,x) bind(c,name="assign")
-      use iso_c_binding, only : c_ptr
-      use c_routines
+      use iso_c_binding, only : c_ptr, c_double
+      use c_routines, only : mult
       implicit none
 
       type(c_ptr) s
-      double precision x(n)
+      real(c_double) x(n)
       integer n, i
 
       do i = 1,n
