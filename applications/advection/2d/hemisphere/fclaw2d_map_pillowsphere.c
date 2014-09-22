83b2050ea3ed9ec5fc920a2a92672a89d32c9d9f
Donna <donna.calhoun@gmail.com>
2014-09-21 23:28:25 -0600

    More new files for hemisphere example

---
+++ applications/advection/2d/hemisphere/fclaw2d_map_pillowsphere.c
@@ -0,0 +1,82 @@
+/* Pillow grid surface.  Matches p4est_connectivity_new_pillow (). */
+
+#include <fclaw2d_map.h>
+
+static int
+fclaw2d_map_query_pillowsphere (fclaw2d_map_context_t * cont, int query_identifier)
+{
+    switch (query_identifier)
+    {
+    case FCLAW2D_MAP_QUERY_IS_USED:
+        return 1;
+        /* no break necessary after return statement */
+    case FCLAW2D_MAP_QUERY_IS_SCALEDSHIFT:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_AFFINE:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_NONLINEAR:
+        return 1;
+    case FCLAW2D_MAP_QUERY_IS_CART:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_GRAPH:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_PLANAR:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_ALIGNED:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_FLAT:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_DISK:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_SPHERE:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_PILLOWDISK:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_SQUAREDDISK:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_PILLOWSPHERE:
+        return 1;
+    case FCLAW2D_MAP_QUERY_IS_CUBEDSPHERE:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_FIVEPATCH:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_HEMISPHERE:
+        return 1;
+    default:
+        printf("\n");
+        printf("fclaw2d_map_query_pillowsphere (fclaw2d_map.c) : Query id not "\
+               "identified;  Maybe the query is not up to date?\nSee "  \
+               "fclaw2d_map_query_defs.h.\n");
+        printf("Requested query id : %d\n",query_identifier);
+        SC_ABORT_NOT_REACHED ();
+    }
+    return 0;
+}
+
+
+static void
+fclaw2d_map_c2m_pillowsphere (fclaw2d_map_context_t * cont, int blockno,
+                              double xc, double yc,
+                              double *xp, double *yp, double *zp)
+{
+    MAPC2M_PILLOWSPHERE(&blockno,&xc,&yc,xp,yp,zp);
+
+    /* These can probably be replaced by C functions at some point. */
+    SCALE_MAP(xp,yp,zp);
+    ROTATE_MAP(xp,yp,zp);
+}
+
+fclaw2d_map_context_t *
+    fclaw2d_map_new_pillowsphere(const double scale,
+                                 double rotate[])
+{
+    fclaw2d_map_context_t *cont;
+
+    cont = FCLAW_ALLOC_ZERO (fclaw2d_map_context_t, 1);
+    cont->query = fclaw2d_map_query_pillowsphere;
+    cont->mapc2m = fclaw2d_map_c2m_pillowsphere;
+    SET_ROTATION(rotate);
+    SET_SCALE(&scale);
+
+    return cont;
+}
---
+++ applications/advection/2d/hemisphere/user_4.6/qinit_manifold.f
@@ -0,0 +1,45 @@
+      subroutine qinit_manifold(maxmx,maxmy,meqn,mbc,mx,my,
+     &      xlower,ylower,dx,dy,q,maux,aux)
+
+      implicit none
+
+      integer maxmx, maxmy, meqn, mbc, mx, my, maux,this_block_idx
+      integer blockno
+      double precision xlower, ylower, dx, dy
+      double precision q(1-mbc:maxmx+mbc, 1-mbc:maxmy+mbc, meqn)
+      double precision aux(1-mbc:maxmx+mbc, 1-mbc:maxmy+mbc, maux)
+
+      integer i,j
+      double precision x,y,z, xlow, ylow, w
+
+      do j = 1-mbc,my+mbc
+         do i = 1-mbc,mx+mbc
+            xlow = xlower + (i-1)*dx
+            ylow = ylower + (j-1)*dy
+            call cellave2(xlow,ylow,dx,dy,w)
+            q(i,j,1) = w
+         enddo
+      enddo
+
+      return
+      end
+
+
+      double precision function  fdisc(xc,yc)
+      implicit none
+
+      double precision xc,yc, xp, yp, zp
+      integer*8 cont, get_context
+      integer blockno, get_block
+
+      cont = get_context()
+      blockno = get_block()
+
+c      call mapc2m(xc,yc,xp,yp,zp)
+
+      call fclaw2d_map_c2m(cont,
+     &      blockno,xc,yc,xp,yp,zp)
+
+
+      fdisc = -xp
+      end
