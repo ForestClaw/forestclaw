e3fcf9562125abdb6c9a220f6bc3eab07ad4811f
Donna <donna.calhoun@gmail.com>
2014-09-22 11:14:23 -0600

    Use an include file rather han a module

--- snippets/tst_iso_c_bindings/assign.f
+++ snippets/tst_iso_c_bindings/assign.f
@@ -1,29 +1,16 @@
-      module c_routines
-      implicit none
-      interface
-         subroutine mult(s,n,x) bind(c,name="mult")
-         use iso_c_binding, only : c_ptr, c_double
-         implicit none
-
-         type(c_ptr) s
-         integer n
-         real(c_double) x(n)   !! double precision works too
-         end subroutine
-      end interface
-      end module
-
-
       subroutine assign(s,n,x) bind(c,name="assign")
-      use iso_c_binding, only : c_ptr, c_double
-      use c_routines, only : mult
+      use iso_c_binding, only : c_ptr
       implicit none
+
+c     # Headers for c routine 'mult'
+      include 'c_routines.i'
 
       type(c_ptr) s
-      real(c_double) x(n)
+      double precision x(n)
       integer n, i
 
       do i = 1,n
-           x(i) = i
+         x(i) = i
       enddo
 
       call mult(s,n,x)
---
+++ snippets/tst_iso_c_bindings/c_routines.i
@@ -0,0 +1,13 @@
+C     -*- FORTRAN -*-
+
+c     # Headers files for C routines
+      interface
+         subroutine mult(s,n,x) bind(c,name="mult")
+         use iso_c_binding, only : c_ptr
+         implicit none
+
+         type(c_ptr) s
+         integer n
+         double precision x(n)   !! double precision works too
+         end subroutine
+      end interface
