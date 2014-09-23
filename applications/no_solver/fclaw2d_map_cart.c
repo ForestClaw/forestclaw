823a033771a757e8e77f70db2cb2722160593946
Donna <donna.calhoun@gmail.com>
2014-09-23 06:59:06 -0600

    Added a few mappings to test

---
+++ applications/no_solver/fclaw2d_map_cart.c
@@ -0,0 +1,83 @@
+/* Cartesian grid, tranformed to Ax + b */
+
+#include <fclaw2d_map.h>
+
+static int
+fclaw2d_map_query_cart (fclaw2d_map_context_t * cont, int query_identifier)
+{
+    switch (query_identifier)
+    {
+    case FCLAW2D_MAP_QUERY_IS_USED:
+        return 1;
+    case FCLAW2D_MAP_QUERY_IS_SCALEDSHIFT:
+        return 1;
+    case FCLAW2D_MAP_QUERY_IS_AFFINE:
+        return 1;
+    case FCLAW2D_MAP_QUERY_IS_NONLINEAR:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_GRAPH:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_PLANAR:
+        return 1;
+    case FCLAW2D_MAP_QUERY_IS_ALIGNED:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_FLAT:
+        return 1;
+    case FCLAW2D_MAP_QUERY_IS_DISK:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_SPHERE:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_PILLOWDISK:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_SQUAREDDISK:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_PILLOWSPHERE:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_CUBEDSPHERE:
+        return 0;
+    case FCLAW2D_MAP_QUERY_IS_FIVEPATCH:
+        return 0;
+    default:
+        printf("\n");
+        printf("fclaw2d_map_query_cart (fclaw2d_map_cart.h) : "\
+               "Query id not identified;  Maybe the query is not up to "\
+               "date?\nSee fclaw2d_map_cart.h.\n");
+        printf("Requested query id : %d\n",query_identifier);
+        SC_ABORT_NOT_REACHED ();
+    }
+    return 0;
+}
+
+
+static void
+fclaw2d_map_c2m_cart(fclaw2d_map_context_t * cont, int blockno,
+                     double xc, double yc,
+                     double *xp, double *yp, double *zp)
+{
+    /* Unit square in [-1,1]x[-1,1] */
+    MAPC2M_CART(&blockno,&xc,&yc,xp,yp,zp);
+    SCALE_MAP(xp,yp,zp);
+    SHIFT_MAP(xp,yp,zp);
+}
+
+
+fclaw2d_map_context_t* fclaw2d_map_new_cart()
+{
+    fclaw2d_map_context_t *cont;
+    double scale;
+    double shift[3];
+
+    cont = FCLAW_ALLOC_ZERO (fclaw2d_map_context_t, 1);
+    cont->query = fclaw2d_map_query_cart;
+    cont->mapc2m = fclaw2d_map_c2m_cart;
+
+    scale = 0.5;
+    shift[0] = 0.5;
+    shift[1] = 0.5;
+    shift[2] = 0;
+    SET_SCALE(&scale);
+    SET_SHIFT(shift);
+
+
+    return cont;
+}
