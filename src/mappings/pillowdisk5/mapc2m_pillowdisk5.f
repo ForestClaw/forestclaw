8e8dbf2dda9be2c61a69d9413ff7fc71e64126c9
Donna <donna.calhoun@gmail.com>
2014-09-27 07:24:44 -0600

    Add mapping that tusesthe disk connectivity  (with 5 blocks) to map to a square in [-1.1]x[-1,1] and then applies the pillow disk

---
+++ src/mappings/pillowdisk5/mapc2m_pillowdisk5.f
@@ -0,0 +1,17 @@
+      subroutine mapc2m_pillowdisk5(blockno,xc1,yc1,
+     &      xp,yp,zp,alpha)
+      implicit none
+
+      integer blockno
+      double precision xc1, yc1
+      double precision xc,yc,zc,xp,yp,zp,alpha
+
+      call mapc2m_fivepatch(blockno,xc1,yc1,xc,yc,zc,alpha)
+
+c     # Map from [-1,1]x[-1,1] back to [0,1]x[0,1]
+      xc = (xc + 1)/2.d0
+      yc = (yc + 1)/2.d0
+
+      call mapc2m_pillowdisk(blockno,xc,yc,xp,yp,zp)
+
+      end
