d8b3a1ef2d61b8064e603d4609535ff49bc12248
Donna <donna.calhoun@gmail.com>
2014-10-26 19:49:18 -0600

    Harded coded 4x4 brick case

--- matlab/mapc2m_brick.m
+++ matlab/mapc2m_brick.m
@@ -2,13 +2,13 @@ function [xp,yp,zp] = mapc2m_brick(xc,yc)
 
 % (mi,mj) brick
 
+ix = [0, 1, 0, 1, 2, 3, 2, 3, 0, 1, 0, 1, 2, 3, 2, 3];
+jy = [0, 0, 1, 1, 0, 0, 1, 1, 2, 2, 3, 3, 2, 2, 3, 3];
+
 blockno = getblocknumber();
 
-mi = 4;
-mj = 4;
-
-j = floor(blockno/mi);
-i = blockno - j*mi;
+j = jy(blockno+1);
+i = ix(blockno+1);
 xp = i + xc;
 yp = j + yc;
 zp = 0*xp;
