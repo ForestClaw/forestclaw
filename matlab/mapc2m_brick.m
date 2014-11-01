32d4ecd94c3022216ffafa447bd00947fc03d18e
Donna <donna.calhoun@gmail.com>
2014-11-01 11:12:41 -0600

    Modified Matlab mapping routines for bricks

--- matlab/mapc2m_brick.m
+++ matlab/mapc2m_brick.m
@@ -2,15 +2,19 @@ function [xp,yp,zp] = mapc2m_brick(xc,yc)
 
 % (mi,mj) brick
 
-ix = [0, 1, 0, 1, 2, 3, 2, 3, 0, 1, 0, 1, 2, 3, 2, 3];
-jy = [0, 0, 1, 1, 0, 0, 1, 1, 2, 2, 3, 3, 2, 2, 3, 3];
+brick_data = load('brick.dat');
+mi = brick_data(1,1);
+mj = brick_data(1,2);
+xv = brick_data(2:end,1);
+yv = brick_data(2:end,2);
+
+% ix = [0, 1, 0, 1, 2, 3, 2, 3, 0, 1, 0, 1, 2, 3, 2, 3];
+% jy = [0, 0, 1, 1, 0, 0, 1, 1, 2, 2, 3, 3, 2, 2, 3, 3];
 
 blockno = getblocknumber();
 
-j = jy(blockno+1);
-i = ix(blockno+1);
-xp = i + xc;
-yp = j + yc;
+xp = (xv(blockno+1) + xc)/mi;
+yp = (yv(blockno+1) + yc)/mj;
 zp = 0*xp;
 
 end
