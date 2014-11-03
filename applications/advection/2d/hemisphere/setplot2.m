e211d7fc3c7f5334a95e61a0f8a7fa8fa4ac4560
Donna <donna.calhoun@gmail.com>
2014-11-03 09:20:58 -0700

    Modifed graphics to illustrate blocks for slides

--- applications/advection/2d/hemisphere/setplot2.m
+++ applications/advection/2d/hemisphere/setplot2.m
@@ -1,43 +1,3 @@
-%  SETPLOT2 sets user defined plotting parameters
-%
-%      User defined Matlab script for setting various Clawpack plotting
-%      parameters.  This script is called by PLOTCLAW2.  A default
-%      version of this script can be found in claw/matlab/setplot2.m and
-%      copied to users working directory and modifed to set things up
-%      differently.
-%
-%      Parameters that can be set with SETPLOT2
-%
-%        OutputFlag        - set to 'ascii' (default) to read ascii output
-%                            or to 'hdf' to read hdf output files.
-%        PlotType          - type of plot to produce:
-% 			    - 1 = pcolor on slices (with optional contours)
-% 			    - 2 = contour lines in 2d on white slices
-% 			    - 3 = Schlieren plot on slices
-% 			    - 4 = scatter plot of q vs. r
-%
-%        mq                  - which component of q to plot
-%        UserVariable        - Set to 1 to specify a user defined variable.
-%        UserVariableFile    - name of m-file mapping data to q
-%        MappedGrid          - set to 1 if mapc2p.m exists for nonuniform
-%                              grid
-%        Manifold            - set to 1 if mapc2m.m exists for manifold plot.
-%        MaxFrames           - max number of frames
-%        MaxLevels           - max number of AMR levels
-%        PlotData            - Data on refinement level k is plotted only if
-%                              PlotData(k) == 1
-%        PlotGrid            - PLot grid lines on level k is PlotGrid(k) /= 0
-%        PlotGridEdges       - Plot 2d patch borders if PlotGridEdges(k) /= 0
-%        ContourValues       - Set to desired contour values, or [] for no ...
-% 	                       lines.
-%        x0,y0               - center for scatter plots.
-%        ScatterStyle        - symbols for scatter plot.
-%        LineStyle           - same as ScatterStyle.
-%        UserMap1d           - set to 1 if 'map1d' file exists.
-%
-%      All parameters can be modified by typing 'k' at the PLOTCLAW2 prompt.
-%
-%      See also PLOTCLAW2, SetPlotGrid, setPlotGridEdges.
 
 OutputFlag = 'forestclaw'         % default value.
 
@@ -71,4 +31,4 @@ ContourValues = [];
 
 %---------------------------------
 
-ShowUnderOverShoots = 1;
+ShowUnderOverShoots = 0;
