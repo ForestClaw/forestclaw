d8aa63678b7c5dba865adc3d3616a8bceb794a22
Donna <donna.calhoun@gmail.com>
2014-12-06 11:10:40 -0700

    Add new approach to handling options

--- applications/advection/2d/sphere/sphere.cpp
+++ applications/advection/2d/sphere/sphere.cpp
@@ -1,26 +1,26 @@
 /*
-Copyright (c) 2012 Carsten Burstedde, Donna Calhoun
-All rights reserved.
+  Copyright (c) 2012 Carsten Burstedde, Donna Calhoun
+  All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
 
- * Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
+  * Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <amr_single_step.h>
@@ -31,135 +31,150 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <amr_forestclaw.H>
 #include <amr_utils.H>
 #include <fclaw2d_map_query.h>
+#include <fclaw2d_options.h>
 
 #include "sphere_user.H"
 
-int
-main (int argc, char **argv)
+static int
+    sphere_checkparms (int example, int lp)
 {
-  int			lp;
-  int                   example;
-  sc_MPI_Comm           mpicomm;
-  sc_options_t          *options;
-  p4est_connectivity_t  *conn = NULL;
-  fclaw2d_map_context_t *cont = NULL;
-  fclaw2d_domain_t	*domain = NULL;
-  amr_options_t         samr_options, *gparms = &samr_options;
-  fclaw2d_clawpack_parms_t* clawpack_parms;
+    if (example < 1 || example > 2) {
+        fclaw2d_global_log (lp, "Option --example must be 1 or 2\n");
+        return -1;
+    }
+    return 0;
+}
 
-  double theta, phi;
 
-  lp = SC_LP_PRODUCTION;
-  mpicomm = sc_MPI_COMM_WORLD;
-  fclaw_mpi_init (&argc, &argv, mpicomm, lp);
+int main (int argc, char **argv)
+{
+    int			    lp;
+    sc_MPI_Comm             mpicomm;
+    sc_options_t            *options;
+    p4est_connectivity_t    *conn = NULL;
+    fclaw2d_map_context_t   *cont = NULL;
+
+    fclaw2d_domain_t	    *domain = NULL;
+
+    amr_options_t            samr_options, *gparms = &samr_options;
+    fclaw2d_clawpack_parms_t sclawpack_parms, *clawpack_parms = &sclawpack_parms;
+
+    /* double theta, phi; */
+    fclaw2d_map_data_t smap_data, *map_data = &smap_data;
+    double pi = M_PI;
+    double rotate[2];
+
+    int example, retval;
+
+    lp = SC_LP_PRODUCTION;
+    mpicomm = sc_MPI_COMM_WORLD;
+    fclaw_mpi_init (&argc, &argv, mpicomm, lp);
 
 #ifdef MPI_DEBUG
-  /* this has to go after MPI has been initialized */
-  fclaw2d_mpi_debug();
+    /* this has to go after MPI has been initialized */
+    fclaw2d_mpi_debug();
 #endif
 
-  /* ---------------------------------------------------------------
-     Read in parameters and options
-     --------------------------------------------------------------- */
-  options = sc_options_new (argv[0]);
-  sc_options_add_int (options, 0, "example", &example, 0,
-                      "1 for pillow grid, "\
-                      "2 for cubed sphere ");
+    /* ---------------------------------------------------------------
+       Read in parameters and options
+       --------------------------------------------------------------- */
+    options = sc_options_new (argv[0]);
+    sc_options_add_int (options, 0, "example", &example, 0,
+                        "[Example] 1 for pillow grid, "\
+                        "2 for cubed sphere ");
 
-  sc_options_add_double (options, 0, "theta", &theta, 0,
-                         "Rotation angle theta (degrees) about z axis [0]");
+    /* [Mapping] General mapping functions */
+    fclaw2d_register_map_data(options,map_data); /* sets default values */
+    fclaw2d_read_options_from_file(options);  /* Read options from fclaw2d_defaults.ini */
+    rotate[0] = pi*map_data->theta/180.0;
+    rotate[1] = pi*map_data->phi/180.0;
 
-  sc_options_add_double (options, 0, "phi", &phi, 0,
-                         "Rotation angle phi (degrees) about x axis [0]");
+    /* [forestclaw] General mapping functions */
+    fclaw2d_register_options(options,gparms);
+    fclaw2d_read_options_from_file(options);
+    fclaw2d_postprocess_parms(gparms);
 
-  /* Read parameters from .ini file */
-  gparms = amr_options_new (options); // Sets default values
-  clawpack_parms = fclaw2d_clawpack_parms_new(options);
+    /* [clawpack46] Clawpack solver options */
+    clawpack46_register_options(options,clawpack_parms);
+    clawpack46_read_options_from_file(options);
+    clawpack46_postprocess_parms(clawpack_parms);
 
-  amr_options_parse (options, argc, argv, lp);  // Reads options from a file
+    /* This prints out all the current options */
+    fclaw2d_parse_command_line (options, argc, argv, lp);
 
-  amr_postprocess_parms (gparms);
-  fclaw2d_clawpack_postprocess_parms(clawpack_parms);
+    /* [forestclaw] */
+    fclaw2d_postprocess_parms (gparms);
+    clawpack46_postprocess_parms(clawpack_parms);
 
-  /* Read in clawpack parms, process and check them */
-  amr_checkparms (gparms);
-  fclaw2d_clawpack_checkparms(clawpack_parms,gparms);
+    retval = fclaw2d_checkparms (options, gparms, lp);
+    retval = retval || clawpack46_checkparms(options,clawpack_parms,gparms,lp);
+    retval = retval || sphere_checkparms (example, lp);
 
-  /* ---------------------------------------------------------------
-     Floating point traps
-     -------------------------------------------------------------- */
-  if (gparms->trapfpe == 1)
-  {
-      printf("Enabling floating point traps\n");
-      feenableexcept(FE_INVALID);
-  }
+    if (!retval)
+    {
+        if (gparms->trapfpe == 1)
+        {
+            printf("Enabling floating point traps\n");
+            feenableexcept(FE_INVALID);
+        }
 
-  /* ---------------------------------------------------------------
-     Domain geometry
-     --------------------------------------------------------------- */
+        switch (example) {
+        case 1:
+            conn = p4est_connectivity_new_pillow();
+            cont = fclaw2d_map_new_pillowsphere(map_data->scale,map_data->shift,rotate);
+            break;
+        case 2:
+            conn = p4est_connectivity_new_cubed();
+            cont = fclaw2d_map_new_cubedsphere(map_data->scale,map_data->shift,rotate);
+            break;
+        default:
+            SC_ABORT_NOT_REACHED (); /* must be checked in torus_checkparms */
+        }
 
-  double pi = M_PI;
-  double rotate[2];
-  rotate[0] = pi*theta/180.0;
-  rotate[1] = pi*phi/180.0;
-  double scale[3];
-  double shift[3];
+        domain = fclaw2d_domain_new_conn_map (mpicomm, gparms->minlevel, conn, cont);
 
-  switch (example) {
-  case 1:
-      conn = p4est_connectivity_new_pillow();
-      cont = fclaw2d_map_new_pillowsphere(scale,shift,rotate);
-      break;
-  case 2:
-      conn = p4est_connectivity_new_cubed();
-      cont = fclaw2d_map_new_cubedsphere(scale,shift,rotate);
-      break;
-    default:
-      sc_abort_collective ("Parameter example must be 1 (pillow sphere) or 2 (cubed sphere)");
-  }
+        /* ---------------------------------------------------------------
+           Print some diagnostics.  TODO: Should this really be in main()?
+           --------------------------------------------------------------- */
 
-  domain = fclaw2d_domain_new_conn_map (mpicomm, gparms->minlevel, conn, cont);
+        if (gparms->verbosity > 0)
+        {
+            fclaw2d_domain_list_levels(domain, lp);
+            fclaw2d_domain_list_neighbors(domain, lp);
+        }
 
-  /* ---------------------------------------------------------------
-     Print some diagnostics.  TODO: Should this really be in main()?
-     --------------------------------------------------------------- */
+        /* ---------------------------------------------------------------
+           Set domain data.
+           --------------------------------------------------------------- */
+        init_domain_data(domain);
 
-  if (gparms->verbosity > 0)
-  {
-      fclaw2d_domain_list_levels(domain, lp);
-      fclaw2d_domain_list_neighbors(domain, lp);
-  }
+        /* Store parameters */
+        set_domain_parms(domain,gparms);
+        set_clawpack_parms(domain,clawpack_parms);
 
-  /* ---------------------------------------------------------------
-     Set domain data.
-     --------------------------------------------------------------- */
-  init_domain_data(domain);
+        /* Link solvers to the domain */
+        link_problem_setup(domain,sphere_problem_setup);
 
-  /* Store parameters */
-  set_domain_parms(domain,gparms);
-  set_clawpack_parms(domain,clawpack_parms);
+        sphere_link_solvers(domain);
 
-  /* Link solvers to the domain */
-  link_problem_setup(domain,sphere_problem_setup);
+        /* --------------------------------------------------
+           Initialize and run the simulation
+           -------------------------------------------------- */
+        amrinit(&domain);
+        amrrun(&domain);
+        amrreset(&domain);
 
-  sphere_link_solvers(domain);
+        /* --------------------------------------------------
+           Clean up the mapping context.
+           -------------------------------------------------- */
+        fclaw2d_map_destroy (cont);
+    }
 
-  /* --------------------------------------------------
-     Initialize and run the simulation
-     -------------------------------------------------- */
-  amrinit(&domain);
-  amrrun(&domain);
-  amrreset(&domain);
+    fclaw2d_options_destroy(gparms);  /* Delete any array options */
+    sc_options_destroy(options);
+    fclaw2d_clawpack_parms_delete(clawpack_parms);
 
-  /* --------------------------------------------------
-     Clean up.
-     -------------------------------------------------- */
-  fclaw2d_map_destroy (cont);
-  amr_options_destroy(gparms);
-  sc_options_destroy(options);
-  fclaw2d_clawpack_parms_delete(clawpack_parms);
+    fclaw_mpi_finalize ();
 
-  fclaw_mpi_finalize ();
-
-  return 0;
+    return 0;
 }
--- applications/test_parms/test_parms.cpp
+++ applications/test_parms/test_parms.cpp
@@ -1,107 +1,108 @@
 /*
-Copyright (c) 2012 Carsten Burstedde, Donna Calhoun
-All rights reserved.
+  Copyright (c) 2012 Carsten Burstedde, Donna Calhoun
+  All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
 
- * Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
+  * Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <fclaw2d_clawpack.H>
+#include <fclaw2d_options.h>
 
-
-int
-main (int argc, char **argv)
+static int
+    test_parms_checkparms (int example, int lp)
 {
-  int		        lp;
-  sc_MPI_Comm           mpicomm;
-  sc_options_t          *options;
-
-  lp = SC_LP_PRODUCTION;
-  mpicomm = sc_MPI_COMM_WORLD;
-  fclaw_mpi_init (&argc, &argv, mpicomm, lp);
-
-
-  /* ----------------------------------------------------------
-     Read in command line options
-     ---------------------------------------------------------- */
-  int example;
-  options = sc_options_new (argv[0]);
-  sc_options_add_int (options, 0, "example", &example, 0,
-                      "1 for AMR options, " \
-                      "2 for Clawpack parms");
-
-  /* ---------------------------------------------------------------
-     Read parameters from .ini file, parse command line, and
-     do parameter checking.
-     -------------------------------------------------------------- */
-  amr_options_parse(options,argc,argv,lp);
+    if (example < 1 || example > 2) {
+        fclaw2d_global_log (lp, "Option --example must be 1 or 2\n");
+        return -1;
+    }
+    return 0;
+}
 
 
-  switch (example) {
-  case 1:
-      amr_options_t  *gparms;
+int main (int argc, char **argv)
+{
+    int		              lp;
+    sc_MPI_Comm               mpicomm;
+    sc_options_t             *options;
+    fclaw2d_clawpack_parms_t  sclawparms,  *clawpack_parms = &sclawparms;
+    amr_options_t             sparms, *gparms = &sparms;
 
-      /* [forestclaw] General ForestClaw parameters */
-      gparms = fclaw2d_new_options();
-      fclaw2d_register_options(options,gparms);
-      fclaw2d_read_options_from_file(options);
-      amr_postprocess_parms(gparms);  /* convert array inputs */
+    int example, retval;
+    fclaw2d_map_data_t smap_data, *map_data=&smap_data;
 
-      /* Command line arguments */
-      amr_options_parse(options,argc,argv,lp);
+    lp = SC_LP_PRODUCTION;
+    mpicomm = sc_MPI_COMM_WORLD;
+    fclaw_mpi_init (&argc, &argv, mpicomm, lp);
 
-      amr_postprocess_parms(gparms);
-      amr_checkparms2(options,gparms,lp);
+    options = sc_options_new (argv[0]);
 
-      amr_options_destroy(gparms);
+    /* [Example] Register example option */
+    sc_options_add_int (options, 0, "example", &example, 0,
+                        "[Example] Example 1 or 2. ");
 
-      break;
-  case 2:
-      fclaw2d_clawpack_parms_t  *clawpack_parms;
+    fclaw2d_read_options_from_file(options);  /* Read options from fclaw2d_defaults.ini */
+    fclaw2d_parse_command_line(options,argc,argv,lp);
+    retval = test_parms_checkparms(example,lp);
 
-      /* [clawpack46] Parameters from Clawpack solver */
-      clawpack_parms = clawpack46_new_options();
-      clawpack46_register_options(options,clawpack_parms);
-      clawpack46_read_options_from_file(options);
-      fclaw2d_clawpack_postprocess_parms(clawpack_parms);  /* convert array inputs */
+    /* [Mapping] General mapping parameters (mi,mj,scale,shift,phi,theta) */
+    fclaw2d_register_map_data(options,map_data); /* sets default values */
+    fclaw2d_read_options_from_file(options);  /* Read options from fclaw2d_defaults.ini */
+    fclaw2d_parse_command_line(options,argc,argv,lp);
 
-      /* Command line arguments */
-      amr_options_parse(options,argc,argv,lp);
+    /* [forestclaw] General ForestClaw parameters */
+    fclaw2d_register_options(options,gparms);
+    fclaw2d_read_options_from_file(options);
+    fclaw2d_parse_command_line(options,argc,argv,lp);
+    fclaw2d_postprocess_parms(gparms);  /* convert array inputs */
+    retval = retval || fclaw2d_checkparms(options,gparms,lp);
 
-      fclaw2d_clawpack_postprocess_parms(clawpack_parms);
+    /* [clawpack46] Parameters from Clawpack solver */
+    clawpack46_register_options(options,clawpack_parms);
+    clawpack46_read_options_from_file(options);
+    fclaw2d_parse_command_line(options,argc,argv,lp);
+    clawpack46_postprocess_parms(clawpack_parms);  /* convert array inputs */
+    retval = retval || clawpack46_checkparms(options,clawpack_parms,gparms,lp);
 
-      fclaw2d_clawpack_parms_delete(clawpack_parms);
+    if (!retval)
+    {
+        printf("\n");
+        switch (example)
+        {
+        case 1:
+            printf("Running example 1 ... Done!\n");
+            break;
+        case 2:
+            printf("Running example 2 ... Done!\n");
+            break;
+        default:
+            SC_ABORT_NOT_REACHED (); /* must be checked in torus_checkparms */
+        }
+        printf("\n");
+    }
 
-      break;
-  default:
-      sc_abort_collective ("Use command line --example=1 or --example=2");
-  }
+    fclaw2d_clawpack_parms_delete(clawpack_parms);
+    fclaw2d_options_destroy(gparms);
+    sc_options_destroy (options);
 
+    fclaw_mpi_finalize ();
 
-  /* -----------------------------------------------
-     Skip everything that actually does something...
-     ----------------------------------------------- */
-
-  sc_options_destroy (options);
-
-  fclaw_mpi_finalize ();
-
-  return 0;
+    return 0;
 }
