58a7c8bda64331b108417404553d0756b9b1f2f5
Donna <donna.calhoun@gmail.com>
2014-12-10 19:03:18 -0700

    Add third argument which can be used to add space between bricks (for plotting)

--- matlab/mapc2m_brick.m
+++ matlab/mapc2m_brick.m
@@ -1,6 +1,12 @@
-function [xp,yp,zp] = mapc2m_brick(xc,yc)
+function [xp,yp,zp] = mapc2m_brick(xc,yc,s)
 
-% (mi,mj) brick
+if (nargin < 3)
+   s = 0;
+end
+
+% This adds some space between blocks.
+xc = (1-s)*(xc - 0.5) + 0.5;
+yc = (1-s)*(yc - 0.5) + 0.5;
 
 brick_data = load('brick.dat');
 mi = brick_data(1,1);
