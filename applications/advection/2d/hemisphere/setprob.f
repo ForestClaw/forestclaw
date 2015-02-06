90bb2016be272323522bf42293711cbdbe6352a2
donnacalhoun <donnacalhoun@boisestate.edu>
2015-02-06 10:13:54 -0700

    (hemisphere) Cleaned up setprob.f

--- applications/advection/2d/hemisphere/setprob.f
+++ applications/advection/2d/hemisphere/setprob.f
@@ -2,37 +2,14 @@
       implicit none
 
       double precision kappa,tfinal
-      double precision rot_angle(2), scale
-      logical isflat
-      integer get_map_value
-      logical ishemisphere
 
       double precision pi
       common /compi/ pi
 
       pi = 4.d0*atan(1.d0)
 
-c     # read in data to see whether we should run with fixed rotation or not.
-      open(7,file='setprob.data')
-      read(7,*) kappa
-      read(7,*) Tfinal
-      close(7)
-
-c     # These are the values that work well with the hemisphere.  Other
-c     # values don't work so well, since we don't have any inflow
-c     # conditions set.
-
       kappa = 0
       tfinal = 5.0
       call set_wind_parms(kappa,tfinal);
 
-c     # -------------------------------------------------
-c     # Mapping routines
-c     # -------------------------------------------------
-
-c     # Set mapping scaling
-      scale = 1
-      rot_angle(1) = 0
-      rot_angle(2) = 0
-c      call setup_mappedgrid(rot_angle,scale)
       end
