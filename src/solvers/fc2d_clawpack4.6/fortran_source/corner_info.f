9fad48dfaed7149dfab272e235797641a3037ef2
donnacalhoun <donnacalhoun@boisestate.edu>
2015-02-06 11:21:22 -0700

    (clawpack) files in new directory

---
+++ src/solvers/fc2d_clawpack4.6/fortran_source/corner_info.f
@@ -0,0 +1,30 @@
+      subroutine set_corners(block_corner_count)
+      implicit none
+
+      integer block_corner_count(0:3)
+      integer i
+      integer fclaw2d_corner_com(0:3)
+
+      common /comcorners/ fclaw2d_corner_com
+
+      do i = 0,3
+         fclaw2d_corner_com(i) = block_corner_count(i)
+      enddo
+
+      end
+
+      subroutine get_corners(block_corner_count)
+      implicit none
+
+      integer block_corner_count(0:3)
+
+      integer i
+      integer fclaw2d_corner_com(0:3)
+
+      common /comcorners/ fclaw2d_corner_com
+
+      do i = 0,3
+         block_corner_count(i) = fclaw2d_corner_com(i)
+      enddo
+
+      end
