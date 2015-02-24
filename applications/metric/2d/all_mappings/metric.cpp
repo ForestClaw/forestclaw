1e54217fb214c8fa91e4b3904fcda493de6f80b1
donnaaboise <donna.calhoun@gmail.com>
2015-02-24 16:10:37 -0700

    (metric) Add new registration process

--- applications/metric/2d/all_mappings/metric.cpp
+++ applications/metric/2d/all_mappings/metric.cpp
@@ -27,6 +27,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <amr_utils.H>
 #include <fclaw2d_map.h>
 
+#include <fclaw_register.h>
 #include "metric_user.H"
 
 typedef struct user_options
@@ -88,7 +89,7 @@ void register_user_options (fclaw_app_t * app,
 }
 
 
-void run_program(fclaw_app_t* app, amr_options_t* gparms,
+void run_program(fclaw_app_t* app,
                  user_options_t* user)
 {
     sc_MPI_Comm            mpicomm;
@@ -98,9 +99,12 @@ void run_program(fclaw_app_t* app, amr_options_t* gparms,
     fclaw2d_domain_t	     *domain;
     fclaw2d_map_context_t    *cont = NULL;
 
+    amr_options_t* gparms;
+
 
     mpicomm = fclaw_app_get_mpi_size_rank (app, NULL, NULL);
 
+    gparms = fclaw_forestclaw_get_options(app);
 
     /* ---------------------------------------------------------------
        Domain geometry
@@ -218,7 +222,6 @@ main (int argc, char **argv)
 
   /* Options */
   sc_options_t              *options;
-  amr_options_t             samr_options, *gparms = &samr_options;
   user_options_t                suser_options, *user = &suser_options;
 
   int retval;
@@ -227,7 +230,7 @@ main (int argc, char **argv)
   app = fclaw_app_new (&argc, &argv, user);
   options = fclaw_app_get_options (app);
 
-  fclaw_options_register_general (app, "fclaw_options.ini", gparms);
+  fclaw_forestclaw_register(app,"fclaw_options.ini");
 
   /* User defined options (defined above) */
   register_user_options (app, "fclaw_options.ini", user);
@@ -243,9 +246,10 @@ main (int argc, char **argv)
 
   if (!retval & !vexit)
   {
-      run_program(app, gparms,user);
+      run_program(app,user);
   }
 
+  fclaw_forestclaw_destroy(app);
   fclaw_app_destroy (app);
 
   return 0;
