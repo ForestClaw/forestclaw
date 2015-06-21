b43e80ba00139d8d280d8688ec758af43b0a812c
donnaaboise <donna.calhoun@gmail.com>
2015-06-21 18:50:36 +0100

    (cubedsphere) Parallel is working

--- src/fclaw2d_corner_neighbors.cpp
+++ src/fclaw2d_corner_neighbors.cpp
@@ -475,7 +475,6 @@ void cb_corner_fill(fclaw2d_domain_t *domain,
                 {
                     /* Interpolate from remote patch (coarse grid) to 'this' patch (fine grid) */
                     int coarse_icorner = transform_data_finegrid.icorner;
-
                     fedisableexcept(FE_INVALID);
                     coarse_cp->interpolate_corner_ghost(coarse_icorner,refratio,fine_cp,
                                                       time_interp,&transform_data_finegrid);
