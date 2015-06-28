3ee6a75764820c1f949aacbba8678b84ddfd199f
donnaaboise <donna.calhoun@gmail.com>
2015-06-28 13:04:10 +0100

    (mountain) Fix header h/H issue

--- applications/Mountain/mountain.cpp
+++ applications/Mountain/mountain.cpp
@@ -23,7 +23,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include "mountain_user.H"
+#include "mountain_user.h"
 
 #include "fclaw2d_forestclaw.h"
 #include "fclaw2d_clawpatch.h"
