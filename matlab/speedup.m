655d192ed9a2b1162ac11258aeb74f199405985b
donnaaboise <donna.calhoun@gmail.com>
2015-06-28 12:54:40 +0100

    Added plots of efficiency

--- matlab/speedup.m
+++ matlab/speedup.m
@@ -14,9 +14,6 @@
 %
 
 function alphaout = speedup(p,T)
-figure(2);
-clf;
-
 T = T(:);
 p = p(:);
 
@@ -25,6 +22,8 @@ a = polyfit(-log(p),log(T),1);
 alpha = a(1);
 C = exp(a(2));
 
+figure(2);
+clf;
 loglog(p,T,'.','markersize',30);
 hold on;
 ph(1) = loglog(p,C*p.^(-alpha),'r');
@@ -32,11 +31,42 @@ ph(1) = loglog(p,C*p.^(-alpha),'r');
 ph(2) = loglog(p,T(1)./p,'k--');
 loglog(p,T(1)./p,'k.','markersize',30);
 
-title('Strong scaling','fontsize',18);
+title('Strong scaling (slope = \alpha)','fontsize',18);
 str1 = sprintf('Actual (\\alpha = %6.3f)\n',alpha);
 str2 = sprintf('Optimal (\\alpha = 1)');
 lh = legend(ph,{str1,str2},'location','southwest');
+xlabel('Proc count','fontsize',16);
+ylabel('Speed-up','fontsize',16);
 set(lh,'fontsize',16);
+set(gca,'fontsize',16);
+
+% efficiency vs. speedup.
+clear ph;
+figure(3)
+clf
+pv = logspace(0,5,1000);
+T_llsq = C*pv.^(-alpha);
+S_llsq = T(1)./T_llsq;
+S_computed = T(1)./T;
+S_true = pv;
+
+eff_llsq = S_llsq./pv;
+eff_computed = S_computed./p;
+eff_true = S_true./pv;
+semilogx(p,eff_computed,'.','markersize',30);
+hold on;
+ph(1) = semilogx(pv,eff_llsq,'b');
+ph(2) = semilogx(pv,eff_true,'b--');
+
+
+title('Efficiency (S/p)','fontsize',18);
+str1 = sprintf('Actual');
+str2 = sprintf('Optimal (eff. = 1)');
+lh = legend(ph([1 2]),{str1,str2},'location','southwest');
+set(lh,'fontsize',16);
+xlabel('Proc count','fontsize',16);
+ylabel('Efficiency','fontsize',16);
+set(gca,'fontsize',16);
 
 
 S = T(1)./T;
