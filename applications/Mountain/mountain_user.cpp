49b8be57b163f66f42887783a1fe5159488d60e9
donnaaboise <donna.calhoun@gmail.com>
2015-06-28 13:01:56 +0100

    (mountain) Fix header h/H issue

--- applications/Mountain/mountain_user.cpp
+++ applications/Mountain/mountain_user.cpp
@@ -23,11 +23,11 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include "mountain_user.H"
+#include "mountain_user.h"
 
-#include "fclaw2d_forestclaw.H"
-#include "fclaw2d_clawpatch.H"
-#include "fc2d_clawpack46.H"
+#include "fclaw2d_forestclaw.h"
+#include "fclaw2d_clawpatch.h"
+#include "fc2d_clawpack46.h"
 
 
 static fc2d_clawpack46_vtable_t classic_claw;
