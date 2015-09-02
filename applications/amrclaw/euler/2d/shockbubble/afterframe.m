a3ec465240ef6f2752765ef215ff86a22b4f8088
donnaaboise <donna.calhoun@gmail.com>
2015-09-01 20:43:49 -0600

    (shockbubble) Matlab/ini files

--- applications/amrclaw/euler/2d/shockbubble/afterframe.m
+++ applications/amrclaw/euler/2d/shockbubble/afterframe.m
@@ -35,45 +35,23 @@ if (prt)
     MaxFrames = 31;
     axis off;
     delete(get(gca,'title'));
-    set(gcf,'papersize',[4,1]);
+    set(gcf,'papersize',[8,2]);
     set(gca,'position',[0 0 1 1]);
-    set(gcf,'paperposition',[0 0 4 1]);
+    set(gcf,'paperposition',[0 0 8 2]);
     id = input('Input id to use : ');
     if ~isempty(id)
     
-        % With mesh
-        setpatchborderprops('linewidth',1);
-        hidegridlines;
-        if (PlotType == 3)
-            fname = sprintf('results_%03d/fc_sb_schlrn_%02d.png',id,Frame);            
-        else
-            fname = sprintf('results_%03d/fc_sb_%02d.png',id,Frame);
-        end
-        yn = 'y';
-        if (exist(fname))
-            str = sprintf('Are you sure you want to overwrite %s (y/[n]) ? ',...
-                fname);            
-            yn = input(str,'s');               
-            if isempty(yn)
-                yn = 'n';
-            end
-        end      
-        if (strcmp(lower(yn),'y') == 1)
-            fprintf('Printing %s\n',fname);
-            print('-r1024','-dpng',fname);
-        end
-        
         % No mesh
         hidegridlines;
         hidepatchborders;
         if (PlotType == 3)
-            fname = sprintf('results_%03d/fc_sb_schlrn_nomesh_%02d.png',id,Frame);            
+            fname = sprintf('results_%03d/fc_sb_schlrn_nomesh_%04d.png',id,Frame);            
         else
-            fname = sprintf('results_%03d/fc_sb_nomesh_%02d.png',id,Frame);
+            fname = sprintf('results_%03d/fc_sb_%04d.png',id,Frame);
         end
         if (strcmp(lower(yn),'y') == 1)
             fprintf('Printing %s\n',fname);
-            print('-r1024','-dpng',fname);
+            print('-r512','-dpng',fname);
         end
                 
     end
