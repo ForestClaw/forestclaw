61bf07d6f97c58da03cd71fcd8db059f8a805554
donnaaboise <donna.calhoun@gmail.com>
2016-01-03 22:20:25 +0000

    Update conservation check routines

--- src/fortran_source2d/fclaw2d_diagnostics_fort.f
+++ src/fortran_source2d/fclaw2d_diagnostics_fort.f
@@ -1,34 +1,38 @@
 c    # ----------------------------------------------------------------------------------
 c    # Output and diagnostics
 c    # ----------------------------------------------------------------------------------
-      subroutine compute_sum(mx,my,mbc,meqn,dx,dy,q,area,sum)
+      subroutine fclaw2d_fort_conservation_check(mx,my,mbc,meqn,
+     &      dx,dy,area,q,sum)
       implicit none
 
       integer mx,my,mbc,meqn
-      double precision dx, dy, sum
+      double precision dx, dy, dxdy
+      double precision sum(meqn)
       double precision q(1-mbc:mx+mbc,1-mbc:my+mbc,meqn)
 
       include 'metric_terms.i'
 
-      integer i,j
+      integer i,j,m
       integer*8 cont, get_context
       logical fclaw2d_map_is_used
 
       cont = get_context()
 
-      if (fclaw2d_map_is_used(cont)) then
-         sum = 0
-         do i = 1,mx
+      dxdy = dx*dy
+      do m = 1,meqn
+         if (fclaw2d_map_is_used(cont)) then
             do j = 1,my
-               sum = sum + q(i,j,1)*area(i,j)
+               do i = 1,mx
+                  sum(m) = sum(m) + q(i,j,1)*area(i,j)
+               enddo
             enddo
-         enddo
-      else
-         sum = 0
-         do i = 1,mx
+         else
             do j = 1,my
-               sum = sum + q(i,j,1)*dx*dy
+               do i = 1,mx
+                  sum(m) = sum(m) + q(i,j,1)*dx*dy
+               enddo
             enddo
-         enddo
-      endif
+         endif
+      enddo
+
       end
