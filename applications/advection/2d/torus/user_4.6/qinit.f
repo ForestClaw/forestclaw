2c30af919d5a3a4fcf104a019d08917223b5b23a
donnaaboise <donna.calhoun@gmail.com>
2016-02-15 15:17:47 -0700

    Twisted torus mapping

--- applications/advection/2d/torus/user_4.6/qinit.f
+++ applications/advection/2d/torus/user_4.6/qinit.f
@@ -10,7 +10,8 @@
 
       integer i,j
       integer blockno, fc2d_clawpack46_get_block
-      double precision x,y,z, xlow, ylow, w
+      double precision x,y,z, xlow, ylow, w,xc,yc
+      double precision q0
 
       blockno = fc2d_clawpack46_get_block()
 
@@ -20,6 +21,10 @@
             ylow = ylower + (j-1)*dy
             call cellave2(blockno,xlow,ylow,dx,dy,w)
             q(i,j,1) = w
+
+c            xc = xlow + dx/2.d0
+c            yc = ylow + dy/2.d0
+c            q(i,j,1) = q0(blockno,xc,yc)
          enddo
       enddo
 
@@ -33,7 +38,7 @@
       double precision xc,yc, xp, yp, zp, rp
       integer blockno
       integer*8 cont, get_context
-      double precision th, tp
+      double precision th, tp, r2, r0
       logical iscart
       logical fclaw2d_map_is_used
 
@@ -49,9 +54,15 @@
             rp = sqrt(xp**2 + yp**2)
             fdisc = rp-0.25d0
          else
+c           # Torus or annulus
             th = atan2(yp,xp)
             tp = abs(th + pi/2.d0)
             fdisc = tp - pi/8.d0
+
+c           # Sphere
+            r0 = 0.4d0
+    2       r2 = (xp - 1.0)**2 + yp**2 + (zp-0.4d0)**2
+            fdisc = r2 - r0**2
          endif
       else
          xp = xc
@@ -59,5 +70,45 @@
          rp = sqrt(xp**2 + yp**2)
          fdisc = rp-0.25d0
       endif
+
+      end
+
+      double precision function  q0(blockno,xc,yc)
+      implicit none
+
+      double precision xc,yc, xp, yp, zp, rp
+      integer blockno
+      integer*8 cont, get_context
+      double precision r,r0
+      logical fclaw2d_map_is_used
+      double precision Hsmooth
+
+      double precision pi
+      common /compi/ pi
+
+      cont = get_context()
+
+      if (fclaw2d_map_is_used(cont)) then
+         call fclaw2d_map_c2m(cont,
+     &         blockno,xc,yc,xp,yp,zp)
+
+         r0 = 0.4d0
+         r = sqrt((xp - 1.0)**2 + yp**2 + (zp-r0)**2)
+         q0 = Hsmooth(r + r0) - Hsmooth(r - r0)
+      else
+         xp = xc
+         yp = yc
+         rp = sqrt(xp**2 + yp**2)
+         q0 = rp-0.25d0
+      endif
+
+      end
+
+      double precision function Hsmooth(r)
+      implicit none
+
+      double precision r
+
+      Hsmooth = (tanh(r/0.05d0) + 1)/2.0
 
       end
--- matlab/mapc2m_twisted_torus.m
+++ matlab/mapc2m_twisted_torus.m
@@ -1,10 +1,10 @@
-function [xp,yp,zp] = mapc2m_torus(xc,yc)
+function [xp,yp,zp] = mapc2m_twisted_torus(xc,yc)
 
-alpha = 0.4;
+alpha = 0.4;  % outer radius/inner radius
 
 r = 1 + alpha*cos(2*pi*(xc+yc));
 xp = r.*cos(2*pi*xc);
 yp = r.*sin(2*pi*xc);
-zp = alpha.*sin(2*pi*(yc+yc));
+zp = alpha.*sin(2*pi*(xc+yc));
 
 end
