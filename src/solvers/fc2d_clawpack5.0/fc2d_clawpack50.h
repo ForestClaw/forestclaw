ab423614168b9a9afd085caea38f998769519ec6
YuHsuan Shih <yoyoshih13@gmail.com>
2016-04-02 15:38:08 -0400

    Create clawpack version 5.0 solver

---
+++ src/solvers/fc2d_clawpack5.0/fc2d_clawpack50.h
@@ -0,0 +1,381 @@
+/*
+  Copyright (c) 2012 Carsten Burstedde, Donna Calhoun
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef FC2D_CLAWPACK50_H
+#define FC2D_CLAWPACK50_H
+
+#include <fclaw2d_forestclaw.h>
+#include <fclaw_package.h>
+
+#include "fc2d_clawpack50_options.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#if 0
+}
+#endif
+#endif
+
+
+typedef void (*fc2d_clawpack50_setprob_t)();
+
+typedef void (*fc2d_clawpack50_bc2_t)(const int* maxmx, const int* maxmy,
+                                      const int* meqn, const int* mbc,
+                                      const int* mx, const int* my,
+                                      const double* xlower, const double* ylower,
+                                      const double* dx, const double* dy,
+                                      const double q[], const int* maux,
+                                      const double aux[], const double* t,
+                                      const double* dt, const int mthbc[]);
+
+typedef  void (*fc2d_clawpack50_qinit_t)(const int* maxmx, const int* maxmy,
+                                         const int* meqn,const int* mbc,
+                                         const int* mx, const int* my,
+                                         const double* xlower, const double* ylower,
+                                         const double* dx, const double* dy,
+                                         double q[], const int* maux, double aux[]);
+
+typedef void (*fc2d_clawpack50_setaux_t)(const int* maxmx, const int* maxmy, const int* mbc,
+                                         const int* mx, const int* my,
+                                         const double* xlower, const double* ylower,
+                                         const double* dx, const double* dy,
+                                         const int* maux, double aux[]);
+
+typedef void (*fc2d_clawpack50_b4step2_t)(const int* maxmx, const int* maxmy,
+                                          const int* mbc,
+                                          const int* mx, const int* my, const int* meqn,
+                                          double q[], const double* xlower,
+                                          const double* ylower,
+                                          const double* dx, const double* dy,
+                                          const double* t, const double* dt,
+                                          const int* maux, double aux[]);
+
+typedef void (*fc2d_clawpack50_src2_t)(const int* maxmx, const int* maxmy, const int* meqn,
+                                       const int* mbc, const int* mx,const int* my,
+                                       const double* xlower, const double* ylower,
+                                       const double* dx, const double* dy, double q[],
+                                       const int* maux, double aux[], const double* t,
+                                       const double* dt);
+
+typedef void (*fc2d_clawpack50_rpn2_t)(const int* ixy,const int* maxm, const int* meqn,
+                                       const int* mwaves, const int* mbc,const int* mx,
+                                       double ql[], double qr[], double auxl[], double auxr[],
+                                       double wave[], double s[],double amdq[], double apdq[]);
+
+
+typedef void (*fc2d_clawpack50_rpt2_t)(const int* ixy, const int* maxm, const int* meqn,
+                                       const int* mwaves, const int* mbc,const int* mx,
+                                       double ql[], double qr[], double aux1[], double aux2[],
+                                       double aux3[], const int* imp, double dsdq[],
+                                       double bmasdq[], double bpasdq[]);
+
+
+typedef void (*fc2d_clawpack50_flux2_t)(const int* ixy,const int* maxm, const int* meqn,
+                                        const int* maux,const int* mbc,const int* mx,
+                                        double q1d[], double dtdx1d[],
+                                        double aux1[], double aux2[], double aux3[],
+                                        double faddm[],double faddp[], double gaddm[],
+                                        double gaddp[],double cfl1d[], double fwave[],
+                                        double s[], double amdq[],double apdq[],double cqxx[],
+                                        double bmasdq[], double bpasdq[],
+                                        fc2d_clawpack50_rpn2_t rpn2,
+                                        fc2d_clawpack50_rpt2_t rpt2,
+                                        const int* mwaves, const int* mcapa,
+                                        int method[], int mthlim[]);
+
+typedef void (*fc2d_clawpack50_fluxfun_t)(const int* meqn, double q[], double aux[],
+                                          double fq[]);
+
+
+typedef struct fc2d_clawpack50_vtable
+{
+    fc2d_clawpack50_setprob_t setprob;
+    fc2d_clawpack50_bc2_t bc2;
+    fc2d_clawpack50_qinit_t qinit;
+    fc2d_clawpack50_setaux_t setaux;
+    fc2d_clawpack50_b4step2_t b4step2;
+    fc2d_clawpack50_src2_t src2;
+    fc2d_clawpack50_rpn2_t rpn2;
+    fc2d_clawpack50_rpt2_t rpt2;
+    fc2d_clawpack50_fluxfun_t fluxfun;
+} fc2d_clawpack50_vtable_t;
+
+
+void fc2d_clawpack50_set_vtable(const fc2d_clawpack50_vtable_t* vt);
+
+void fc2d_clawpack50_init_vtable(fc2d_clawpack50_vtable_t* vt);
+
+#define CLAWPACK50_BC2 FCLAW_F77_FUNC(clawpack50_bc2,CLAWPACK50_BC2)
+void CLAWPACK50_BC2(const int* maxmx, const int* maxmy, const int* meqn,
+                     const int* mbc, const int* mx, const int* my,
+                     const double* xlower, const double* ylower,
+                     const double* dx, const double* dy, const double q[],
+                     const int* maux, const double aux[], const double* t,
+                     const double* dt, const int mthbc[]);
+
+/* --------------------------------------------------------------------
+   Classic routines
+   - These are provided only for convenience;  these files are not
+   compiled into the library, but will be provided by the user.
+   -------------------------------------------------------------------- */
+
+/* Macros for C/Fortran portability */
+#define SETPROB FCLAW_F77_FUNC(setprob,SETPROB)
+#define QINIT   FCLAW_F77_FUNC(qinit,QINIT)
+#define SETAUX  FCLAW_F77_FUNC(setaux,SETAUX)
+#define B4STEP2 FCLAW_F77_FUNC(b4step2,B4STEP2)
+#define SRC2    FCLAW_F77_FUNC(src2,SRC2)
+#define BC2     FCLAW_F77_FUNC(bc2,BC2)
+#define RPN2    FCLAW_F77_FUNC(rpn2,RPN2)
+#define RPT2    FCLAW_F77_FUNC(rpt2,RPT2)
+
+/* These will be converted to MACROS slowly ... */
+
+void SETPROB();
+
+void QINIT(const int* maxmx, const int* maxmy, const int* meqn,
+            const int* mbc, const int* mx, const int* my,
+            const double* xlower, const double* ylower,
+            const double* dx, const double* dy,
+            double q[], const int* maux, double aux[]);
+
+void SETAUX(const int* maxmx, const int* maxmy, const int* mbc,
+             const int* mx, const int* my,
+             const double* xlower, const double* ylower,
+             const double* dx, const double* dy,
+             const int* maux, double aux[]);
+
+void BC2(const int* maxmx, const int* maxmy, const int* meqn,
+          const int* mbc, const int* mx, const int* my,
+          const double* xlower, const double* ylower,
+          const double* dx, const double* dy, const double q[],
+          const int* maux, const double aux[], const double* t,
+          const double* dt, const int mthbc[]);
+
+
+void B4STEP2(const int* maxmx, const int* maxmy, const int* mbc,
+              const int* mx, const int* my, const int* meqn,
+              double q[], const double* xlower, const double* ylower,
+              const double* dx, const double* dy,
+              const double* t, const double* dt,
+              const int* maux, double aux[]);
+
+void SRC2(const int* maxmx, const int* maxmy, const int* meqn,
+           const int* mbc, const int* mx,const int* my,
+           const double* xlower, const double* ylower,
+           const double* dx, const double* dy, double q[],
+           const int* maux, double aux[], const double* t,
+           const double* dt);
+
+/* Riemann solvers */
+void RPN2(const int* ixy,const int* maxm, const int* meqn, const int* mwaves,
+           const int* mbc,const int* mx, double ql[], double qr[],
+           double auxl[], double auxr[], double wave[],
+           double s[], double amdq[], double apdq[]);
+
+void RPT2(const int* ixy, const int* maxm, const int* meqn, const int* mwaves,
+           const int* mbc, const int* mx, double ql[], double qr[],
+           double aux1[], double aux2[], double aux3[], const int* imp,
+           double dsdq[], double bmasdq[], double bpasdq[]);
+
+/* --------------------------------------------------------------------
+   Time stepping
+   -------------------------------------------------------------------- */
+#define CLAWPACK50_STEP2_WRAP FCLAW_F77_FUNC(clawpack50_step2_wrap,CLAWPACK50_STEP2_WRAP)
+void CLAWPACK50_STEP2_WRAP(const int* maxm, const int* meqn, const int* maux,
+                            const int* mbc, const int method[], const int mthlim[],
+                            const int* mcapa, const int* mwaves, const int* mx,
+                            const int* my, double qold[], double auxold[],
+                            const double* dx, const double* dy, const double* dt,
+                            const double* cfl, double work[], const int* mwork,
+                            const double* xlower, const double* ylower, const int* level,
+                            const double* t, double fp[], double fm[], double gp[],
+                            double gm[],
+                            fc2d_clawpack50_rpn2_t rpn2,
+                            fc2d_clawpack50_rpt2_t rpt2,
+                            fc2d_clawpack50_flux2_t flux2,
+                            int block_corner_count[],int* ierror);
+
+#define CLAWPACK50_FLUX2 FCLAW_F77_FUNC(clawpack50_flux2,CLAWPACK50_FLUX2)
+void CLAWPACK50_FLUX2(const int* ixy,const int* maxm, const int* meqn,
+                      const int* maux,const int* mbc,const int* mx,
+                      double q1d[], double dtdx1d[],
+                      double aux1[], double aux2[], double aux3[],
+                      double faddm[],double faddp[], double gaddm[],
+                      double gaddp[],double cfl1d[], double fwave[],
+                      double s[], double amdq[],double apdq[],double cqxx[],
+                      double bmasdq[], double bpasdq[],
+                      fc2d_clawpack50_rpn2_t rpn2,fc2d_clawpack50_rpt2_t rpt2,
+                      const int* mwaves, const int* mcapa,
+                      int method[], int mthlim[]);
+
+#define CLAWPACK50_FLUX2FW FCLAW_F77_FUNC(clawpack50_flux2fw,CLAWPACK50_FLUX2FW)
+void CLAWPACK50_FLUX2FW(const int* ixy,const int* maxm, const int* meqn, //
+                        const int* maux,const int* mbc,const int* mx,
+                        double q1d[], double dtdx1d[],
+                        double aux1[], double aux2[], double aux3[],
+                        double faddm[],double faddp[], double gaddm[],
+                        double gaddp[],double cfl1d[], double fwave[],
+                        double s[], double amdq[],double apdq[],double cqxx[],
+                        double bmasdq[], double bpasdq[],
+                        fc2d_clawpack50_rpn2_t rpn2,fc2d_clawpack50_rpt2_t rpt2,
+                        const int* mwaves, const int* mcapa,
+                        int method[], int mthlim[]);
+
+#define CLAWPACK50_SET_CAPACITY FCLAW_F77_FUNC(clawpack50_set_capacity,CLAWPACK50_SET_CAPACITY)
+void CLAWPACK50_SET_CAPACITY(const int* mx, const int *my, const int *mbc,
+                             const double *dx, const double* dy, double area[],
+                             const int *mcapa, const int* maux, double aux[]);
+
+
+#define CLAWPACK50_SET_BLOCK FCLAW_F77_FUNC(clawpack50_set_block,CLAWPACK50_SET_BLOCK)
+void CLAWPACK50_SET_BLOCK(int* blockno);
+
+#define FC2D_CLAWPACK50_GET_BLOCK FCLAW_F77_FUNC(fc2d_clawpack50_get_block, \
+                                                 FC2D_CLAWPACK50_GET_BLOCK)
+int FC2D_CLAWPACK50_GET_BLOCK();
+
+
+#define CLAWPACK50_UNSET_BLOCK FCLAW_F77_FUNC(clawpack50_unset_block, \
+                                              CLAWPACK50_UNSET_BLOCK)
+void CLAWPACK50_UNSET_BLOCK();
+
+/***************************** MINIMAL API ******************************/
+
+void fc2d_clawpack50_register_vtable (fclaw_package_container_t *
+                                      pkg_container,
+                                      fc2d_clawpack50_options_t *
+                                      clawopt);
+
+/* -------------------------------------------------------------------------
+   New routines
+   ------------------------------------------------------------------------- */
+void fc2d_clawpack50_define_auxarray(fclaw2d_domain_t* domain,
+                                     fclaw2d_patch_t* this_patch);
+
+void fc2d_clawpack50_aux_data(fclaw2d_domain_t* domain,
+                              fclaw2d_patch_t *this_patch,
+                              double **aux, int* maux);
+
+int fc2d_clawpack50_get_maux(fclaw2d_domain_t* domain);
+void fc2d_clawpack50_maux(fclaw2d_domain_t* domain, int* maux);
+
+void fc2d_clawpack50_register (fclaw_app_t* app, const char *configfile);
+
+void fc2d_clawpack50_package_register(fclaw_app_t* app,
+                                      fc2d_clawpack50_options_t* clawopt);
+
+int fc2d_clawpack50_get_package_id (void);
+
+fc2d_clawpack50_options_t* fc2d_clawpack50_get_options(fclaw2d_domain_t *domain);
+
+/* -------------------------------------------------------------------------
+   Routines that won't change
+   ------------------------------------------------------------------------- */
+void
+    fc2d_clawpack50_setprob(fclaw2d_domain_t* domain);
+
+void
+    fc2d_clawpack50_setaux(fclaw2d_domain_t *domain,
+                           fclaw2d_patch_t *this_patch,
+                           int this_block_idx,
+                           int this_patch_idx);
+
+void
+    fc2d_clawpack50_set_capacity(fclaw2d_domain_t *domain,
+                                 fclaw2d_patch_t *this_patch,
+                                 int this_block_idx,
+                                 int this_patch_idx);
+
+void
+    fc2d_clawpack50_qinit(fclaw2d_domain_t *domain,
+                          fclaw2d_patch_t *this_patch,
+                          int this_block_idx,
+                          int this_patch_idx);
+
+void
+    fc2d_clawpack50_b4step2(fclaw2d_domain_t *domain,
+                            fclaw2d_patch_t *this_patch,
+                            int this_block_idx,
+                            int this_patch_idx,
+                            double t,
+                            double dt);
+
+void
+    fc2d_clawpack50_bc2(fclaw2d_domain_t *domain,
+                        fclaw2d_patch_t *this_patch,
+                        int this_block_idx,
+                        int this_patch_idx,
+                        double t,
+                        double dt,
+                        fclaw_bool intersects_bc[],
+                        fclaw_bool time_interp);
+
+void
+    fc2d_clawpack50_src2(fclaw2d_domain_t *domain,
+                         fclaw2d_patch_t *this_patch,
+                         int this_block_idx,
+                         int this_patch_idx,
+                         double t,
+                         double dt);
+
+
+/* A single step method that advances the solution a single step on a single grid
+   using a time step dt determined by the subcycle manager */
+double
+    fc2d_clawpack50_step2(fclaw2d_domain_t *domain,
+                          fclaw2d_patch_t *this_patch,
+                          int this_block_idx,
+                          int this_patch_idx,
+                          double t,
+                          double dt);
+
+/* Use this ro return only the right hand side of the clawpack algorithm */
+double
+    fc2d_clawpack50_step2_rhs(fclaw2d_domain_t *domain,
+                              fclaw2d_patch_t *this_patch,
+                              int this_block_idx,
+                              int this_patch_idx,
+                              double t,
+                              double *rhs);
+
+double
+fc2d_clawpack50_update(fclaw2d_domain_t *domain,
+                       fclaw2d_patch_t *this_patch,
+                       int this_block_idx,
+                       int this_patch_idx,
+                       double t,
+                       double dt);
+
+#ifdef __cplusplus
+#if 0
+{
+#endif
+}
+#endif
+
+
+#endif /* !FC2D_CLAWPACH50_H */
---
+++ src/solvers/fc2d_clawpack5.0/fc2d_clawpack50_options.c
@@ -0,0 +1,216 @@
+/*
+Copyright (c) 2012 Carsten Burstedde, Donna Calhoun
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "fc2d_clawpack50_options.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#if 0
+}
+#endif
+#endif
+
+typedef struct fc2d_clawpack50_package
+{
+  int is_registered;
+  fc2d_clawpack50_options_t clawopt;
+}
+fc2d_clawpack50_package_t; 
+
+static void*
+options_register (fclaw_app_t * app, void *package, sc_options_t * opt)
+{
+    fc2d_clawpack50_package_t *clawpkg;
+    fc2d_clawpack50_options_t *clawopt;
+
+    FCLAW_ASSERT (app != NULL);
+    FCLAW_ASSERT (package != NULL);
+
+    clawpkg = (fc2d_clawpack50_package_t*) package;
+    clawopt = &clawpkg->clawopt;
+
+    FCLAW_ASSERT (clawopt != NULL);
+
+    fclaw_options_add_int_array (opt, 0, "order", &clawopt->order_string,
+                               "2 2", &clawopt->order, 2,
+                               "[clawpack50] Normal and transverse orders [2 2]");
+
+    sc_options_add_int (opt, 0, "mcapa", &clawopt->mcapa, -1,
+                        "[clawpack50] Location of capacity function in aux array [-1]");
+
+    sc_options_add_int (opt, 0, "maux", &clawopt->maux, 0,
+                        "[clawpack50] Number of auxiliary variables [0]");
+
+    sc_options_add_bool (opt, 0, "src_term", &clawopt->src_term, 0,
+                         "[clawpack50] Source term option [F]");
+
+    sc_options_add_bool (opt, 0, "use_fwaves", &clawopt->use_fwaves, 0,
+                         "[clawpack50] Use fwaves flux-form [F]");
+
+
+    sc_options_add_int (opt, 0, "mwaves", &clawopt->mwaves, 1,
+                        "[clawpack50] Number of waves [1]");
+
+    fclaw_options_add_int_array (opt, 0, "mthlim", &clawopt->mthlim_string, NULL,
+                                 &clawopt->mthlim, clawopt->mwaves,
+                                 "[clawpack50] Waves limiters (one entry per wave; " \
+                                 "values 0-4) [NULL]");
+
+    clawpkg->is_registered = 1;
+    return NULL;
+}
+
+fclaw_exit_type_t
+fc2d_clawpack50_postprocess (fc2d_clawpack50_options_t * clawopt)
+{
+    fclaw_options_convert_int_array (clawopt->mthlim_string, &clawopt->mthlim,
+                                     clawopt->mwaves);
+    fclaw_options_convert_int_array (clawopt->order_string, &clawopt->order,
+                                     2);
+
+    return FCLAW_NOEXIT;
+}
+
+static fclaw_exit_type_t
+options_postprocess (fclaw_app_t * app, void *package, void *registered)
+{
+    fc2d_clawpack50_package_t *clawpkg;
+    fc2d_clawpack50_options_t *clawopt;
+
+    FCLAW_ASSERT (app != NULL);
+    FCLAW_ASSERT (package != NULL);
+
+    clawpkg = (fc2d_clawpack50_package_t*) package;
+    FCLAW_ASSERT (clawpkg->is_registered);
+
+    clawopt = &clawpkg->clawopt;
+    FCLAW_ASSERT (clawopt != NULL);
+
+    return fc2d_clawpack50_postprocess (clawopt);
+}
+
+fclaw_exit_type_t
+fc2d_clawpack50_check (fc2d_clawpack50_options_t * clawopt)
+{
+    clawopt->method[0] = 0;  /* Time stepping is controlled outside of clawpack */
+
+    clawopt->method[1] = clawopt->order[0];
+    clawopt->method[2] = clawopt->order[1];
+    clawopt->method[3] = 0;  /* No verbosity allowed in fortran subroutines */
+    clawopt->method[4] = clawopt->src_term;
+    clawopt->method[5] = clawopt->mcapa;
+    clawopt->method[6] = clawopt->maux;
+
+    if (clawopt->use_fwaves)
+    {
+        fclaw_global_essentialf("clawpack50 : fwaves not yet implemented\n");
+        return FCLAW_EXIT_QUIET;
+    }
+
+    if (clawopt->maux == 0 && clawopt->mcapa > 0)
+    {
+        fclaw_global_essentialf("clawpack50 : bad maux/mcapa combination\n");
+        return FCLAW_EXIT_ERROR;
+    }
+
+    /* Should also check mthbc, mthlim, etc. */
+
+    return FCLAW_NOEXIT;    /* Nothing can go wrong here! */
+}
+
+static fclaw_exit_type_t
+options_check (fclaw_app_t * app, void *package, void *registered)
+{
+    fc2d_clawpack50_package_t *clawpkg;
+    fc2d_clawpack50_options_t *clawopt;
+
+    FCLAW_ASSERT (app != NULL);
+    FCLAW_ASSERT (package != NULL);
+
+    clawpkg = (fc2d_clawpack50_package_t*) package;
+    FCLAW_ASSERT (clawpkg->is_registered);
+
+    clawopt = &clawpkg->clawopt;
+    FCLAW_ASSERT (clawopt != NULL);
+
+    return fc2d_clawpack50_check (clawopt);
+}
+
+void
+fc2d_clawpack50_reset (fc2d_clawpack50_options_t * clawopt)
+{
+    fclaw_options_destroy_array (clawopt->order);
+    fclaw_options_destroy_array (clawopt->mthlim);
+}
+
+static void
+options_destroy (fclaw_app_t * app, void *package, void *registered)
+{
+    fc2d_clawpack50_package_t *clawpkg;
+    fc2d_clawpack50_options_t *clawopt;
+
+    FCLAW_ASSERT (app != NULL);
+    FCLAW_ASSERT (package != NULL);
+
+    clawpkg = (fc2d_clawpack50_package_t*) package;
+    FCLAW_ASSERT (clawpkg->is_registered);
+
+    clawopt = &clawpkg->clawopt;
+    FCLAW_ASSERT (clawopt != NULL);
+
+    fc2d_clawpack50_reset (clawopt);
+    FCLAW_FREE (clawpkg);
+}
+
+static const fclaw_app_options_vtable_t clawpack50_options_vtable = {
+    options_register,
+    options_postprocess,
+    options_check,
+    options_destroy,
+};
+
+/* ----------------------------------------------------------
+   Public interface to clawpack options
+   ---------------------------------------------------------- */
+fc2d_clawpack50_options_t*  fc2d_clawpack50_options_register (fclaw_app_t * app,
+                                                              const char *configfile)
+{
+    fc2d_clawpack50_package_t *clawpkg;
+
+    FCLAW_ASSERT (app != NULL);
+
+    clawpkg = FCLAW_ALLOC (fc2d_clawpack50_package_t, 1);
+    fclaw_app_options_register (app, "clawpack50", configfile,
+                                &clawpack50_options_vtable, clawpkg);
+    return &clawpkg->clawopt;
+}
+
+#ifdef __cplusplus
+#if 0
+{
+#endif
+}
+#endif
