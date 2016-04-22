b0ed45ee8d796f1823f57fc2569ace6c80040ad3
donnaaboise <donna.calhoun@gmail.com>
2016-04-22 19:16:46 -0400

    (bowl) Renamed directory

---
+++ applications/geoclaw/bowl_radial/bowl_user.h
@@ -0,0 +1,105 @@
+/*
+Copyright (c) 2012 Carsten Burstedde, Donna Calhoun
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef BOWL_USER_H
+#define BOWL_USER_H
+
+#include <fclaw2d_forestclaw.h>
+#include <fc2d_geoclaw.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#if 0
+}
+#endif
+#endif
+
+#define BOWL_WRITE_HEADER FCLAW_F77_FUNC(bowl_write_header, BOWL_WRITE_HEADER)
+
+void BOWL_WRITE_HEADER(const int* iframe, const double* time,
+                        const int* mfields, const int* ngrids);
+
+#define BOWL_WRITE_FILE FCLAW_F77_FUNC(bowl_write_file, BOWL_WRITE_FILE)
+void BOWL_WRITE_FILE(const int* mx,        const int* my,
+                      const int* meqn,      const int* mbc,
+                      const double* xlower, const double* ylower,
+                      const double* dx,     const double* dy,
+                      double q[],           const int* iframe,
+                      const int* patch_num, const int* level,
+                      const int* blockno,   const int* mpirank);
+
+void bowl_link_solvers(fclaw2d_domain_t *domain);
+
+void bowl_problem_setup(fclaw2d_domain_t* domain);
+
+void bowl_patch_setup(fclaw2d_domain_t *domain,
+                       fclaw2d_patch_t *this_patch,
+                       int this_block_idx,
+                       int this_patch_idx);
+
+void bowl_patch_initialize(fclaw2d_domain_t *domain,
+                            fclaw2d_patch_t *this_patch,
+                            int this_block_idx,
+                            int this_patch_idx);
+
+void bowl_patch_physical_bc(fclaw2d_domain *domain,
+                             fclaw2d_patch_t *this_patch,
+                             int this_block_idx,
+                             int this_patch_idx,
+                             double t,
+                             double dt,
+                             fclaw_bool intersects_bc[],
+                             fclaw_bool time_interp);
+
+double bowl_patch_single_step_update(fclaw2d_domain_t *domain,
+                                      fclaw2d_patch_t *this_patch,
+                                      int this_block_idx,
+                                      int this_patch_idx,
+                                      double t,
+                                      double dt);
+
+int bowl_patch_tag4refinement(fclaw2d_domain_t *domain,
+                                      fclaw2d_patch_t *this_patch,
+                                      int this_block_idx, int this_patch_idx,
+                                      int initflag);
+
+int bowl_patch_tag4coarsening(fclaw2d_domain_t *domain,
+                                      fclaw2d_patch_t *this_patch,
+                                      int blockno,
+                                      int patchno);
+
+
+/* Mappings */
+fclaw2d_map_context_t* fclaw2d_map_new_nomap();
+
+#ifdef __cplusplus
+#if 0
+{
+#endif
+}
+#endif
+
+#endif
