33460896cb4354309b8637397f608a670cf43ea8
donnaaboise <donna.calhoun@gmail.com>
2016-04-22 16:02:32 -0400

    (swirl_mapped) Qinit file should include mappings

--- applications/advection/2d/swirl_mapped/user_5.0/qinit.f
+++ applications/advection/2d/swirl_mapped/user_5.0/qinit.f
@@ -17,15 +17,24 @@ c     #     0.1  otherwise
 
        integer i, j, mq
        double precision xi, yj, s
+       double precision xc,yc,zc,xp,yp,zp
+
+       integer*8 cont, get_context
+       integer blockno, fc2d_clawpack5_get_block
+
+       blockno = fc2d_clawpack5_get_block()
+       cont = get_context()
 
        do mq = 1,meqn
           do i = 1-mbc,mx+mbc
              xi = xlower + (i-0.5d0)*dx
              do j = 1-mbc,my+mbc
+                call fclaw2d_map_c2m(cont,
+     &                blockno,xi,yj,xp,yp,zp)
                 yj = ylower + (j-0.5d0)*dy
 
                 s = 0.5d0
-                if (xi .lt. s) then
+                if (xp .lt. s) then
                    q(mq,i,j) = 0.d0
                 else
                    q(mq,i,j) = 1.d0
