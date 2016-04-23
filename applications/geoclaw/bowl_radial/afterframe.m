f8f0f6a931fb8940c8861a8dac85a57c4484aaa7
YuHsuan Shih <yoyoshih13@gmail.com>
2016-04-23 16:49:11 -0400

    Add matlab files

---
+++ applications/geoclaw/bowl_radial/afterframe.m
@@ -0,0 +1,36 @@
+s = 1e-2;
+axis([-100 100 -100 100])
+daspect([1 1 1]);
+axis on;
+
+%yrbcolormap;
+showpatchborders(1:10);
+%caxis([0,1])
+%qlo = 0;
+%qhi = 1;
+% under_label = sprintf('0 - %7.1e',qlo-qmin);
+% over_label = sprintf('1 + %7.1e',qmax-qhi);
+% fprintf('%6s %12s\n','qmin',under_label);
+% fprintf('%6s %12s\n\n','qmax',over_label);
+
+
+% if (ShowUnderOverShoots)
+%     qlo = 0;
+%     qhi = 1;
+%     colorbar_underover(under_label,over_label);
+% end
+
+
+view(2);
+
+NoQuery = 0;
+prt = false;
+if (prt)
+  filename = framename(Frame,'bowl0000','png');
+  print('-dpng',filename);
+end
+
+shg
+
+clear afterframe;
+clear mapc2m;
