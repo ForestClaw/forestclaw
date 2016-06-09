bd650e7d6bbb083a478c1420ba7622cc0ad1206f
Donna Calhoun <donna.calhoun@gmail.com>
2016-06-09 15:13:11 +0100

    Update output routines to simplify writing the header

--- src/fortran_source2d/5/fclaw2d_output_ascii_fort2.f
+++ src/fortran_source2d/5/fclaw2d_output_ascii_fort2.f
@@ -1,5 +1,5 @@
-      subroutine fclaw2d_fort_write_header(iframe,time,
-     &      meqn,ngrids)
+      subroutine fclaw2d_fort_write_header(matname1,matname2,
+     &      time,meqn,ngrids)
       implicit none
 
       integer iframe,meqn,ngrids
@@ -9,17 +9,8 @@
       double precision time
       integer matunit1, matunit2, nstp,ipos,idigit
 
-      matname1 = 'fort.qxxxx'
-      matname2 = 'fort.txxxx'
       matunit1 = 10
       matunit2 = 15
-      nstp     = iframe
-      do ipos = 10, 7, -1
-         idigit = mod(nstp,10)
-         matname1(ipos:ipos) = char(ichar('0') + idigit)
-         matname2(ipos:ipos) = char(ichar('0') + idigit)
-         nstp = nstp / 10
-      enddo
 
       open(unit=matunit2,file=matname2)
       write(matunit2,1000) time,meqn,ngrids
@@ -34,74 +25,33 @@
 
       end
 
-      subroutine fclaw2d_output_new_qfile(iframe)
+
+      subroutine fclaw2d_fort_write_file(matname1,
+     &      mx,my,meqn,mbc, xlower,ylower, dx,dy,
+     &      q,patch_num,level,blockno,mpirank)
       implicit none
 
-      integer iframe
       character*10 matname1
-      integer matunit1, nstp,ipos,idigit
-
-      matname1 = 'fort.qxxxx'
-      matunit1 = 10
-      nstp     = iframe
-      do ipos = 10, 7, -1
-         idigit = mod(nstp,10)
-         matname1(ipos:ipos) = char(ichar('0') + idigit)
-         nstp = nstp / 10
-      enddo
-
-      open(unit=matunit1,file=matname1,status='replace')
-      close(matunit1)
-      end
-
-
-      subroutine fclaw2d_fort_write_file(mx,my,meqn,mbc,
-     &      xlower,ylower, dx,dy,q,iframe,patch_num,level,
-     &      blockno,mpirank)
-
-      implicit none
-
       integer meqn,mbc,mx,my
-      integer iframe,patch_num, level, blockno, mpirank
+      integer patch_num, level, blockno, mpirank
       double precision xlower, ylower,dx,dy
 
       double precision q(meqn,1-mbc:mx+mbc,1-mbc:my+mbc)
 
-      character*10 matname1
       integer matunit1
-      integer nstp,ipos,idigit
       integer i,j,mq
 
-      matname1 = 'fort.qxxxx'
       matunit1 = 10
-      nstp     = iframe
-      do ipos = 10, 7, -1
-         idigit = mod(nstp,10)
-         matname1(ipos:ipos) = char(ichar('0') + idigit)
-         nstp = nstp / 10
-      enddo
-
       open(matunit1,file=matname1,position='append');
 
-      write(matunit1,1001) patch_num, level, blockno, mpirank, mx, my
- 1001 format(i5,'                 grid_number',/,
-     &       i5,'                 AMR_level',/,
-     &       i5,'                 block_number',/,
-     &       i5,'                 mpi_rank',/,
-     &       i5,'                 mx',/,
-     &       i5,'                 my')
+      call fclaw2d_fort_write_grid_header(matunit1,
+     &      mx,my,xlower,ylower, dx,dy,patch_num,level,
+     &      blockno,mpirank)
 
-
-      write(matunit1,1002) xlower,ylower,dx,dy
- 1002 format(e24.16,'    xlow', /,
-     &       e24.16,'    ylow', /,
-     &       e24.16,'    dx', /,
-     &       e24.16,'    dy',/)
 
       if (meqn .gt. 5) then
-c        # Format statement 109 below will not work.
-         write(6,'(A,A)') 'Warning (out2.f) : meqn > 5; ',
-     &         'change format statement 109.'
+         write(6,'(A,A)') 'Warning (fclaw2d_fort_write_file.f) ',
+     &         ': meqn > 5;  change format statement 109.'
          stop
       endif
 
@@ -120,5 +70,34 @@ c      write(6,*) 'WARNING : (claw_out2.f ) Setting q to 0'
   120 format (5E26.16)
 
       close(matunit1)
+
+      end
+
+      subroutine fclaw2d_fort_write_grid_header(matunit1,
+     &      mx,my,xlower,ylower, dx,dy,patch_num,level,
+     &      blockno,mpirank)
+
+      implicit none
+
+      integer matunit1, mx, my
+      integer patch_num, level, blockno, mpirank
+      double precision xlower, ylower,dx,dy
+
+
+      write(matunit1,1001) patch_num, level, blockno, mpirank, mx, my
+ 1001 format(i5,'                 grid_number',/,
+     &       i5,'                 AMR_level',/,
+     &       i5,'                 block_number',/,
+     &       i5,'                 mpi_rank',/,
+     &       i5,'                 mx',/,
+     &       i5,'                 my')
+
+
+      write(matunit1,1002) xlower,ylower,dx,dy
+ 1002 format(e24.16,'    xlow', /,
+     &       e24.16,'    ylow', /,
+     &       e24.16,'    dx', /,
+     &       e24.16,'    dy',/)
+
 
       end
