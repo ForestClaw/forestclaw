e90e1a850309517e65d48e4a9b13e95f3687125f
Donna Calhoun <donna.calhoun@gmail.com>
2016-09-01 16:35:38 -0600

    (bowl_radial) Added plotting for scatter plot

--- applications/geoclaw/bowl_radial/afterframe.m
+++ applications/geoclaw/bowl_radial/afterframe.m
@@ -1,35 +1,54 @@
-s = 1e-2;
-axis([-100 100 -100 100])
-daspect([1 1 1]);
-axis on;
-
-showpatchborders(1:6);
-hidepatchborders(7)
-cv = linspace(qmin,qmax,20);
-% drawcontourlines(cv);
-set(gca,'zlim',[-8,0]);   % Need so that all patchborders show up
-
-rybcolormap;
-tol = 1e-1;
-caxis([-tol,tol])
-% caxis([qmin,qmax])
-daspect([20,20,1]);
-
-hold on;
+% bowl parameters
 zmin = 80;
 ep = 0.01;
-w = sqrt(zmin/ep);
-th = linspace(0,2*pi,500);
-plot(w*cos(th),w*sin(th),'k','linewidth',2);
-hold off;
 
-view(2);
-colorbar
+if (PlotType == 1)
+    s = 1e-2;
+    axis([-100 100 -100 100])
+    daspect([1 1 1]);
+    axis on;
+    
+    showpatchborders(1:6);
+    hidepatchborders(7)
+    cv = linspace(qmin,qmax,20);
+    % drawcontourlines(cv);
+    set(gca,'zlim',[-8,0]);   % Need so that all patchborders show up
+    
+    rybcolormap;
+    tol = 5;
+    caxis([-tol,tol])
+    % caxis([qmin,qmax])
+    daspect([20,20,1]);
+    
+    hold on;
+    zmin = 80;
+    ep = 0.01;
+    w = sqrt(zmin/ep);
+    th = linspace(0,2*pi,500);
+    plot(w*cos(th),w*sin(th),'k','linewidth',2);
+    hold off;
+    view(2);
+    colorbar
+else
+    hold on;
+    r = linspace(0,100,500);
+    r2 = r.^2;
+    b = ep*r.^2 - zmin;
+    b(b >= 0) = 0;
+    plot(r,b,'k','linewidth',2);  
+    xlim([0,100]);
+    ylim([-zmin,20]);    
+    daspect([1 1 1])    
+    [h_amr, labels_amr] = getlegendinfo;
+    h = legend(h_amr,labels_amr,'location','southeast');
+    hold off;
+end
+
 NoQuery = 0;
 prt = false;
 if (prt)
-  filename = framename(Frame,'bowl0000','png');
-  print('-dpng',filename);
+    filename = framename(Frame,'bowl0000','png');
+    print('-dpng',filename);
 end
 
 shg
