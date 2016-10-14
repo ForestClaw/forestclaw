aeb86b48bc6125a31a5bdb66846a4a92a8f7e961
Donna Calhoun <donna.calhoun@gmail.com>
2016-10-13 20:53:22 -0600

    (latlong) New latlong application, with support for both 4.6 and 5.0 clawpack

---
+++ applications/advection/2d/latlong/latlong_user.h
@@ -0,0 +1,83 @@
+/*
+Copyright (c) 2012 Carsten Burstedde, Donna Calhoun
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef LATLONG_USER_H
+#define LATLONG_USER_H
+
+#include <fclaw2d_domain.h>
+#include "../all/clawpack_user.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#if 0
+}
+#endif
+#endif
+
+typedef struct user_options
+{
+    int example;
+    int claw_version;
+
+    const char *latitude_string;
+    double *latitude;
+
+    const char *longitude_string;
+    double *longitude;
+
+    int is_registered;
+}
+user_options_t;
+
+void latlong_link_solvers(fclaw2d_domain_t *domain);
+
+void latlong_problem_setup(fclaw2d_domain_t *domain);
+
+void latlong_patch_setup(fclaw2d_domain_t *domain,
+                         fclaw2d_patch_t *this_patch,
+                         int this_block_idx,
+                         int this_patch_idx);
+
+
+const user_options_t* latlong_user_get_options(fclaw2d_domain_t* domain);
+
+fclaw2d_map_context_t *
+    fclaw2d_map_new_latlong (fclaw2d_map_context_t* brick,
+                             const double scale[],
+                             const double shift[],
+                             const double rotate[],
+                             const double lat[],
+                             const double longitude[],
+                             const int a, const int b);
+
+#ifdef __cplusplus
+#if 0
+{
+#endif
+}
+#endif
+
+#endif /* LATLONG_USER_H */
