43a0111a2e2cdcb745edca5661c2396f12420df7
Donna Calhoun <donna.calhoun@gmail.com>
2016-10-21 12:56:26 +0200

    (filament) Matlab/ini files

--- applications/advection/2d/filament/mapc2m.m
+++ applications/advection/2d/filament/mapc2m.m
@@ -1,7 +1,7 @@
 function [xp,yp,zp] = mapc2m(xc,yc)
 
 map = 'nomap';
-% map = 'cart';
+% map = 'cart';   % brick
 % map = 'fivepatch';
 
 % This domain should be in [0,2],[0,2]
@@ -16,54 +16,16 @@ switch map
         yp = yc;
     case 'cart'
         % (xc,yc) in [0,1]x[0,1]
-        s = 0.00;
+        s = 0.005;
         [xc1,yc1,~] = mapc2m_brick(xc,yc,s);
         [xp,yp,~] = mapc2m_cart(xc1,yc1);
                 
         xp = xp + shift(1);
         yp = yp + shift(2);
-    case 'pillowdisk'
-        [xp,yp,~] = mapc2m_pillowdisk(xc,yc);
-        xp = xp + shift(1);
-        yp = yp + shift(2);
-        % (xp,yp) in [-1,1]x[-1,1]
-    case 'squareddisk'
-        [xp,yp,~] = mapc2m_squareddisk(xc,yc);
-        s = 0.0;
-        b = getblocknumber();
-        switch b
-            case 0
-                yp = yp - s;
-            case 1
-                xp = xp - s;
-            case 3
-                xp = xp + s;
-            case 4
-                yp = yp + s;
-        end
-        xp = xp + shift(1);
-        yp = yp + shift(2);
-    case 'pillowdisk5'
-        [xp,yp,~] = mapc2m_pillowdisk5(xc,yc);
-        s = 0.0;
-        b = getblocknumber();
-        switch b
-            case 0
-                yp = yp - s;
-            case 1
-                xp = xp - s;
-            case 3
-                xp = xp + s;
-            case 4
-                yp = yp + s;
-        end
-        xp = xp + shift(1);
-        yp = yp + shift(2);
-        
     case 'fivepatch'
         [xp,yp,~] = mapc2m_fivepatch(xc,yc);
         b = getblocknumber();
-        s = 0.0;
+        s = 0.005;
         switch b
             case 0
                 yp = yp - s;
