5fd039f0c537808df0736bbbfdca97500007cf02
Donna Calhoun <donna.calhoun@gmail.com>
2016-10-29 21:53:00 -0600

    (gaussian) Upgrade applications;  add support for clawpack 5.0

--- applications/NCAR_transport/gaussian/afterframe.m
+++ applications/NCAR_transport/gaussian/afterframe.m
@@ -2,38 +2,24 @@ axis image;
 daspect([1 1 1]);
 axis off;
 
-plot_choices = {'partition','solution','mesh'};
-plot_type = plot_choices{2};
 yrbcolormap;
-qlo = 0.1;
-qhi = 1.1;
-under_label = sprintf('%3.1f - %7.1e',qlo,qlo-qmin);
-over_label = sprintf('%3.1f + %7.1e',qhi,qmax-qhi);
-fprintf('%6s %12s\n','qmin',under_label);
-fprintf('%6s %12s\n\n','qmax',over_label);
-
-if (ShowUnderOverShoots)
-    colorbar_underover(under_label,over_label);
-end;
-
-% showgridlines(1:4);
-showpatchborders(1:8);
+showpatchborders;
 setpatchborderprops(1:8,'linewidth',1);
-view([-51.5,0]);
+hidepatchborders(7);
+% hidepatchborders;
+view([65.5,12]);
+
+% showgridlines(3);
 
 setviews;
-% set(gcf,'visible','off');
-
 NoQuery = 0;
 prt = false;
 if (prt)
-  MaxFrames = 50;
-  filename = framename(Frame,'mesh000','png');
+  MaxFrames = 41;
+  filename = framename(Frame,'ccb0000','png');
   fprintf('Printing file %s\n',filename);
-  print('-dpng',filename);
+  print('-dpng','-r800',filename);
 end
-
-shg
 
 clear afterframe;
 clear mapc2m;
