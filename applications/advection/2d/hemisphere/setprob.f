8cf7583d3e77f2df37498bd361f03452d4eccc74
Donna Calhoun <donna.calhoun@gmail.com>
2016-10-30 11:34:36 -0600

    (hemisphere) Make sure common block value is set

--- applications/advection/2d/hemisphere/setprob.f
+++ applications/advection/2d/hemisphere/setprob.f
@@ -4,6 +4,11 @@
       double precision pi
       common /compi/ pi
 
+      double precision revs_per_second
+      common /spherecomm/ revs_per_second
+
       pi = 4.d0*atan(1.d0)
+
+      revs_per_second = 0.5d0
 
       end
