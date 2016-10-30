606220446400353818724a9ed9709d968213e1d9
Donna Calhoun <donna.calhoun@gmail.com>
2016-10-30 11:24:03 -0600

    (hemisphere) Minor fix to default settings

--- applications/advection/2d/hemisphere/mapc2m.m
+++ applications/advection/2d/hemisphere/mapc2m.m
@@ -2,14 +2,14 @@ function [xp,yp,zp] = mapc2m(xc,yc)
 
 global notpillowsphere;
 
-% map = 'pillow';
 map = 'pillowsphere5';
+% map = 'pillow';
 
 switch map
     case 'pillow'
         notpillowsphere = false;
         [xp,yp,zp] = mapc2m_pillowsphere(xc,yc);
-        
+
     case 'pillowsphere5'
         notpillowsphere = true;
         [xc1,yc1,zc1] = mapc2m_fivepatch(0.5,0.5);
@@ -17,8 +17,8 @@ switch map
         yc1 = (yc1 + 1)/2;
         [xp,yp,zp]= mapc2m_pillowsphere(xc1,yc1);
         v = [xp,yp,zp];
-        
-        [xp,yp,zp] = mapc2m_fivepatch(xc,yc);  
+
+        [xp,yp,zp] = mapc2m_fivepatch(xc,yc);
         xc = (xp + 1)/2;
         yc = (yp + 1)/2;
         [xp,yp,zp] = mapc2m_pillowsphere(xc,yc);
