27a680a4735cca5bb3997643673feeeab3f5773f
Donna Calhoun <donna.calhoun@gmail.com>
2016-11-18 06:27:32 -0700

    (replicated) Updated qinit for 5.0 version

--- applications/advection/2d/replicated/user_5.0/qinit.f90
+++ applications/advection/2d/replicated/user_5.0/qinit.f90
@@ -7,15 +7,15 @@ SUBROUTINE clawpack5_qinit(meqn,mbc,mx,my,xlower,ylower,dx,dy,q,maux,aux)
   DOUBLE PRECISION aux(maux,1-mbc:mx+mbc, 1-mbc:my+mbc)
 
   INTEGER i,j
-  INTEGER blockno, fc2d_clawpack46_get_block
+  INTEGER blockno, fc2d_clawpack5_get_block
   DOUBLE PRECISION xlow, ylow, w, xc, yc
 
-  blockno = fc2d_clawpack46_get_block()
+  blockno = fc2d_clawpack5_get_block()
 
   DO j = 1-mbc,my+mbc
      DO i = 1-mbc,mx+mbc
-        xlow = xlower + (i-1)*dx
-        ylow = ylower + (j-1)*dy
+        xlow = MODULO(xlower,1.d0) + (i-1)*dx
+        ylow = MODULO(ylower,1.d0) + (j-1)*dy
         CALL cellave2(blockno,xlow,ylow,dx,dy,w)
         q(1,i,j) = w
      ENDDO
