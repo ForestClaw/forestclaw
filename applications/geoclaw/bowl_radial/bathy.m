342d6c54a78c126cf6074d8a83fbc5c59b72c410
Donna Calhoun <donna.calhoun@gmail.com>
2016-11-20 23:51:17 -0700

    (bowl/geoclaw) Update options/Matlab file

--- applications/geoclaw/bowl_radial/bathy.m
+++ applications/geoclaw/bowl_radial/bathy.m
@@ -1,12 +1,8 @@
 function eta = bathy(data)
 % read q data:
-mbathy = 4;
-mheight = 1;
 h = data(:,1);
-b = data(:,4);
-eta = zeros(size(h));
-m = b > 0;
-eta(b>0) = nan;
+eta = data(:,4);
+% eta = zeros(size(h));
+eta(h <= 0) = nan;
 
-eta(b<=0) = h(b<=0) + b(b<=0);
 end
