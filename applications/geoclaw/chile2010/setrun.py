8e00fd5aafa3143a98af8d96826d8464dab419a4
YuHsuan Shih <yoyoshih13@gmail.com>
2017-01-04 17:52:23 -0500

    change the maximum level and refine_ration in setrun.py to ourput correct amr.data used for plotting

--- applications/geoclaw/chile2010/setrun.py
+++ applications/geoclaw/chile2010/setrun.py
@@ -280,12 +280,12 @@ def setrun(claw_pkg='geoclaw'):
     amrdata = rundata.amrdata
 
     # max number of refinement levels:
-    amrdata.amr_levels_max = 3
+    amrdata.amr_levels_max = 5
 
     # List of refinement ratios at each level (length at least mxnest-1)
-    amrdata.refinement_ratios_x = [2,6]
-    amrdata.refinement_ratios_y = [2,6]
-    amrdata.refinement_ratios_t = [2,6]
+    amrdata.refinement_ratios_x = [2,6,1,1]
+    amrdata.refinement_ratios_y = [2,6,1,1]
+    amrdata.refinement_ratios_t = [2,6,1,1]
 
 
     # Specify type of each aux variable in amrdata.auxtype.
