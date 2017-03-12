558a3023dc592f8f543b75fb0636bf5249d42820
Donna Calhoun <donna.calhoun@gmail.com>
2017-03-12 12:13:13 -0600

    (filament) No longer necessary to call init routines in vtable setup

--- applications/advection/2d/filament/filament_user.cpp
+++ applications/advection/2d/filament/filament_user.cpp
@@ -28,13 +28,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <fclaw2d_forestclaw.h>
 #include <fclaw2d_clawpatch.h>
 
-#if 0
-static fclaw2d_vtable_t fclaw2d_vt;
-
-static fc2d_clawpack46_vtable_t classic_claw46;
-static fc2d_clawpack5_vtable_t classic_claw5;
-#endif 
-
 void filament_link_solvers(fclaw2d_domain_t *domain)
 {
     const user_options_t* user = filament_user_get_options(domain);
@@ -44,9 +37,6 @@ void filament_link_solvers(fclaw2d_domain_t *domain)
 
     if (user->claw_version == 4)
     {
-        // fc2d_clawpack46_set_vtable_defaults(&fclaw2d_vt, &classic_claw46);
-        // fc2d_clawpack46_set_vtable_defaults();
-        
         fc2d_clawpack46_vt()->setprob   = &SETPROB;
         fc2d_clawpack46_vt()->qinit     = &CLAWPACK46_QINIT;
         if (gparms->manifold)
@@ -70,13 +60,9 @@ void filament_link_solvers(fclaw2d_domain_t *domain)
             fc2d_clawpack46_vt()->rpt2      = &CLAWPACK46_RPT2ADV;
         }
 
-        // fc2d_clawpack46_set_vtable(classic_claw46);
     }
     else if (user->claw_version == 5)
     {
-        // fc2d_clawpack5_set_vtable_defaults(&fclaw2d_vt, &classic_claw5);
-        // fc2d_clawpack5_set_vtable_defaults();
-
         fc2d_clawpack5_vt()->setprob   = &SETPROB;
         fc2d_clawpack5_vt()->qinit     = &CLAWPACK5_QINIT;
         if (gparms->manifold)
@@ -98,10 +84,7 @@ void filament_link_solvers(fclaw2d_domain_t *domain)
             fc2d_clawpack5_vt()->rpn2      = &CLAWPACK5_RPN2ADV;
             fc2d_clawpack5_vt()->rpt2      = &CLAWPACK5_RPT2ADV;
         }
-
-        // fc2d_clawpack5_set_vtable(classic_claw5);
     }
-    fclaw2d_set_vtable();
 }
 
 void filament_patch_setup_manifold(fclaw2d_domain_t *domain,
