fe44ea9a7ff3c513e7fb1e41bab9e893c2070bf1
Donna Calhoun <donna.calhoun@gmail.com>
2017-04-11 15:16:40 -0600

    Added 2d arrays to snippet

--- snippets/tst_f90_alloc/array.f90
+++ snippets/tst_f90_alloc/array.f90
@@ -1,64 +1,89 @@
 MODULE test_mod
     IMPLICIT NONE
 
-    DOUBLE PRECISION, POINTER :: x_comp_sp(:) 
-    INTEGER :: nx_submet = 5
+    DOUBLE PRECISION, POINTER :: x_comp_sp(:)     
+    DOUBLE PRECISION, POINTER :: concen(:,:) 
+
+    INTEGER :: nx = 5
 
 END MODULE test_mod
 
-SUBROUTINE store_ptrs(fc_ptr)
+SUBROUTINE store_ptrs(fc_x_comp_sp_ptr,fc_concen_ptr)
     use iso_c_binding
     use test_mod
     IMPLICIT NONE
 
-    TYPE(c_ptr) :: fc_ptr
+    TYPE(c_ptr) :: fc_x_comp_sp_ptr, fc_concen_ptr
 
-    fc_ptr = c_loc(x_comp_sp)
+    fc_x_comp_sp_ptr = c_loc(x_comp_sp)
+    fc_concen_ptr = c_loc(concen)
 end
 
-SUBROUTINE copy_ptrs2mod(fc_ptr)
+SUBROUTINE copy_ptrs2mod(fc_x_comp_sp_ptr,fc_concen_ptr)
     use iso_c_binding
     use test_mod
     IMPLICIT NONE
 
-    TYPE(c_ptr) :: fc_ptr
-    INTEGER :: i
+    TYPE(c_ptr) :: fc_x_comp_sp_ptr, fc_concen_ptr
+    double precision, pointer :: xp(:)   !! Needed only for 2d and higher dim arrays
+    INTEGER :: i,j
 
-    CALL c_f_pointer(fc_ptr,x_comp_sp,[1])
+    CALL c_f_pointer(fc_x_comp_sp_ptr,x_comp_sp,[1])
 
-    DO i = 1,nx_submet
-        WRITE(6,*) x_comp_sp(i)
-    ENDDO 
+    CALL c_f_pointer(fc_concen_ptr,xp,[1])
+    concen(1:nx,1:nx) => xp
+
+    write(6,*) 'concentration'
+    DO i = 1,nx
+        WRITE(6,'(12F8.0)') (concen(i,j),j=1,nx)
+    END DO 
+    write(6,*) ' '
+
+
+    write(6,*) 'x_comp_sp'
+    do i = 1,nx
+        write(6,'(F12.0)') x_comp_sp(i)
+    end do
+    write(6,*) ' '
 
 end
 
-subroutine deallocate_arrays(fc_ptr)
+subroutine deallocate_arrays(fc_x_comp_sp_ptr,fc_concen_ptr)
     use iso_c_binding
     use test_mod
     implicit none
 
-    type(c_ptr) :: fc_ptr
+    type(c_ptr) :: fc_x_comp_sp_ptr, fc_concen_ptr
+    double precision, pointer :: xp(:)
 
-    CALL c_f_pointer(fc_ptr,x_comp_sp,[1])
+    CALL c_f_pointer(fc_concen_ptr,xp,[1])
+    concen(1:nx,1:nx) => xp
+    DEALLOCATE(concen)  !! May also be okay to just de-allocate xp
 
-    DEALLOCATE(x_comp_sp)
+    CALL c_f_pointer(fc_x_comp_sp_ptr,x_comp_sp,[1])
+    DEALLOCATE(x_comp_sp)  
 
 end
 
 
-SUBROUTINE use_array(value)
+SUBROUTINE use_array(value1,value2)
     use iso_c_binding
     USE test_mod
     IMPLICIT NONE
 
-    DOUBLE PRECISION value
-    INTEGER i
+    DOUBLE PRECISION value1, value2
+    INTEGER i,j
 
-    ALLOCATE(x_comp_sp(nx_submet))
+    ALLOCATE(x_comp_sp(nx), concen(nx,nx))
 
-    do i = 1,nx_submet
-        x_comp_sp(i) = value
-    enddo 
+    do i = 1,nx
+        do j = 1,nx
+            concen(i,j) = value1
+        end do
+        x_comp_sp(i) = value2
+    end do 
+
+
 
 end
 
--- snippets/tst_f90_alloc/prog.c
+++ snippets/tst_f90_alloc/prog.c
@@ -1,37 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void use_array_(double *v);
-void store_ptrs_(double ** ptr);
-void copy_ptrs2mod_(double ** ptr);
-void deallocate_arrays_(double** ptr);
+void use_array_(double *v1, double *v2);
+void store_ptrs_(double ** ptr1, double** ptr2);
+void copy_ptrs2mod_(double ** ptr1, double **ptr2);
+void deallocate_arrays_(double** ptr1, double ** ptr2);
+
+typedef struct fc_patch
+{
+    double * x_comp_sp;
+    double * concen;
+} fc_patch_t;
+
 
 int main()
 {
-    double *fc_x_comp_sp_1;  // patch 1
-    double *fc_x_comp_sp_2;  // patch 2
-    double value;
+    double *fc_x_comp_sp_1, *fc_concen_1;  // patch 1
+    double *fc_x_comp_sp_2, *fc_concen_2;  // patch 2
+    fc_patch_t p1, p2;
+    double value1, value2;
 
-    value = 1.0;
-    use_array_(&value);    
-    store_ptrs_(&fc_x_comp_sp_1);
+    value1 = 1.0;
+    value2 = 5.0;
+    use_array_(&value1,&value2);    
+    store_ptrs_(&p1.x_comp_sp, &p1.concen);
 
-    
-    value = 5.0;
-    use_array_(&value);
-    store_ptrs_(&fc_x_comp_sp_2);
+    value1 = 2.0;
+    value2 = 11.0;
+    use_array_(&value1, &value2);
+    store_ptrs_(&p2.x_comp_sp, &p2.concen);
 
+    printf("Print values of x_comp_sp after allocation and usage\n");
     for(int i = 0; i < 5; i++)
     {
-    	printf("%f %f\n",fc_x_comp_sp_1[i],fc_x_comp_sp_2[i]);
-        fc_x_comp_sp_1[i] = 47;
+    	printf("%f %f\n",p1.x_comp_sp[i], p2.x_comp_sp[i]);
+        p1.x_comp_sp[i] = 47;
     }
 
-    copy_ptrs2mod_(&fc_x_comp_sp_1);
-    copy_ptrs2mod_(&fc_x_comp_sp_2);
+    printf("\n");
+    printf("Copying ptrs back to module (after x_comp_sp_1 set to 47)\n");
+    copy_ptrs2mod_(&p1.x_comp_sp, &p1.concen);
+    copy_ptrs2mod_(&p2.x_comp_sp, &p2.concen);
 
-    deallocate_arrays_(&fc_x_comp_sp_1);
-    deallocate_arrays_(&fc_x_comp_sp_2);
+    printf("\n");
+    printf("Deallocating arrays\n\n");
+    deallocate_arrays_(&p1.x_comp_sp, &p1.concen);
+    deallocate_arrays_(&p2.x_comp_sp, &p2.concen);
 
     for(int i = 0; i < 5; i++)
     { 
