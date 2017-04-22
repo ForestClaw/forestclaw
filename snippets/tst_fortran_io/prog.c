df283f180a4ffe21a0be89ac59a96a27e91ea919
Donna Calhoun <donna.calhoun@gmail.com>
2017-04-22 09:39:19 -0600

    (snippet) More work on MPI I/O

--- snippets/tst_fortran_io/prog.c
+++ snippets/tst_fortran_io/prog.c
@@ -1,6 +1,7 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
+#define  MASTER     0
 
 typedef enum
 {
@@ -17,21 +18,22 @@ void print_hello_(int* mpirank);
 
 int main (int argc, char *argv[])
 {
-    int  numprocs, mpirank, len;
+    int  numtasks, mpirank, len;
     char hostname[MPI_MAX_PROCESSOR_NAME];
     output_t verb;
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD,&mpirank);
     MPI_Get_processor_name(hostname, &len);
 
     verb = DEBUG;
     print_hello_(&mpirank);
 
+    printf ("Hello from task %d on %s!\n", mpirank, hostname);
     if (mpirank == 0)
     {
-        printf("Number of MPI tasks is: %d\n",numprocs);
+        printf("Number of MPI tasks is: %d\n",numtasks);
     }
     MPI_Finalize();
 }
