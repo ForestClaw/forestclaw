d60821f74560e2cf8bca2e9243c42b58fa0cb397
Donna Calhoun <donna.calhoun@gmail.com>
2017-06-29 10:21:14 +0100

    (geoclaw/flat) Make two waves more obvious

--- applications/geoclaw/flat_bathymetry/maketopo.py
+++ applications/geoclaw/flat_bathymetry/maketopo.py
@@ -37,7 +37,7 @@ def topo(x,y):
     flatbathy
     """
     # value of z at origin:  Try zmin = 80 for shoreline or 250 for no shore
-    z = -100*ones(x.shape)
+    z = ones(x.shape)*-100.e0
     return z
 
 
@@ -46,10 +46,11 @@ def qinit(x,y):
     Gaussian hump:
     """
     from numpy import where
-    ze = -((x + 50)**2)/100.
-    z = 10*exp(ze)
-
-    return where(z > 1e-3,z,0)
+    ze = -((x+0e0)**2)/10.
+    z = 1.0 + 0.01*exp(ze)
+    # ze = -((x+0e0)**2 + (y+0e0)**2)/10.
+    # z = where(ze>-10., 40.e0*exp(ze), 0.)
+    return z
 
 if __name__=='__main__':
     maketopo()
