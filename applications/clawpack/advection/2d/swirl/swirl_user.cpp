4475f4ba54c001a6f9d921f1abb9b79e1bdb09bc
Donna Calhoun <donna.calhoun@gmail.com>
2017-07-04 15:01:30 +0100

    (swirl) Set setaux in swirl_user (wasn't noticed before, because we relied on b4step2.f

--- applications/clawpack/advection/2d/swirl/swirl_user.cpp
+++ applications/clawpack/advection/2d/swirl/swirl_user.cpp
@@ -37,12 +37,16 @@ void swirl_link_solvers(fclaw2d_global_t *glob)
 {
     fclaw2d_vtable_t *vt = fclaw2d_vt();
 
+
     vt->problem_setup = &swirl_problem_setup;  /* Version-independent */
+    fclaw2d_patch_vtable_t *patch_vt = fclaw2d_patch_vt();
 
     const user_options_t* user = swirl_get_options(glob);
     if (user->claw_version == 4)
     {
-        fc2d_clawpack46_vtable_t *clawpack46_vt = fc2d_clawpack46_vt();
+        fc2d_clawpack46_vtable_t *clawpack46_vt = fc2d_clawpack46_vt();        
+        patch_vt->setup = fc2d_clawpack46_setaux;
+
         clawpack46_vt->qinit     = &CLAWPACK46_QINIT;
         clawpack46_vt->setaux    = &CLAWPACK46_SETAUX;
         clawpack46_vt->rpn2      = &CLAWPACK46_RPN2ADV;
@@ -52,6 +56,8 @@ void swirl_link_solvers(fclaw2d_global_t *glob)
     else if (user->claw_version == 5)
     {
         fc2d_clawpack5_vtable_t *clawpack5_vt = fc2d_clawpack5_vt();
+        patch_vt->setup = fc2d_clawpack5_setaux;
+
         clawpack5_vt->qinit     = &CLAWPACK5_QINIT;
         clawpack5_vt->setaux    = &CLAWPACK5_SETAUX;
         clawpack5_vt->b4step2   = &CLAWPACK5_B4STEP2;
