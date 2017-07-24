2d02a9b0b50c6bec3909bd4b50784e88b748e003
Donna Calhoun <donna.calhoun@gmail.com>
2017-07-24 15:23:33 +0100

    (clawpack/advection/1d reference solution) Update 1d reference solution

--- applications/clawpack/advection/2d/swirl_cons/1dadv/setaux.f
+++ applications/clawpack/advection/2d/swirl_cons/1dadv/setaux.f
@@ -3,7 +3,7 @@
       implicit double precision (a-h,o-z)
       dimension aux(1-mbc:maxmx+mbc, maux)
 
-      double precision xe,xc, pi, dx1, dx2
+      double precision xe,xc, pi, dx1, dx2, a
       integer i, example
 
       common /compi/ pi
@@ -26,13 +26,21 @@ c             # Velocity should have a delta function at x=0.5
                   aux(i,2) = 0            
               endif
           elseif (example .eq. 2) then
-              aux(i,1) = cos(2*pi*xe) + 1.d0
+              aux(i,1) = cos(2*pi*xe)
               aux(i,2) = -2*pi*sin(2*pi*xc)
           elseif (example .eq. 3) then
               aux(i,1) = 0.1d0*sin(2*pi*xe)*sin(16*pi*xe)
               dx1 =  2*pi*cos(2*pi*xc)*sin(16*pi*xc)
               dx2 = 16*pi*sin(2*pi*xc)*cos(16*pi*xc)
               aux(i,2) = 0.1*(dx1 + dx2)
+          elseif (example .eq. 4) then
+              a = 0.01d0
+              aux(i,1) = tanh((xe-0.5d0)/a)
+              aux(i,2) = (1-tanh((xc-0.5d0)/a)**2)/a
+          elseif (example .eq. 5) then
+              a = 0.1d0
+              aux(i,1) = -tanh((xe-0.5d0)/a)
+              aux(i,2) = -(1-tanh((xc-0.5d0)/a)**2)/a
           endif
       enddo
 
