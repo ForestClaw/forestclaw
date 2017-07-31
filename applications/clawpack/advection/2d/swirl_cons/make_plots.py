70385a773f0f7b640ebe14eceac33646ffdc8cfc
Donna Calhoun <donna.calhoun@gmail.com>
2017-07-31 15:53:21 +0100

    (clawpack/advection (cons)) More work on conservative advection

--- applications/clawpack/advection/2d/swirl_cons/make_plots.py
+++ applications/clawpack/advection/2d/swirl_cons/make_plots.py
@@ -2,7 +2,7 @@
 """ 
 Plot swirl using Clawpack's visclaw graphics.  This file can be run as ; 
 
-    % python plot_swirl.py
+    % python make_plots.py
 
 To learn more about visclaw graphics, see www.clawpack.org
     
@@ -27,6 +27,8 @@ def setplot(plotdata):
     plotdata.clearfigures()  # clear any old figures,axes,items data
     plotdata.format = 'forestclaw'
     
+    example = 1
+
     # ------------------------------------------------------------
     # Figure for pcolor plot
     # ------------------------------------------------------------
@@ -45,124 +47,12 @@ def setplot(plotdata):
     plotitem.plot_var = 0
     plotitem.imshow_cmap = colormaps.yellow_red_blue
     plotitem.imshow_cmin = 0.0
-    plotitem.imshow_cmax = 1.0
+    plotitem.imshow_cmax = 16.0
     plotitem.add_colorbar = True
-    plotitem.amr_celledges_show = [True, True, False]
+    plotitem.amr_celledges_show = [False, False, False]
     plotitem.amr_patchedges_show = [True, True]
     plotitem.show = True       # show on plot?
     
-    # ------------------------------------------------------------
-    # Figure for contour plot
-    # ------------------------------------------------------------
-    plotfigure = plotdata.new_plotfigure(name='contour', figno=1)
-
-    # Set up for axes in this figure:
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.xlimits = 'auto'
-    plotaxes.ylimits = 'auto'
-    plotaxes.title = 'q[0]'
-    plotaxes.scaled = True
-
-    # Set up for item on these axes:
-    plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
-    plotitem.plot_var = 0
-    plotitem.contour_nlevels = 20
-    plotitem.contour_min = 0.01
-    plotitem.contour_max = 0.99
-    plotitem.amr_contour_colors = ['b','k','r']
-    plotitem.show = True       # show on plot?
-
-    # ------------------------------------------------------------
-    # Figure for tikz plots (use KML plotting for now)
-    # ------------------------------------------------------------
-
-    # To create publication quality graphics with AMR mesh lines : 
-    # Run code with following options, set in fclaw_options.ini
-    #   -- Set --tikz-out=T
-    #   -- Set --tikz-figsize
-    #   -- set --tikz-plot-prefix and --tikz-plot-suffix
-    #    
-    # Running the code will create a series of files tikz_XXXX.tex, which will include 
-    # graphics files <prefix>_XXXX.<suffix>.  
-    #
-    # Run file plot_swirl.py to create <plotdir>/_GoogleEearth.kmz
-    # To extract frame N, use 'unzip' (or something equivalent)
-    #
-    # Example : 
-    # In fclaw_options.ini
-    #     [Options]
-    #        --tikz-out = T
-    #        --tikz-figsize = 4 4    # in inches
-    #        --tikz-plot-prefix = 'plot'
-    #        --tikz-plot-suffix = 'png'
-    #
-    # Running the code will produce files 'tikz_XXXX.tex', which will 
-    # include a file plot_XXXX.png
-    #
-    # Run plot_swirl.py to create <plotdir>/_GoogleEarth.kmz.  Extract frames from 
-    # this file 
-    # 
-    # Example : Extract frame0004fig2.png
-    # 
-    #   % unzip _plots/_GoogleEarth.kmz fig2/frame0004fig2/frame0004fig2.png
-    #   % cp fig2/frame0004fig2/frame0004fig2.png plot_0004.png
-    #   % pdflatex tikz_0004.tex
-    # 
-    # View tikz_0004.pdf in appropriate PDF viewer. 
-    # ------------------------------------------------------------
-    plotfigure = plotdata.new_plotfigure(name='swirl (tikz)', figno=2)
-    plotfigure.use_for_kml = True
-    plotfigure.kml_xlimits = [0,1]
-    plotfigure.kml_ylimits = [0,1]
-
-    mx = 8
-    maxlevel = 6
-    resolution = mx*2**maxlevel
-    figsize = [4.0,4.0]
-    dpi = resolution/figsize[0]
-
-    plotfigure.kml_figsize = figsize  
-    plotfigure.kml_dpi = dpi
-
-    # Color axis : transparency below 0.1*(cmax-cmin)
-    cmin = 0
-    cmax = 1
-    cmap = colormaps.yellow_red_blue  # transparent --> light blue --> dark blue
-
-    # Water
-    plotaxes = plotfigure.new_plotaxes('tikz')
-    plotaxes.xlimits = [0,1]
-    plotaxes.ylimits = [0,1]
-    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
-    plotitem.plot_var = 0   # Plot height field h.    
-    plotitem.pcolor_cmap = cmap
-    plotitem.pcolor_cmin = cmin
-    plotitem.pcolor_cmax = cmax
-
-    def kml_colorbar(filename):
-        geoplot.kml_build_colorbar(filename,cmap,cmin,cmax)
-
-    plotfigure.kml_colorbar = kml_colorbar
-
-
-    #-----------------------------------------
-    # Figures for gauges
-    #-----------------------------------------
-    plotfigure = plotdata.new_plotfigure(name='q', figno=300, \
-                    type='each_gauge')
-    plotfigure.clf_each_gauge = True
-
-    # Set up for axes in this figure:
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.xlimits = 'auto'
-    plotaxes.ylimits = 'auto'
-    plotaxes.title = 'q'
-
-    # Plot q as blue curve:
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = 0
-    plotitem.plotstyle = 'b-'
-
     #-----------------------------------------
 
     # Parameters used only when creating html and/or latex hardcopy
@@ -179,8 +69,6 @@ def setplot(plotdata):
     plotdata.latex_figsperline = 2           # layout of plots
     plotdata.latex_framesperline = 1         # layout of plots
     plotdata.latex_makepdf = False           # also run pdflatex?
-
-    plotdata.kml = False      # Set to true to get tikz output
 
     return plotdata
 
