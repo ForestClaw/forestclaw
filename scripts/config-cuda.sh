6ccf114e17d460156767381b5022b0a895b3e5cf
Donna Calhoun <donnacalhoun@boisestate.edu>
2018-09-09 12:53:21 -0600

    Only compile select applications when using CudaClaw

---
+++ scripts/config-cuda.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+
+../forestclaw/configure \
+    --enable-cudaclaw \
+    --without-blas \
+    --enable-debug \
+    --with-p4est=/home/donnacalhoun/projects/ForestClaw/code/p4est-build-serial/local \
+    --with-sc=/home/donnacalhoun/projects/ForestClaw/code/p4est-build-serial/local \
+    CC=gcc \
+    CFLAGS="-O0 -g -std=c99" \
+    CXX=g++ \
+    CXXFLAGS="-O0 -g" \
+    FC=gfortran \
+    F77=gfortran \
+    FFLAGS="-O0 -g" \
+    LIBS="-L/cm/shared/apps/cuda90/toolkit/lib64/ -lcuda -lcudart" \
+    --disable-shared
+
+
+#     FLIBS="-L/cm/shared/apps/gcc/4.8.1/lib64 -lgfortran" \
+    
