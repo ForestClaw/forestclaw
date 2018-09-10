54a8b37ac6cd8e68e1b14d1a93640847fa70fc69
Donna Calhoun <donnacalhoun@boisestate.edu>
2018-09-10 11:31:22 -0600

    Updated configure script for Cudaclaw

--- scripts/config-cuda.sh
+++ scripts/config-cuda.sh
@@ -6,8 +6,10 @@
     --enable-cudaclaw \
     --without-blas \
     --enable-debug \
+    --with-cuda=/cm/shared/apps/cuda90/toolkit \
     --with-p4est=/home/donnacalhoun/projects/ForestClaw/code/p4est-build-serial/local \
     --with-sc=/home/donnacalhoun/projects/ForestClaw/code/p4est-build-serial/local \
+    CPP="gcc -E" \
     CC=gcc \
     CFLAGS="-O0 -g -std=c99" \
     CXX=g++ \
@@ -16,8 +18,11 @@
     F77=gfortran \
     FFLAGS="-O0 -g" \
     LIBS="-L/cm/shared/apps/cuda90/toolkit/lib64/ -lcuda -lcudart" \
-    --disable-shared
+    NVCC=nvcc \
+    CUDA_CFLAGS=\
+    CUDA_LDFLAGS=\
+    --disable-shared \
 
-
+#   --disable-shared
 #     FLIBS="-L/cm/shared/apps/gcc/4.8.1/lib64 -lgfortran" \
     
