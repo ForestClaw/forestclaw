9cacd26827905a8b5f4838b366bf135236bcef93
Donna Calhoun <donnacalhoun@boisestate.edu>
2018-09-30 21:08:47 -0600

    (swirl) Use pointers for Riemann solver

--- applications/clawpack/advection/2d/swirl/swirl_user.cpp
+++ applications/clawpack/advection/2d/swirl/swirl_user.cpp
@@ -47,13 +47,14 @@ void swirl_link_solvers(fclaw2d_global_t *glob)
 		fc2d_cudaclaw_vtable_t *cudaclaw_vt = fc2d_cudaclaw_vt();        
 
 		cudaclaw_vt->fort_qinit     = &CUDACLAW_QINIT;
-		cudaclaw_vt->fort_setaux    = &CLAWPACK46_SETAUX;
-		cudaclaw_vt->fort_rpn2      = &CLAWPACK46_RPN2ADV;
-		cudaclaw_vt->fort_rpt2      = &CLAWPACK46_RPT2ADV;
 		cudaclaw_vt->fort_b4step2   = &CUDACLAW_B4STEP2;
+		cudaclaw_vt->fort_setaux    = &CLAWPACK46_SETAUX;
+		
+		//cudaclaw_vt->fort_rpn2      = &CLAWPACK46_RPN2ADV;
+		//cudaclaw_vt->fort_rpt2      = &CLAWPACK46_RPT2ADV;
 
-        //swirl_assign_rpn2(&cudaclaw_vt->cuda_rpn2);
-        //FCLAW_ASSERT(cudaclaw_vt->cuda_rpn2 != NULL);
+        swirl_assign_rpn2(&cudaclaw_vt->cuda_rpn2);
+        FCLAW_ASSERT(cudaclaw_vt->cuda_rpn2 != NULL);
 	}
 	else
 	{
