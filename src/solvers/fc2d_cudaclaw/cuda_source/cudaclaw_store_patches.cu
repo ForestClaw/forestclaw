3fef85d7339c3fa04db94f491a2af0202107b26e
Donna Calhoun <donnacalhoun@boisestate.edu>
2018-10-03 08:42:20 -0600

    (cudaclaw) REmove memcopy from store_fluxes (added by Shawn's mrege)

--- src/solvers/fc2d_cudaclaw/cuda_source/cudaclaw_store_patches.cu
+++ src/solvers/fc2d_cudaclaw/cuda_source/cudaclaw_store_patches.cu
@@ -17,11 +17,6 @@ void fc2d_cudaclaw_store_buffer(fclaw2d_global_t* glob,
     double *qold, *aux;
     int meqn, maux;
 
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    float milliseconds;
-
     cudaclaw_fluxes_t *fluxes = (cudaclaw_fluxes_t*) 
                fclaw2d_patch_get_user_data(glob,this_patch);
 
@@ -33,20 +28,5 @@ void fc2d_cudaclaw_store_buffer(fclaw2d_global_t* glob,
     fluxes->qold = qold;
     fluxes->aux = aux;
 
-    cudaEventRecord(start);
-
-    {
-        PROFILE_CUDA_GROUP("fc2d_cudaclaw_store_buffer:cudaMemcpy",5);
-        cudaMemcpy(fluxes->qold_dev, qold, fluxes->num_bytes, cudaMemcpyHostToDevice);
-        cudaMemcpy(fluxes->aux_dev, aux, fluxes->num_bytes_aux, cudaMemcpyHostToDevice);
-    }
-
     flux_array[iter % FC2D_CUDACLAW_BUFFER_LEN] = *fluxes;
-
-    cudaEventRecord(stop);
-    cudaEventSynchronize(stop);
-    milliseconds = 0;
-    cudaEventElapsedTime(&milliseconds, start, stop); 
-    glob->timers[FCLAW2D_TIMER_CUDA_MEMCOPY].cumulative += milliseconds*1e-3;
-
 }
