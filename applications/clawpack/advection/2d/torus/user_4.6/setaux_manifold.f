2db43ffe05c0d018d54e8f12a38a915a44318672
Donna Calhoun <donnacalhoun@boisestate.edu>
2018-12-27 11:23:26 +0100

    (torus) Don't use this version anymore

--- applications/clawpack/advection/2d/torus/user_4.6/setaux_manifold.f
+++ applications/clawpack/advection/2d/torus/user_4.6/setaux_manifold.f
@@ -1,62 +1,58 @@
-      subroutine torus46_setaux(maxmx, maxmy, mbc,mx,my,
-     &      xlower,ylower, dx,dy, maux,aux)
-      implicit none
+c      subroutine torus46_setaux(maxmx, maxmy, mbc,mx,my,
+c     &      xlower,ylower, dx,dy, maux,aux)
+c      implicit none
+c
+c      integer maxmx, maxmy, mbc, mx,my, meqn, maux
+c      double precision dx,dy, xlower, ylower, z
+c      double precision  aux(1-mbc:mx+mbc,1-mbc:my+mbc, maux)
+c
+c      integer i,j, blockno, fc2d_clawpack46_get_block
+c      double precision dxdy, t, xll, yll, psi
+c
+c      integer example
+c
+c      common /excomm_example/ example
+c
+c
+c      blockno = fc2d_clawpack46_get_block()
+c
+c
+c      if (example <= 1) then
+c         call torus46_velocity_psi_comp(mx,my,mbc,dx,dy,
+c     &       blockno,xlower,ylower,aux,maux)
+c      endif
+c      
+c
+c      return
+c      end
 
-      integer maxmx, maxmy, mbc, mx,my, meqn, maux
-      double precision dx,dy, xlower, ylower, z
-      double precision  aux(1-mbc:mx+mbc,1-mbc:my+mbc, maux)
 
-      integer i,j, blockno, fc2d_clawpack46_get_block
-      double precision dxdy, t, xll, yll, psi
 
-      blockno = fc2d_clawpack46_get_block()
+c      subroutine torus46_set_center_velocities(mx,my,mbc,
+c     &      dx,dy,blockno,xlower,ylower,aux,maux)
+c      implicit none
+c
+c      integer mx,my,mbc,maux,blockno
+c      double precision dx,dy, xlower,ylower
+c
+c      double precision xc,yc, t
+c      double precision aux(1-mbc:mx+mbc,1-mbc:my+mbc,maux)
+c
+c      integer i,j
+c      double precision vn
+c
+c      t = 0.d0  !! not used
+c      do i = 1-mbc,mx+mbc
+c         do j = 1-mbc,my+mbc
+cc           # x-faces
+c            xc = xlower + (i-0.5)*dx
+c            yc = ylower + (j-0.5)*dy
+c
+c            torus_center_velocity(blockno,xc,yc,t, u, v)
+c            aux(i,j,2) = u
+c            aux(i,j,3) = v
+c         enddo
+c      enddo
+c
+c      end
 
-      call torus46_velocity_psi_comp(mx,my,mbc,dx,dy,
-     &      blockno,xlower,ylower,aux,maux)
-
-      return
-      end
-
-      subroutine torus46_velocity_psi_comp(mx,my,mbc,
-     &      dx,dy,blockno,xlower,ylower,aux,maux)
-      implicit none
-
-      integer mx,my,mbc,maux,blockno
-      double precision dx,dy, xlower,ylower
-
-      double precision xd1(2),xd2(2), t
-      double precision aux(1-mbc:mx+mbc,1-mbc:my+mbc,maux)
-
-      integer i,j
-      double precision vn
-
-      t = 0.d0  !! not used
-      do i = 1-mbc,mx+mbc
-         do j = 1-mbc,my+mbc
-c           # x-faces
-            xd1(1) = xlower + (i-1)*dx
-            xd1(2) = ylower + j*dy
-
-            xd2(1) = xlower + (i-1)*dx
-            xd2(2) = ylower + (j-1)*dy
-
-            call get_vel_psi_comp(blockno,xd1,xd2,dy,vn,t)
-            aux(i,j,2) = vn
-         enddo
-      enddo
-
-      do j = 1-mbc,my+mbc
-         do i = 1-mbc,mx+mbc
-c           # y-faces
-            xd1(1) = xlower + i*dx
-            xd1(2) = ylower + (j-1)*dy
-
-            xd2(1) = xlower + (i-1)*dx
-            xd2(2) = ylower + (j-1)*dy
-
-            call get_vel_psi_comp(blockno,xd1,xd2,dx,vn,t)
-            aux(i,j,3) = -vn
-         enddo
-      enddo
-
-      end
