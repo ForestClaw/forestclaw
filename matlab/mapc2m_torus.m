5154eda043587fbdee18352a00d8ab68b14a041f
Donna Calhoun <donnacalhoun@boisestate.edu>
2019-02-01 08:29:08 -0500

    (matlab) Add beta parameter for more general torus map

--- matlab/mapc2m_torus.m
+++ matlab/mapc2m_torus.m
@@ -1,9 +1,18 @@
-function [xp,yp,zp] = mapc2m_torus(xc,yc,alpha)
+function [xp,yp,zp] = mapc2m_torus(xc,yc,alpha,beta)
 
-r = 1 + alpha*cos(2*pi*yc);
-zp = alpha.*sin(2*pi*yc);
-th = 2*pi*xc;
-xp = r.*cos(th);
-yp = r.*sin(th);
+if (nargin < 4)
+    beta = 0;
+end
+
+r = alpha*(1 + beta*sin(2*pi*xc));
+R = 1 + r.*cos(2*pi*yc);
+
+% at yc = 0.5,  R = 1-r(x);
+% X = (1-r(x))*cos(2*pi*x);
+% Y = (1-r(x))*sin(2*pi*x);
+
+xp = R.*cos(2*pi*xc);
+yp = R.*sin(2*pi*xc);
+zp = r.*sin(2*pi*yc);
 
 end
