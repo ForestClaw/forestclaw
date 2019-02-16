a95367967b992e2fed595a74c09e2d6d631cf1a4
Donna Calhoun <donnacalhoun@boisestate.edu>
2019-02-16 13:34:56 -0500

    (swirl) Fix python scripts

--- applications/clawpack/advection/2d/swirl/p_00002.py
+++ applications/clawpack/advection/2d/swirl/p_00002.py
@@ -8,7 +8,7 @@ np = 2
 exec = "swirl"
 arg_list = ["mpirun","-n",str(np),exec,"--inifile=timing.ini"]
 jobid = random.randint(1000,9999)
-outfile = "{:s}_0000{:d}.o{:d}".format(np,jobid)
+outfile = "{:s}_0000{:d}.o{:d}".format(exec,np,jobid)
 f = open(outfile,'w')
 po = subprocess.Popen(arg_list,stdout=f)
 print("Starting process {:d} with jobid {:d} on {:d} processor(s).".format(po.pid,jobid,np))
