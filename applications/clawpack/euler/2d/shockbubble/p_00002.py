77a7449ea99de9b3a840759a637193808ecefda4
Donna Calhoun <donnacalhoun@boisestate.edu>
2019-02-16 13:23:46 -0500

    (shockbubble) Add python scripts for timing test; Add config file for timing

--- applications/clawpack/euler/2d/shockbubble/p_00002.py
+++ applications/clawpack/euler/2d/shockbubble/p_00002.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 import random
 
-np = 4
+np = 2
 exec = "shockbubble"
 
 arg_list = ["mpirun","-n",str(np),exec,"--inifile=timing.ini"]
@@ -13,4 +13,4 @@ outfile = "{:s}_0000{:d}.o{:d}".format(np,jobid)
 f = open(outfile,'w')
 po = subprocess.Popen(arg_list,stdout=f)
 print("Starting process {:d} with jobid {:d} on {:d} processor(s).".format(po.pid,jobid,np))
-# po.wait()
+#po.wait()
