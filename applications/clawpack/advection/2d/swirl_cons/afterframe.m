75a2bf5b772335d33bcbd1b9ac4fe234319354cc
Donna Calhoun <donnacalhoun@boisestate.edu>
2019-03-12 09:17:00 +0000

    (clawpack/swirlcons) Update matlab/.ini files

--- applications/clawpack/advection/2d/swirl_cons/afterframe.m
+++ applications/clawpack/advection/2d/swirl_cons/afterframe.m
@@ -3,91 +3,52 @@ axis([-s 1+s -s 1+s])
 daspect([1 1 1]);
 axis on;
 
-ex = 1;
-lstr = 'no limiter';
-mstr = 'WD';
+fprintf('%-10s %16.8e\n','qmin',qmin);
+fprintf('%-10s %16.8e\n','qmax',qmax);
 
-if (PlotType == 4)
-    hold on;
-    [grids, soln] = test_cons();
-    mg =length(grids);
-    solnq = soln{Frame+1}.q;
-    for m = 1:mg
-        g = grids{m};
-        q = solnq{m};
-        plot(g.xc(:),q(:),'bx','linewidth',2);
-    end
-    axis([0 1 0 3.1]);
-    hold off;    
-elseif (ShowUnderOverShoots == 1)
+if (ShowUnderOverShoots == 1)
     qlo = 0;
     qhi = 1;
     under_label = sprintf('%3.1f - %7.1e',qlo,qlo-qmin);
     over_label = sprintf('%3.1f + %7.1e',qhi,qmax-qhi);
-    fprintf('%-10s %16.8e\n','qmin',qmin);
-    fprintf('%-10s %16.8e\n','qmax',qmax);
     colorbar_underover(under_label,over_label);
-else
-    switch ex
-        case 1
-            ca = [-1.4, 2];
-        case 2
-            ca = [-1, 2.5];    
-        case 3
-            ca = 1*[-1,1];
-            % ca = 1e-15*[-1,1];
-    end    
-    %yrbcolormap;
-    colormap(parula);
-    colorbar;
-    caxis(ca);
-    % title(sprintf('Example %d',ex),'fontsize',18);
-    title(sprintf('%s (%s)',mstr,lstr),'fontsize',18);
-
-    cv = linspace(ca(1),ca(2),11);
-    % drawcontourlines(cv);
-    
-    one_patch = length(amrdata) == 1;
-    if (one_patch)
-        figure(2);  
-        clf;    
-        [xm,ym]     = meshgrid(xcenter,ycenter);
-        h = surf(xm,ym,q,'edgecolor','none');
-        caxis(ca);
-        set(gca,'xlim',[0 1],'ylim',[0 1],'zlim',ca,'box','on');
-        colorbar;
-        camlight;
-        xlabel('x','fontsize',16);
-        ylabel('y','fontsize',16);
-        % title(sprintf('Example %d',ex),'fontsize',18);
-        title(sprintf('%s (%s)',mstr,lstr),'fontsize',18);
-        figure(1);    
-        
-    end
-    setpatchborderprops('linewidth',1);
-    showpatchborders;
-    daspect([1 1 1]);
-    view(2);        
-    
-    fprintf('%-10s %16.8e\n','qmin',qmin);
-    fprintf('%-10s %16.8e\n','qmax',qmax);    
+end
+if (mq == 3)
+    % Plot the error
+    ca = [-max([qmin,qmax]),max([qmin,qmax])];
+else    
+    % Plot the solution
+    ca = [-1.4, 2];
 end
 
-% showgridlines
-% hidegridlines(5);
-% hidepatchborders(6)
-% setpatchborderprops('linewidth',2);
-hidepatchborders;
+colormap(jet);
+% colorbar;
+caxis(ca);
+
+showpatchborders;
+setpatchborderprops('linewidth',1);
 hidegridlines;
 
-showgridlines;
-showpatchborders;
+hold on;
+refine_threshold = 0.3;
+u = 1;
+v = 1;
+maxlevel = 5;  % Doesn't have to match true maxlevel
+plot_refine_contours(mx,maxlevel,t,u,v,refine_threshold);
+hold off;
 
+% This is used for creating vectorized PDFs
+prt_tikz = false;
+if (prt_tikz)
+    figsize = [64,64];  % Should match tikz figsize.
+    maxlevel = 8;
+    dpi = mx*2^maxlevel/figsize(1);
+    prefix = 'plot';    
+    % caxis([-1,1]*5e-6)
+    caxis(ca/10);
+    plot_tikz_fig(Frame,figsize,prefix,dpi);    
+end
 
-
-
-set(gca,'fontsize',16);
-% set(gca,'box','on');
 
 NoQuery = 0;
 prt = false;
