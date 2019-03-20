fbede890a2d7beafbf5ef76ab1f881015fa76e22
Donna Calhoun <donnacalhoun@boisestate.edu>
2019-03-20 11:03:10 +0100

    (annulus) Pass in single argument

--- matlab/mapc2m_annulus.m
+++ matlab/mapc2m_annulus.m
@@ -1,7 +1,6 @@
-function [xp,yp,zp] = mapc2m_annulus(xc,yc)
+function [xp,yp,zp] = mapc2m_annulus(xc,yc,beta)
 
-alpha = 0.4;
-r = alpha + (1-alpha)*yc;
+r = beta + (1-beta)*yc;
 xp = r.*cos(2*pi*xc);
 yp = r.*sin(2*pi*xc);
 zp = 0*xp;
--- src/mappings/annulus/mapc2m_annulus.f
+++ src/mappings/annulus/mapc2m_annulus.f
@@ -1,12 +1,12 @@
-      subroutine mapc2m_annulus(blockno,xc,yc,xp,yp,zp,alpha)
+      subroutine mapc2m_annulus(blockno,xc,yc,xp,yp,zp,beta)
       implicit none
 
       integer blockno
-      double precision xc,yc,xp,yp,zp, r, alpha
+      double precision xc,yc,xp,yp,zp, r, beta
       double precision pi
       common /compi/ pi
 
-      r = alpha + (1-alpha)*yc
+      r = beta + (1-beta)*yc
       xp = r*cos(2*pi*xc)
       yp = r*sin(2*pi*xc)
       zp = 0
