92bf814cc24069a42c952d2bb0257eaeae3a64a8
Donna Calhoun <donnacalhoun@boisestate.edu>
2019-05-03 21:47:40 +0100

    (elliptic) Update initial conditions, tagging routines

--- applications/elliptic/mgtest/afterframe.m
+++ applications/elliptic/mgtest/afterframe.m
@@ -1,11 +1,25 @@
+ax = 0;
+bx = 1;
+ay = 0;
+by = 1;
 s = 1e-2;
-axis([-s 1+s -s 1+s])
+axis([ax-s bx+s ay-s by+s])
 daspect([1 1 1]);
 axis off;
 
 showpatchborders;
+setpatchborderprops('linewidth',1);
 
 view(2);
+
+rhs_choice = 3;
+if rhs_choice == 3
+    th = linspace(0,2*pi,500);
+    r = 0.25;
+    hold on;
+    plot(r*cos(th)+0.5,r*sin(th)+0.5,'k','linewidth',2);
+    hold off;
+end
 
 NoQuery = 0;
 prt = false;
--- applications/elliptic/mgtest/fortran/mgtest_fort_rhs.f90
+++ applications/elliptic/mgtest/fortran/mgtest_fort_rhs.f90
@@ -15,7 +15,7 @@ subroutine mgtest_fort_rhs(mbc,mx,my,xlower,ylower,dx,dy,q)
     COMMON /common_pi/ pi, pi2
 
     INTEGER i,j
-    DOUBLE PRECISION x,y,r2
+    DOUBLE PRECISION x,y,r,r2, r0, hsmooth
 
     do i = 1-mbc,mx+mbc
         do j = 1-mbc,my+mbc
@@ -24,13 +24,28 @@ subroutine mgtest_fort_rhs(mbc,mx,my,xlower,ylower,dx,dy,q)
             if (rhs_choice .eq. 1) then
                 r2 = (x-x0)**2 + (y-y0)**2
                 q(i,j) = exp(-alpha*r2)
-            else
+            elseif (rhs_choice .eq. 2) then
                 q(i,j) = sin(pi*a*x)*sin(pi*b*y)
+            elseif (rhs_choice .eq. 3) then
+                r0 = 0.25
+                r = sqrt((x-0.5)**2 + (y-0.5)**2)
+                q(i,j) = Hsmooth(r + r0) - Hsmooth(r - r0)
             endif
         end do
     end do
 
 end subroutine mgtest_fort_rhs
+
+
+double precision function Hsmooth(r)
+    implicit none
+
+    double precision r
+
+    Hsmooth = (tanh(r/0.02d0) + 1)/2.
+
+end function Hsmooth
+
 
 
 
