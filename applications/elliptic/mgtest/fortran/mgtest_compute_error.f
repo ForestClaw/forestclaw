ab151333e3d42c27ec72ae37b38803a9f4bcf80e
Donna Calhoun <donnacalhoun@boisestate.edu>
2019-05-29 11:36:57 +0100

    (elliptic/mgtest) add t back in as argument

--- applications/elliptic/mgtest/fortran/mgtest_compute_error.f
+++ applications/elliptic/mgtest/fortran/mgtest_compute_error.f
@@ -1,16 +1,16 @@
       subroutine mgtest_compute_error(blockno, mx,my,mbc,meqn,
-     &      dx,dy,xlower,ylower,q,error)
+     &      dx,dy,xlower,ylower,t,q,error)
       implicit none
 
       integer mx,my,mbc,meqn, blockno
-      double precision dx, dy, xlower, ylower
+      double precision dx, dy, xlower, ylower, t
       double precision q(1-mbc:mx+mbc,1-mbc:my+mbc,meqn)
       double precision error(1-mbc:mx+mbc,1-mbc:my+mbc,meqn)
 
       integer*8 cont, get_context
 
       integer i,j,m
-      double precision xc,yc, qexact
+      double precision xc,yc, mgtest_qexact
       double precision xc1, yc1, zc1
 
       cont = get_context()
@@ -25,7 +25,7 @@ c           # Map each block (in [0,1]x[0,1]) to single domain in
 c           # [0,1]x[0,1]
             call fclaw2d_map_brick2c(cont,blockno,xc,yc,xc1,yc1,zc1)
 
-            error(i,j,1) = q(i,j,1) - qexact(xc1,yc1);
+            error(i,j,1) = q(i,j,1) - qexact(xc1,yc1)
          enddo
       enddo
 
