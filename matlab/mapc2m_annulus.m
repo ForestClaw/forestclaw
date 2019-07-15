60136b5a9a32310cf9fc7096ceb7c8c659cd82c4
Donna Calhoun <donnacalhoun@boisestate.edu>
2019-07-15 23:24:41 +0100

    Update annulus mapping to include theta range

--- matlab/mapc2m_annulus.m
+++ matlab/mapc2m_annulus.m
@@ -1,8 +1,12 @@
-function [xp,yp,zp] = mapc2m_annulus(xc,yc,beta)
+function [xp,yp,zp] = mapc2m_annulus(xc,yc,beta,theta)
+
+t1 = theta(1);
+t2 = theta(2);
 
 r = beta + (1-beta)*yc;
-xp = r.*cos(2*pi*xc);
-yp = r.*sin(2*pi*xc);
+t = t1 + (t2-t1)*xc;
+xp = r.*cos(2*pi*t);
+yp = r.*sin(2*pi*t);
 zp = 0*xp;
 
 end
