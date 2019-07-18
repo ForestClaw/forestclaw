50ab02d4cec230ca6fb691b60350cf088cd76d9d
Donna Calhoun <donnacalhoun@boisestate.edu>
2019-07-18 18:46:17 +0100

    (paper/amrclaw/torus) Update routines

--- applications/paper/transport_torus/qinit.f
+++ applications/paper/transport_torus/qinit.f
@@ -7,22 +7,25 @@
       double precision q(meqn,1-mbc:mx+mbc, 1-mbc:my+mbc)
       double precision aux(maux,1-mbc:mx+mbc, 1-mbc:my+mbc)
 
-      double precision pi, pi2
-      common /compi/ pi, pi2
-
       integer i,j
-      double precision xc,yc, th, gm
+      double precision xc,yc
+
+      integer init_choice
+      common /initchoice_comm/ init_choice
 
       do j = 1-mbc,my+mbc
-          yc = ylower + (j-0.5)*dy
-          gm = pi2*yc
           do i = 1-mbc,mx+mbc     
               xc = xlower + (i-0.5)*dx
-              th = pi2*xc
-              if (abs(th-pi) .le. pi/2 .and. abs(gm-pi) .le. pi/2) then
+              yc = ylower + (j-0.5)*dy
+              if (init_choice .eq. 0) then
+                  if (abs(xc-0.5) .le. 0.25 
+     &                    .and. abs(yc-0.5) .le. 0.25) then
+                      q(1,i,j) = 1.d0
+                  else
+                      q(1,i,j) = 0.d0
+                  endif
+              elseif (init_choice .eq. 1) then
                   q(1,i,j) = 1.d0
-              else
-                  q(1,i,j) = 0
               endif
           enddo
       enddo
