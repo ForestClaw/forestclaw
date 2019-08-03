b53fcbc3fa87ae826223dd4eb2a72fa1431be452
Donna Calhoun <donnacalhoun@boisestate.edu>
2019-08-03 13:31:55 +0100

    (clawpack/burgers) Add 2nd order Riemann solvers

--- applications/clawpack/burgers/2d/burgers_order3/setplot2.m
+++ applications/clawpack/burgers/2d/burgers_order3/setplot2.m
@@ -3,7 +3,7 @@ OutputDir = './';            % Default (reassign them here anyway)
 
 ForestClaw = 1;     % Plot using ForestClaw preferences.
 
-PlotType = 4;                % type of plot to produce:
+PlotType = 1;                % type of plot to produce:
 			     % 1 = pseudo-color (pcolor)
 			     % 2 = contour
 			     % 3 = Schlieren
