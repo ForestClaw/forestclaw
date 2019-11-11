110c1cb6713d839b90f8348a8adac2acb563ea94
Donna Calhoun <donnacalhoun@boisestate.edu>
2019-11-11 15:09:25 -0700

    (paper/sphere) PLotting for filament test

--- applications/paper/sphere/plot_filament.m
+++ applications/paper/sphere/plot_filament.m
@@ -1,51 +1,226 @@
-clear all;
-clf;
+function plot_filament()
 
-fname = cell(6,1);
+close all
 
-fname{1} = 'lf-CLAW-dx1.5-sp-CFL0.95.dat';
-lstr{1} = '\Delta\lambda = 1.50^o (sp)';
+% fname = cell(num_files,1);
 
-fname{2} = 'lf-CLAW-dx0.75-sp-CFL0.95.dat';
-lstr{2} = '\Delta\lambda = 0.75^o (sp)';
+mx = 30;
+nf = 1;
 
-fname{3} = 'lf-CLAW-dx0.28125-sp-CFL0.95.dat';
-lstr{3} = '\Delta\lambda = 0.28^o (sp)';
+nfiles_max = 21;
 
-fname{4} = 'lf-CLAW-dx1.5-un-CFL0.95.dat';
-lstr{4} = '\Delta\lambda = 1.50^o (un)';
+% Limiters
+% 0 - no limiter
+% 1 - Minmod
+% 2 - Superbee
+% 3 - Van Leer
+% 4 - Montonized Centered
 
-fname{5} = 'lf-CLAW-dx0.75-un-CFL0.95.dat';
-lstr{5} = '\Delta\lambda = 0.75^o (un)';
+nolim = true;
+withlim = [false, false, false, true];  % Use limiter ID
+levels = [true, true, true, true];
+uniform = true;
+adapt = false;
 
-fname{6} = 'lf-CLAW-dx0.1875-un-CFL0.95.dat';
-lstr{6} = '\Delta\lambda = 0.19^o (un)';
+
+% ------------- Initial conditions -------------
+fname = 'level2_uniform_nolim_t0.dat';
+lstr = sprintf('t=0.0');
+s = add_to_plot(fname,lstr,nf,'r','.',10,false);
+nf = nf+1;
+
+% ------------------ MAXLEVEL=1 ----------------
+maxlevel=1;
+deg_eff = 90/(mx*2^maxlevel);
+sym_color = 'r';
 
 
-s.sym = {'r.-','g.-','b.-','ro-','go-','bo-'};
-s.size = [30, 30, 30, 10, 10, 10];
+add2plot = nolim & levels(1) & uniform;
+fname = 'level1_uniform_nolim_t2.5.dat';
+lstr = sprintf('%.2f^o (U,0)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'.',30,add2plot);
+nf = nf+1;
+
+add2plot = withlim(1) & levels(1) & uniform;
+fname = 'level1_uniform_withlim_1_t2.5.dat';
+lstr = sprintf('%.2f^o (U,1)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'s',10,add2plot);
+nf = nf+1;
+
+add2plot = withlim(2) & levels(1) & uniform;
+fname = 'level1_uniform_withlim_2_t2.5.dat';
+lstr = sprintf('%.2f^o (U,2)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'d',10,add2plot);
+nf = nf+1;
+
+add2plot = withlim(3) & levels(1)  & uniform; 
+fname = 'level1_uniform_withlim_3_t2.5.dat';
+lstr = sprintf('%.2f^o (U,3)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'*',10,add2plot);
+nf = nf+1;
+
+add2plot = withlim(4) & levels(1) & uniform;
+fname = 'level1_uniform_withlim_4_t2.5.dat';
+lstr = sprintf('%.2f^o (U,4)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'v',10,add2plot);
+nf = nf+1;
+
+% ------------------ MAXLEVEL=2 ----------------
+maxlevel=2;
+deg_eff = 90/(mx*2^maxlevel);
+sym_color = 'b';
+
+add2plot = nolim & levels(2) & uniform;
+fname = 'level2_uniform_nolim_t2.5.dat';
+lstr = sprintf('%.2f^o (U,0)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'.',30,add2plot);
+nf = nf+1;
+
+add2plot = withlim(1) & levels(2) & uniform;
+fname = 'level2_uniform_withlim_1_t2.5.dat';
+lstr = sprintf('%.2f^o (U,1)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'s',10,add2plot);
+nf = nf+1;
+
+add2plot = withlim(2) & levels(2) & uniform;
+fname = 'level2_uniform_withlim_2_t2.5.dat';
+lstr = sprintf('%.2f^o (U,2)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'d',10,add2plot);
+nf = nf+1;
+
+add2plot = withlim(3) & levels(2) & uniform; 
+fname = 'level2_uniform_withlim_3_t2.5.dat';
+lstr = sprintf('%.2f^o (U,3)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'*',10,add2plot);
+nf = nf+1;
+
+add2plot = withlim(4) & levels(2) & uniform;
+fname = 'level2_uniform_withlim_4_t2.5.dat';
+lstr = sprintf('%.2f^o (U,4)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'v',10,add2plot);
+nf = nf+1;
+
+% ------------------ MAXLEVEL=3 ----------------
+maxlevel=3;
+deg_eff = 90/(mx*2^maxlevel);
+sym_color = 'g';
+
+add2plot = nolim & levels(3) & uniform;
+fname = 'level3_uniform_nolim_t2.5.dat';
+lstr = sprintf('%.2f^o (U,0)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'.',30,add2plot);
+nf = nf+1;
+
+add2plot = withlim(1) & levels(3) & uniform;
+fname = 'level3_uniform_withlim_1_t2.5.dat';
+lstr = sprintf('%.2f^o (U,1)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'s',10,add2plot);
+nf = nf+1;
+
+add2plot = withlim(2) & levels(3) & uniform;
+fname = 'level3_uniform_withlim_2_t2.5.dat';
+lstr = sprintf('%.2f^o (U,2)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'d',10,add2plot);
+nf = nf+1;
+
+add2plot = withlim(3) & levels(3) & uniform;
+fname = 'level3_uniform_withlim_3_t2.5.dat';
+lstr = sprintf('%.2f^o (U,3)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'*',10,add2plot);
+nf = nf+1;
+
+add2plot = withlim(4) & levels(3) & uniform;
+fname = 'level3_uniform_withlim_4_t2.5.dat';
+lstr = sprintf('%.2f^o (U,4)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'v',10,add2plot);
+nf = nf+1;
+
+% ------------------ MAXLEVEL=4 ----------------
+maxlevel=4;
+deg_eff = 90/(mx*2^maxlevel);
+sym_color = 'm';
+
+add2plot = nolim  & levels(4) & uniform;
+fname = 'level4_uniform_nolim_t2.5.dat';
+lstr = sprintf('%.2f^o (U,0)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'.',30,add2plot);
+nf = nf + 1;
+
+add2plot = withlim(1) & levels(4) & uniform;
+fname = 'level4_uniform_withlim_1_t2.5.dat';
+lstr = sprintf('%.2f^o (U,1)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'s',10,add2plot);
+nf = nf + 1;
+
+add2plot = withlim(2) & levels(4) & uniform;
+fname = 'level4_uniform_withlim_2_t2.5.dat';
+lstr = sprintf('%.2f^o (U,2)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'d',10,add2plot);
+nf = nf + 1;
+
+add2plot = withlim(3) & levels(4) & uniform;
+fname = 'level4_uniform_withlim_3_t2.5.dat';
+lstr = sprintf('%.2f^o (U,3)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'*',10,add2plot);
+nf = nf + 1;
+
+add2plot = withlim(4) & levels(4) & uniform;
+fname = 'level4_uniform_withlim_4_t2.5.dat';
+lstr = sprintf('%.2f^o (U,4)',deg_eff);
+s(nf) = add_to_plot(fname,lstr,nf,sym_color,'v',10,add2plot);
+nf = nf + 1;
+
+% -----------------------------------------------
+
+if (nf > nfiles_max+1)
+    error('Maxinum number of files exceeded');
+end
+
 
 plot([0 1],[100 100],'k-','linewidth',3);
 hold on;
 plot([0 1],[0 0],'k-','linewidth',3);
 
-idx = [1 4 2 5 3 6];
-p = zeros(6,1);
-for i = 1:6
-  ii = idx(i);
-  fn = fname{ii};
-  d = load(fn);
-  p(i) = ...
-      plot(d(:,1),d(:,2),s.sym{ii},'markersize',s.size(ii),'linewidth',2);
-  hold on;
+p = zeros(nfiles_max,1);
+for i = 1:nfiles_max
+    if s(i).add2plot
+        fn = s(i).file;
+        fprintf('Plotting results for ''%s''\n',fn);
+        d = load(fn);
+        p(i) = ...
+            plot(d(:,1),d(:,2),'color',s(i).color,'marker',...
+                s(i).sym,'markersize',s(i).size);
+            set(p(i),'linestyle','-','linewidth',1);
+        hold on;    
+    end
 end
 set(gca,'xtick',[0:0.1:(1+1e-8)]);
-axis([0.1 1 -5 130]);
+axis([0.1 1 -5 140]);
 grid on
 
-lh = legend(p,lstr{idx},3);
-set(lh,'fontsize',16);
+m = find(p ~= 0);
+lh = legend(p(m),{s(m).lstr},'location','southwest');
+set(lh,'fontsize',12);
 
 title('Filament diagnostic (cosine hill)','fontsize',18);
 xlabel('Filament threshold','fontsize',16);
-set(gca,'fontsize',16);
+set(gca,'fontsize',14);
+
+shg
+
+end
+
+function s = add_to_plot(fname,lstr,n,c,sty,ms,add2plot)
+
+s = struct('file',[],'lstr',[],'n',[],'color',[],'sym',[],...
+    'size',[],'add2plot',[]);
+
+s.file = fname;
+s.lstr = lstr;
+s.n = n;
+s.color = c;
+s.sym = sty;
+s.size = ms;
+s.add2plot = add2plot;
+
+end
