2a93f378c4612738873411f882a6d89d08f21ab0
Donna Calhoun <donnacalhoun@boisestate.edu>
2020-02-10 07:28:41 -0700

    (clawpack4.6) Move imp argument in call to rpt2

--- src/solvers/fc2d_clawpack4.6/fortran_source/clawpack46_flux2fw.f
+++ src/solvers/fc2d_clawpack4.6/fortran_source/clawpack46_flux2fw.f
@@ -180,7 +180,7 @@ c
 c
        if (method(3).eq.0) go to 999   !# no transverse propagation
 c
-       if (method(3).eq.2) then
+       if (method(2).gt.1 .and. method(3).eq.2) then
 c         # incorporate cqxx into amdq and apdq so that it is split also.
           do 150 i = 1, mx+1
              do 150 m=1,meqn
@@ -195,9 +195,9 @@ c      --------------------------------------------
 c
 c
 c     # split the left-going flux difference into down-going and up-going:
-      call rpt2(ixy,maxm,meqn,mwaves,mbc,mx,
+      call rpt2(ixy,1,maxm,meqn,mwaves,maux,mbc,mx,
      &          q1d,q1d,aux1,aux2,aux3,
-     &          1,amdq,bmasdq,bpasdq)
+     &          amdq,bmasdq,bpasdq)
 c
 c     # modify flux below and above by B^- A^- Delta q and  B^+ A^- Delta q:
       do 160 m=1,meqn
@@ -212,9 +212,9 @@ c
   160          continue
 c
 c     # split the right-going flux difference into down-going and up-going:
-      call rpt2(ixy,maxm,meqn,mwaves,mbc,mx,
+      call rpt2(ixy,2,maxm,meqn,mwaves,maux,mbc,mx,
      &          q1d,q1d,aux1,aux2,aux3,
-     &          2,apdq,bmasdq,bpasdq)
+     &          apdq,bmasdq,bpasdq)
 c
 c     # modify flux below and above by B^- A^+ Delta q and  B^+ A^+ Delta q:
       do 180 m=1,meqn
