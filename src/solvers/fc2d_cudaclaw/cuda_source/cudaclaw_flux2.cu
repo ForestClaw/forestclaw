43e81597eb468f3ac535a4fc92fc1c4ad80cc7ac
Donna Calhoun <donnacalhoun@boisestate.edu>
2020-02-19 02:38:14 -0700

    (cudaclaw) Fix bug in pointer arithmetic

--- src/solvers/fc2d_cudaclaw/cuda_source/cudaclaw_flux2.cu
+++ src/solvers/fc2d_cudaclaw/cuda_source/cudaclaw_flux2.cu
@@ -375,13 +375,13 @@ void cudaclaw_flux2_and_update(const int mx,   const int my,
             double *const amdq   = wave   + meqn*mwaves;  
             double *const apdq   = amdq   + meqn;         
 
-            for(int mq = 0; mq < meqn; mq++)
+            if (ix > 1) 
             {
-                if (ix > 1) 
-                {
-                    ql -= meqn;
-                }
-                else
+                ql -= meqn;
+            }
+            else
+            {
+                for(int mq = 0; mq < meqn; mq++)
                 {
                     int I_q = I + mq*zs;
                     ql[mq] = qold[I_q - 1];  
