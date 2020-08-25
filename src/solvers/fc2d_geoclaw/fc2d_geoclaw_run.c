814a2eee7b469052b81f04db9e6f81e84307fce0
Donna Calhoun <donnacalhoun@boisestate.edu>
2020-08-24 20:57:47 -0600

    (geoclaw) Include header to get correct linkage

--- src/solvers/fc2d_geoclaw/fc2d_geoclaw_run.c
+++ src/solvers/fc2d_geoclaw/fc2d_geoclaw_run.c
@@ -23,6 +23,8 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <fc2d_geoclaw.h>
+
 #include <fclaw2d_patch.h>
 
 #include <fclaw2d_forestclaw.h>
