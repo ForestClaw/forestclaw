1f842ee065977dc5688c03b63a0534186fbf2114
Donna Calhoun <donnacalhoun@boisestate.edu>
2020-10-03 09:15:40 -0700

    (paper/cylinder) Add cylinder mapping

--- applications/paper/all/mapc2m_cylinder.f90
+++ applications/paper/all/mapc2m_cylinder.f90
@@ -9,14 +9,21 @@ SUBROUTINE mapc2m_cylinder(xc,yc,xp,yp,zp)
     DOUBLE PRECISION R_cyl, h_cyl
     COMMON /cylinder_comm/ r_cyl, h_cyl
 
-    DOUBLE PRECISION r1, R, theta, z
+    DOUBLE PRECISION r1, R, theta, z, yc1
 
+
+    !! finite cylinder : 
+    if (yc .lt. 0 .or. yc .gt. 1) then
+!!        write(6,*) 'yc not in [0,1];  yc = ', yc
+    endif
+
+    !!yc1 = mod(yc,1.0)
+    
     CALL map_comp2cylinder(xc,yc,theta,z)
-
     
     xp = R_cyl*cos(theta)
     yp = R_cyl*sin(theta)
-    zp = h_cyl*z
+    zp = z
 
 END SUBROUTINE MAPC2M_CYLINDER
 
