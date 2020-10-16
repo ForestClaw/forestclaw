b201e8a8579171db9e75d5f08afb60d4637dc348
Donna Calhoun <donnacalhoun@boisestate.edu>
2020-10-16 16:00:47 -0600

    (elliptic/heat) Fix initialization

--- applications/elliptic/heat/fortran/heat_init.f90
+++ applications/elliptic/heat/fortran/heat_init.f90
@@ -7,14 +7,14 @@ subroutine heat_init(blockno, mbc,mx,my,meqn, &
     DOUBLE PRECISION q(1-mbc:mx+mbc,1-mbc:my+mbc,meqn)
 
     INTEGER i,j, m
-    DOUBLE PRECISION xc,yc, heat_qexact_rhs
+    DOUBLE PRECISION xc,yc, heat_qexact
     INTEGER blockno
 
     do i = 1-mbc,mx+mbc
         do j = 1-mbc,my+mbc
             xc = xlower + (i-0.5)*dx
             yc = ylower + (j-0.5)*dy
-            q(i,j,1) = heat_qexact_rhs(xc,yc)
+            q(i,j,1) = heat_qexact(xc,yc)
         end do
     end do
 
