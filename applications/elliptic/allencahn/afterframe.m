2081e36dae575d4f9f3c8ce80e66c9c3d67e14de
Donna Calhoun <donnacalhoun@boisestate.edu>
2020-10-19 20:17:43 -0600

    (elliptic/allencahn) Update Matlab files

--- applications/elliptic/allencahn/afterframe.m
+++ applications/elliptic/allencahn/afterframe.m
@@ -19,24 +19,6 @@ cv = linspace(0,1,11);
 colormap(parula);
 colorbar;
 
-% Surface plot
-if (length(amrdata) == 1)
-    % We are only on a single grid
-    figure(2);
-    clf;    
-    h = surf(xcenter,ycenter,q');
-    set(h,'edgecolor','none');
-    view(3);
-    axis square;
-    % view(vfront);
-    % set(gca,'zlim',[-1.1,1.1]);
-%     set(gcf,'color','k');
-%     set(gcf,'clipping','off');
-%     axis off;
-%     camlight;
-    figure(1);
-end
-
 % Axis labels
 ax = 0;
 bx = 1;
@@ -49,21 +31,45 @@ daspect([1 1 1]);
 xlabel('x','fontsize',16);
 ylabel('y','fontsize',16);
 
+
+% Surface plot
+if (length(amrdata) == 1)
+    % We are only on a single grid
+    figure(2);
+    clf;    
+    h = surf(xcenter,ycenter,q');
+    set(h,'edgecolor','none');
+    view(3);
+    axis square;    
+    % view(vfront);
+    set(gca,'zlim',[-1.1,1.1]);
+%     set(gcf,'color','k');
+%     set(gcf,'clipping','off');
+%     axis off;
+%     camlight;
+    figure(1);
+end
+
+
 NoQuery = 0;
 prt = false;
+MaxFrames = 10000;
 if (prt)
-    hidepatchborders;
-    if (ishandle(circle))
-        delete(circle);
-    end
-    mx = 8;
-    maxlevel = 7;
-    dpi = 2^7;    % fix at 128
-    
-    eff_res = mx*2^maxlevel;
-    figsize = (eff_res/dpi)*[1,1];
-    prefix = 'plot';
-    plot_tikz_fig(Frame,figsize,prefix,dpi)
+    % hidepatchborders;
+    fname = sprintf('allencahn%04d.png',Frame);
+    print(fname,'-dpng');
+    fprintf('Printing %s\n',fname);
+%     if (ishandle(circle))
+%         delete(circle);
+%     end
+%     mx = 16;
+%     maxlevel = 6;
+%     dpi = 2^7;    % fix at 128
+%     
+%     eff_res = mx*2^maxlevel;
+%     figsize = (eff_res/dpi)*[1,1];
+%     prefix = 'plot';
+%     plot_tikz_fig(Frame,figsize,prefix,dpi)
 end
 
 
