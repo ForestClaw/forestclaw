f49dc62036c062631d28fef7a6b0fce0138f0200
Donna Calhoun <donnacalhoun@boisestate.edu>
2020-12-11 19:59:32 -0700

    (elliptic/phasefield) Update Matlab file to print PNG files

--- applications/elliptic/phasefield/afterframe.m
+++ applications/elliptic/phasefield/afterframe.m
@@ -1,4 +1,4 @@
-domain_length = 1.5;
+domain_length = 12;
 %{
 if (t < 0.1)
     dl = 1;
@@ -27,48 +27,110 @@ axis square;
 colorbar;
 
 if (mq == 1)
-    caxis([-1,0]);
+    caxis([-1.1,0]);
     % caxis([-1,-0.99])
 elseif (mq == 2)
     caxis([0,1]);
-    % drawcontourlines([0.5,0.5]);
+    drawcontourlines([0.5,0.5]);
 end
+colormap(parula);
 
-%{
+%%{
 if (mq == 1)
-    [xcm,ycm] = meshgrid(xcenter,ycenter);
-    phi = reshape(amrdata(1).data(2,:),mx,my);
-    hold on;
-    [~,h_phi] = contour(xcm,ycm,phi,[0.1,0.5,0.9],'k');
-
-    cv = linspace(-1,0,11);
+    cv = linspace(0,1,11);
     cv([1 end]) = [];
-    [~,h_u(1)] = contour(xcm,ycm,q,cv,'k');        
-    [~,h_u(2)] = contour(xcm,ycm,q,[-0.99,-0.99],'k--');
-    [~,h_u(3)] = contour(xcm,ycm,q,[-0.999,-0.999],'k--');
+    tau = 1e-2;
+    cv = [tau/2, 0.5, 1-tau/2];
+    cv2 = [tau,1-tau];
+    de = linspace(0,1,mx+1);    
+    dc = de(1:end-1) + 0.5/mx;
+    pmin = 10000;
+    pmax = 0;
+    clear h_phi h_u;
+    for k = 1:length(amrdata)
+        xlow = amrdata(k).xlow;
+        ylow = amrdata(k).ylow;
+        dx = amrdata(k).dx;
+        dy = amrdata(k).dy;
+        xhi = xlow + mx*dx;
+        yhi = ylow + my*dy;
+        xc = xlow + dc*(xhi - xlow);
+        yc = ylow + dc*(yhi - ylow);
+        [xcm,ycm] = meshgrid(xc,yc);
+        
+        % Plot phi
+        phi = reshape(amrdata(k).data(2,:),mx,my)';
+        pmin = min([pmin;phi(:)]);
+        pmax = max([pmax;phi(:)]);
+        hold on;
+        if (min(phi(:)) < max(phi(:)))
+            [~,h_phi(1)] = contour(xcm,ycm,phi,[0.5, 0.5],'k','linewidth',2);
+            hold on;
+            % [~,h_phi(2)] = contour(xcm,ycm,phi,[tau/2, 1-tau/2],'k');
+        end
+        % m = ishandle(h_phi);
+        % set(h_phi(m),'linewidth',2);
+
+        %{
+        % Plot u
+        cv_u = linspace(-1,0,11);
+        cv_u([1 end]) = [];
+        q = reshape(amrdata(k).data(1,:),mx,my)';
+        if (min(q(:)) < max(q(:)))
+            [~,h_u(1,k)] = contour(xcm,ycm,q,cv_u,'k');
+            [~,h_u(2,k)] = contour(xcm,ycm,q,[-0.99,-0.99],'k--');
+            [~,h_u(3,k)] = contour(xcm,ycm,q,[-0.999,-0.999],'k--');
+        end
+        %}
+    end
+           
     hold off;
-    set(h_phi,'linewidth',2);
+else
+    cv = linspace(0,1,11);
+    cv([1 end]) = [];
+    drawcontourlines(cv);
 end
 %}
     
 
-fprintf('%10s %16.8f\n','qmin',qmin);
-fprintf('%10s %16.8f\n','qmax',qmax);
+fprintf('%10s %16.8e\n','pmin',pmin);
+fprintf('%10s %16.8e\n','pmax',pmax);
+fprintf('%10s %16.8e\n\n','pmax-pmin',qmax-qmin);
+fprintf('%10s %16.8f\n','umin',qmin);
+fprintf('%10s %16.8f\n','umax',qmax);
 fprintf('%10s %16.8e\n','qmax-qmin',qmax-qmin);
 
 showpatchborders;
+setpatchborderprops('linewidth',1);
+% hidepatchborders(8);
 
 set(gca,'fontsize',16);
 
 % create files for a movie;  Use 'convert2gif' to convert .jpg files
 % to .gif files;  Use 'convert -delay 10 dendrite???.gif dendrite.gif'
 % to create movie dendrite.gif.
-NoQuery = 0;
-MaxFrames = 14;
+NoQuery = 1;
+MaxFrames = 32;
 mmovie = mod(Frame,1) == 0;
 if (mmovie)
-    prtname = sprintf('dendrite%03d',Frame);
-    print('-dpng',prtname);
+    if (length(amrdata) == 1)
+        prefix = 'single';
+    else
+        prefix = 'amr';
+        hidepatchborders;        
+    end
+    setcontourlineprops('linewidth',1);
+    fs = [16,16];
+    dpi = 256;
+    set(gcf,'paperunits','inches');
+    set(gcf,'papersize',fs);
+    set(gca,'position',[0.08 0.08 0.84 0.84]);
+    set(gcf,'paperposition',[0 0 fs]);
+    set(gcf,'GraphicsSmoothing','off')
+    resstr = sprintf('-r%d',dpi);
+    fname = sprintf('dendrite_%s_%02d.png',prefix,Frame);
+    fprintf('Printing grid ''%s''\n',fname);
+    print('-dpng',resstr,fname);
 end
 
 clear afterframe;
