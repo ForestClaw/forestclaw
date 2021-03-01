1d7466882b75cefb1830aa15a6663a5000ff19dc
Donna Calhoun <donnacalhoun@boisestate.edu>
2021-03-01 16:27:36 -0700

    Updating bathymetry for wavetank

--- applications/clawpack/shallow/2d/wavetank/bathy.f90
+++ applications/clawpack/shallow/2d/wavetank/bathy.f90
@@ -16,7 +16,38 @@ SUBROUTINE bathy_complete(xc,b,slope,d2xzb)
 
     DOUBLE PRECISION xc,b,slope,d2xzb
 
-    b = -1.d0
-    slope = 0
+    DOUBLE PRECISION xlower, ylower, x
+    DOUBLE PRECISION xvals(5), yvals(5)
+
+    DATA xvals /-160.8, -125.5, -90, 0, 40/
+    DATA yvals /-4, -4, -0.45, 0, 2/
+
+    xlower = -160.8
+    xupper = 40.
+
+!!  # Assume ghost cells use values for first/last end points. 
+    if (xc .lt. xlower) then
+        x = xlower
+    else if (xc .gt. xupper) then
+        x = xupper
+    endif
+
+
+    !! From RJL "make_celledges.txt"
+    !!  xzpairs = [(-160.8, -4),       # left edge
+    !!             (-125.5, -4),       # start of first slope
+    !!             (-90,    -0.45),    # start of beach
+    !!             (  0,     0),       # shore
+    !!             ( 40,     2)]       # right edge
+
+    !! Second derivative is always 0
     d2xzb = 0
+    do i = 1,4
+        if (xvals(i) .le. x .and. x .le. xvals(i+1)) then
+            slope = (yvals(i+1) - yvals(i))/(xvals(i+1) - xvals(i))
+            b = yvals(i) + slope*(x - xvals(i))
+            return
+        endif
+    end do
+
 END SUBROUTINE bathy_complete
--- applications/clawpack/shallow/2d/wavetank/user_4.6/qinit.f90
+++ applications/clawpack/shallow/2d/wavetank/user_4.6/qinit.f90
@@ -15,28 +15,17 @@
     DOUBLE PRECISION pi, pi2
     COMMON /compi/ pi, pi2
 
-    double precision :: a,b,h0
-    common /common_initheight/ a,b,h0
+    DOUBLE PRECISION  grav, dry_tolerance, sea_level
+    COMMON /common_swe/ grav, dry_tolerance, sea_level
+
 
     DOUBLE PRECISION xc,yc,hl, hr, ur, ybar
     INTEGER i,j
 
-!!      
-    hl = h0 + a
-    hr = h0
-    ur = 0.d0
-
-    ybar = ylower + my*dy/2.d0
-
     do i = 1-mbc,mx+mbc
-        xc = xlower + (i-0.5)*dx  
         do j = 1-mbc,my+mbc
-            yc = ylower + (j-0.5)*dy
-            if (abs(xc) .lt. b) then
-                q(i,j,1) = hl  !!h0 + a*exp(-20*(yc-ybar)**2)
-            else
-                q(i,j,1) = hr
-            endif
+            eta = sea_level
+            q(i,j,1) = max(sea_level, eta - aux(i,j,1))
             q(i,j,2) = 0
             q(i,j,3) = 0
         end do
