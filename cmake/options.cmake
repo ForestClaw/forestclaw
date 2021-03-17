7fdad54f8f2f28368464d751b7991df4a9590c68
Scott Aiton <scott@aiton.net>
2021-03-17 12:31:52 -0600

    (cmake) added clawpack option

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,6 +2,7 @@ option(mpi "use MPI library" on)
 option(openmp "use OpenMP" on)
 
 option(geoclaw "build Geoclaw")
+option(clawpack "build Clawpack")
 
 option(p4est_external "force build of p4est")
 option(sc_external "force build of libsc")
