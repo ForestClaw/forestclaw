a6e3fc7f0d19f339f94382113d2e6e217dad8bee
Scott Aiton <scott@aiton.net>
2021-03-19 13:42:05 -0600

    (cmake) added poisson example

---
+++ applications/elliptic/elliptic.cmake
@@ -0,0 +1,15 @@
+# --------------------------------------------
+# ThunderEgg solver
+# --------------------------------------------
+
+## Elliptic example
+add_subdirectory(applications/elliptic/poisson)
+
+## Heat equation
+#add_subdirectory(applications/elliptic/heat)
+
+## Allencahn equation
+#add_subdirectory(applications/elliptic/allencahn)
+
+## Crystal growth
+#add_subdirectory(applications/elliptic/phasefield)
