62bfa6f9a89168850d68b05fe5ae717f03af1063
Donna Calhoun <donnacalhoun@boisestate.edu>
2021-04-13 11:29:01 -0600

    Updates to forestclaw.py

--- applications/geoclaw/chile2010/forestclaw.py
+++ applications/geoclaw/chile2010/forestclaw.py
@@ -2,31 +2,30 @@
 
 from configparser import ConfigParser
 
+# TODO : 
+#   1.  Fix problem with 'ascii-out'.  Why is it printing false?
+#   2.  Fix mthbc and mthlim so they follow 'ord_str' idiom.
+#   3.  Check that anything we don't set here is set in fclaw_options.
+
+
 class ForestClawData(object):
     # Several forestclaw attributes (ignore for now)
 
     def __init__(self):
+        self.user = {}
+
         self.minlevel = 0
         self.maxlevel = 0
 
-        self.weighted_partition = True
         self.regrid_interval = 1
         self.refine_threshold = 0.5
-        self.coarsen_threshold = 0.1
-        self.smooth_refine = False
-        self.smooth_level = 0
-        self.outstyle_uses_maxlevel = False
+        self.coarsen_threshold = 0.5
         self.subcycle = False
-        self.advance_one_step = False
-        self.trapfpe = False
-        self.mpi_debug = False
-        self.conservation_check = False
-        self.run_user_diagnostics = False
-        self.output_gauges = False
         self.output = True
         self.verbosity = 'essential'
-        self.report_timing=True
-        self.report_timing_verbosity = 'wall'
+
+        self.mi = 1
+        self.mj = 1
 
 
     def write(self,rundata):
@@ -38,68 +37,66 @@ class ForestClawData(object):
         refinement_data = rundata.refinement_data
         amrdata = rundata.amrdata
 
-        geoclaw['user'] = {
-            'example' : 0
-        }
-        
-        geoclaw['clawpatch'] = {
-        '# Grid dimensions' : None,
-        '   # mx_leaf' : None,
-        '   mx' : clawdata.num_cells[0],"\n"
-        '   # my_leaf' :None, 
+        user = {
+        '   # User defined parameters' : None}
+        for k in self.user.keys():
+            user[f'   {k:}'] = self.user[k]
+
+        geoclaw['user'] = user
+
+        geoclaw['clawpatch'] = {"\n"
+        '   # Grid dimensions' : None,
+        '   mx' : clawdata.num_cells[0],
         '   my' : clawdata.num_cells[1], "\n"
+
         '   # Number of ghost cells': None,
         '   mbc': clawdata.num_ghost, "\n"
 
+        '   # Number of auxiliary variables' : None,
         '   maux': clawdata.num_aux,"\n"
+
         '   # Number of equations' : None,
         '   meqn': clawdata.num_eqn        
             }
 
-        geoclaw['Options'] = {
+        if clawdata.output_step_interval is None:
+            clawdata.output_step_interval = 1
 
-        '# Regridding information' : None,
-        '   # Minimum level':None,
-        '   minlevel' : self.minlevel,"\n"
-        '   # Maximum levels of refinement':None,
+
+        assert self.verbosity in ['silent','essential','production','info','debug'], \
+            "Error (forestclaw) : Invalid value for verbosity"
+
+
+        geoclaw['Options'] = {"\n"
+
+        '# Regridding options' : None,
+        '   minlevel' : self.minlevel,
         '   maxlevel' : self.maxlevel,"\n"
-        "  # Regrid every 'regrid_interval' time steps.":None,
-        '   weighted_partition' : self.weighted_partition,"\n"
 
-        '   regrid-interval' : self.regrid_interval,
-        '   refine-threshold' : self.refine_threshold,
-        '   coarsen-threshold' : self.coarsen_threshold,
-        '   smooth-refine' : self.smooth_refine,
-        '   smooth-level' : self.smooth_level,
-        '   outstyle-uses-maxlevel' : self.outstyle_uses_maxlevel,
-        '   subcycle' : self.subcycle,
-        '   advance-one-step' : self.advance_one_step,"\n"
+        "   # Regrid every 'regrid_interval' time steps using threshold values":None, 
+        '   regrid_interval' : self.regrid_interval,
+        '   refine_threshold' : self.refine_threshold,
+        '   coarsen_threshold' : self.coarsen_threshold, "\n"
 
-        '   # Diagnostics and debugging':None,
-        '   # Trap floating point errors.':None,
-        '   trapfpe' : self.trapfpe,"\n"
-        '   # Attach mpi processes in gdb':None,
-        '   mpi-debug' : self.mpi_debug,"\n"
-        '   conservation-check' : self.conservation_check,
-        '   run-user-diagnostics' : self.run_user_diagnostics,"\n"
+        "   # Smooth refinement (around finest level)":None,
+        '   smooth-refine' : "True",
+        '   smooth-level' : self.maxlevel,"\n"
 
-        '# File and console IO' : None,
-        '   output-gauges' : self.output_gauges,
-        '   output' : self.output,
-        '   verbosity' : self.verbosity,"\n"
 
         '# Time stepping' : None, 
         '   # Final time':None,
         '   tfinal': clawdata.tfinal, "\n"     
 
         '   # Take a fixed time step':None,
-        '   use-fixed-dt': not clawdata.dt_variable,"\n"
-        "  # Initial time step for 'minlevel'":None,
-        '   initial-dt': clawdata.dt_initial,"\n"
-        '   # maximum cfl':None,
-        '   max-cfl': clawdata.cfl_max,"\n"
-        '   # desired cfl':None,
-        '   desired-cfl': clawdata.cfl_desired,"\n"
+        '   use_fixed_dt': not clawdata.dt_variable,"\n"
+
+        "   # Initial time step for 'minlevel'":None,
+        '   initial_dt': clawdata.dt_initial,"\n"
+
+        '   # CFL constraints : Timestep will never exceed max_cfl and will ' "\n"
+        '   # try not to exceed desired_cfl' : None,
+        '   max_cfl': clawdata.cfl_max,
+        '   desired_cfl': clawdata.cfl_desired,"\n"
 
         '   # 1 : Output steps  = tfinal/nout;':None,                  
         '   # 2 : not implemented;':None,                              
@@ -109,16 +106,28 @@ class ForestClawData(object):
         '   # Used for all three out styles;  has different meaning, though':None,                                     
         '   nout': clawdata.num_output_times,"\n"
         '   # Only used if outstyle is 3':None,
-        '   nstep': clawdata.output_style,"\n"
+        '   nstep': clawdata.output_step_interval,"\n"
 
-        '# Mapping' : None,"\n"
-        '   # Domain dimensions' : None,
+        '   # Advanced time stepping' : None, 
+        '   subcycle' : self.subcycle, "\n"
+
+        '# File and console IO' : None,
+        '   output' : self.output,
+        '   verbosity' : self.verbosity,"\n"
+
+
+        '# Domain geometry' : None,"\n"
+        '   # Domain dimensions [ax,bx]x[ay,by]' : None,
         '   ax': clawdata.lower[0],
         '   bx': clawdata.upper[0],
         '   ay': clawdata.lower[1],
-        '   by': clawdata.upper[1]
+        '   by': clawdata.upper[1],"\n"
+
+        '   # Block dimensions' : None,
+        '   mi': self.mi,
+        '   mj': self.mj
         }
-        
+
         #mthbc
         mthbc_in  = [clawdata.bc_lower[0], clawdata.bc_upper[0], 
                      clawdata.bc_lower[1], clawdata.bc_upper[1]]
@@ -148,56 +157,75 @@ class ForestClawData(object):
             lim_str += " " + str(lim[k])
 
         # Apply same idea to order 
-        ord_in  = clawdata.source_split
-
-        ord = [0]*clawdata.order
+        ord = [0]*2
         ord_str = ""
-        bc_dict = {'none':0, 'godunov' : 1, 'strang' : 2}
-        for k in range(clawdata.order):
-            if type(ord_in) == str:
-                ord[k] = bc_dict[ord_in]
-            else:
-                ord[k] = ord_in
+        if clawdata.order in [1,'godunov','Godunov']:
+            ord[0] = 1
+        if clawdata.order in [2,'Lax-Wendroff']:
+            ord[0] = 2
+        ord_str = str(ord[0])
 
-            ord_str += " " + str(ord[k])
+        ord_in = clawdata.transverse_waves
+        if clawdata.transverse_waves in ['none' , 0]:
+            ord[1] = 0
+        if clawdata.transverse_waves in ['increment' , 1]:
+            ord[1] = 1
+        if clawdata.transverse_waves in ['all' , 2]:
+            ord[1] = 2
+        ord_str += " " + str(ord[1])
 
-        # Can we get dashes instead of underscores?   yes
+        # Source terms splitting:
+        #   src_split == 0 or 'none'    ==> no source term (src routine never called)
+        #   src_split == 1 or 'godunov' ==> Godunov (1st order) splitting used, 
+        #   src_split == 2 or 'strang'  ==> Strang (2nd order) splitting used,  not recommended.
 
+        if clawdata.source_split in [0, 'none']:
+            src_split = 0
+        if clawdata.source_split in [1, 'godunov', 'Godunov']:
+            src_split = 1
+        if clawdata.source_split in [2, 'strang', 'Strang']:
+            src_split = 2
+
+        if clawdata.output_format in ['ascii']:      # 'ascii' or 'binary' 
+            ascii_out = 'T'
+        else:
+            ascii_out = 'F'
+
+        print(clawdata.output_format)
+        print(ascii_out)
 
         geoclaw['geoclaw'] = {
             '   # normal and transverse order': None,
-            '   # Order of the method' :None,
-            '   # Source terms splitting:' :None,
-            "   #   src_split == 0 or 'none'    ==> no source term (src routine never called)" :None,
-            "   #   src_split == 1 or 'godunov' ==> Godunov (1st order) splitting used," :None,
-            "   #   src_split == 2 or 'strang'  ==> Strang (2nd order) splitting used,  not recommended." :None,
-            #order': "2 2", "\n" 
+            '   # Order of accuracy:': None,
+            '   #   1 => Godunov,': None,  
+            '   #   2 => Lax-Wendroff plus limiters': None,
             '   order': ord_str,"\n"
 
-            '   # mcapa' : None,
+            '   # Location of capacity function in auxiliary array' : None,
             '   mcapa': clawdata.capa_index,"\n"
-            '   # mbathy' : None,
-            '   mbathy': 1,"\n"
-            '   # src_term' : None,
-            '   src-term': 1,"\n"
 
-            '   # mwaves': None,
+            '   # Source term splitting' : None,
+            '   src_term': src_split,"\n"
+
+            '   # Use an f-waves update (default : True)'
+            '   use-fwaves' : clawdata.use_fwaves,"\n"
+
+            '   # Number of waves': None,
             '   mwaves': clawdata.num_waves,"\n"
 
-            "  # mthlim (is a vector in general, with 'mwaves' entries": None,
-            '    # List of limiters to use for each wave family:': None,
-            '    # Required:  len(limiter) == num_waves': None,
-            '    # Some options:': None,
-            "    #   0 or 'none'     ==> no limiter (Lax-Wendroff)": None,
-            "    #   1 or 'minmod'   ==> minmod": None,
-            "    #   2 or 'superbee' ==> superbee": None,
-            "    #   3 or 'mc'       ==> MC limiter": None,
-            "    #   4 or 'vanleer'  ==> van Leer": None,
-            #'   mthlim': "4 4 4","\n"
+
+            "   # mthlim (is a vector in general, with 'mwaves' entries": None,
+            '   # List of limiters to use for each wave family:': None,
+            '   # Required:  len(limiter) == num_waves': None,
+            '   # Some options:': None,
+            "   #   0 or 'none'     ==> no limiter (Lax-Wendroff)": None,
+            "   #   1 or 'minmod'   ==> minmod": None,
+            "   #   2 or 'superbee' ==> superbee": None,
+            "   #   3 or 'mc'       ==> MC limiter": None,
+            "   #   4 or 'vanleer'  ==> van Leer": None,
             '   mthlim' : lim_str,"\n"
 
             '   # mthbc (=left,right,bottom,top)' : None,
-                # 'mthbc': "1  1  1  1",   # 0,1,2,3
             '   # Choice of BCs at xlower and xupper:':None,
             '   # 0 => user specified (must modify bcN.f to use this option)':None,
             '   # 1 => extrapolation (non-reflecting outflow)':None,
@@ -205,18 +233,11 @@ class ForestClawData(object):
             '   # 3 => solid wall for systems where q(2) is normal velocity':None,
             '   mthbc' : mthbc_str,"\n"
 
-            '# Coarsening' : None,            #(probably going to go away)
-            '   dry-tolerance_c': geo_data.dry_tolerance,
-            '   wave-tolerance_c': refinement_data.wave_tolerance,
-            '   speed-tolerance-entries_c': 6,
-            '   speed-tolerance-c': "1e12 1e12 1e12 1e12 1e12 1e12","\n"
+            '   dry-tolerance-c': geo_data.dry_tolerance,
+            '   wave-tolerance-c': refinement_data.wave_tolerance, "\n"
 
             '   # Output' : None,
-            '   ascii-out': True,"\n"
-
-            '   # Number of lines in gauge file to store in memory before printing' : None,
-            '   gauge-buffer-length': 100
-
+            '   ascii-out': ascii_out
             }
             
         with open('geoclaw.ini','w') as geoclawfile:
