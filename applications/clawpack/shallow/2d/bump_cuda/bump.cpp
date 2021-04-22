8dcdfae227f1df8c0087087fb4afb82a189aef8d
Scott Aiton <scott@aiton.net>
2021-04-22 13:26:57 -0700

    (ci) fixed cuda_bump

--- applications/clawpack/shallow/2d/bump_cuda/bump.cpp
+++ applications/clawpack/shallow/2d/bump_cuda/bump.cpp
@@ -152,7 +152,7 @@ main (int argc, char **argv)
     fclaw2d_clawpatch_options_t *clawpatch_opt;
     fc2d_clawpack46_options_t   *claw46_opt;
     fc2d_clawpack5_options_t    *claw5_opt;
-    fc2d_cudaclaw_options_t     *cuclaw5_opt;
+    fc2d_cudaclaw_options_t     *cuclaw_opt;
 
     fclaw2d_global_t            *glob;
     fclaw2d_domain_t            *domain;
