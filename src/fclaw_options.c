1e598dcb82511642beb5c42d07be9eb194b72d2c
Scott Aiton <scott@aiton.net>
2021-05-26 09:18:45 -0600

    revert fenableexcept options

--- src/fclaw_options.c
+++ src/fclaw_options.c
@@ -368,7 +368,7 @@ fclaw_options_check (fclaw_options_t * fclaw_opt)
     if (fclaw_opt->trapfpe)
     {
         fclaw_global_infof("Enabling floating point traps\n");
-        feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW);
+        // feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW);
         feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW);
     }
 #endif
