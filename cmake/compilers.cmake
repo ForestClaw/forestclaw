cb2617f8f69c9e76d3aeccea324c1c53bc579df8
Scott Aiton <scott@aiton.net>
2021-06-01 13:05:02 -0600

    fix generator expression

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -5,9 +5,9 @@ include(CheckCCompilerFlag)
 check_c_compiler_flag(-Wall _has_wall)
 if(_has_wall)
   add_compile_options(
-    $<$<COMPILE_LANGUAGE:C> -Wall>
-    $<$<COMPILE_LANGUAGE:CXX> -Wall>
-    $<$<COMPILE_LANGUAGE:Fortran> -Wall>
+    $<$<COMPILE_LANGUAGE:C>:-Wall>
+    $<$<COMPILE_LANGUAGE:CXX>:-Wall>
+    $<$<COMPILE_LANGUAGE:Fortran>:-Wall>
     )
 else()
   check_c_compiler_flag(/Wall _has_msvc_wall)
