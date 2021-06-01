1b67f7b89033897935faf331390cb58bb4c9b5dd
Scott Aiton <scott@aiton.net>
2021-06-01 12:44:47 -0600

    check to enable needed dependencies

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,10 +1,10 @@
 option(mpi "use MPI library")
 option(openmp "use OpenMP")
 
+option(clawpack "build Clawpack")
 option(geoclaw "build Geoclaw")
 option(cudaclaw "build CudaClaw")
 option(thunderegg "build ThunderEgg")
-option(clawpack "build Clawpack")
 
 option(thunderegg_external "force build of ThunderEgg")
 option(p4est_external "force build of p4est")
@@ -18,3 +18,25 @@ if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   # will not take effect without FORCE
   set(CMAKE_INSTALL_PREFIX "${PROJECT_BINARY_DIR}/local" CACHE PATH "Install top-level directory" FORCE)
 endif()
+
+# enable needed dependencies
+if(clawpack)
+  set(clawpatch ON)
+  set(clawpack4.6 ON)
+  set(clawpack5 ON)
+endif(clawpack)
+
+if(geoclaw)
+  set(clawpatch ON)
+endif(geoclaw)
+
+if(cudaclaw)
+  set(clawpatch ON)
+  set(clawpack4.6 ON)
+  set(clawpack5 ON)
+endif(cudaclaw)
+
+if(thunderegg)
+  set(clawpatch ON)
+  set(clawpack4.6 ON)
+endif(thunderegg)
