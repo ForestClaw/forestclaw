284524d6a1f60ba7b0bab6a369335c0faa9ce4d5
Donna Calhoun <donnacalhoun@boisestate.edu>
2021-06-14 08:58:06 +0100

    (clawpack/shallow/radialdam) Remove old gradient calculation

--- applications/clawpack/shallow/2d/radialdam/afterframe.m
+++ applications/clawpack/shallow/2d/radialdam/afterframe.m
@@ -26,13 +26,24 @@ if PlotType==4
   end
   if (abs(tref-t) > 1e-8)
     error('times are not compatible');
-  end;
+  end
   hold on;
-  [qref,xref,p] = plotframe1ez(rad1d,mq,'b-');
-  set(p,'LineWidth',2,'markersize',10);
+  [qref,xref,p] = plotframe1ez(rad1d,mq,'k-');
+  set(p,'LineWidth',1.5,'markersize',10);
 
   [h2d,leg_str] = getlegendinfo(0);
-  legend([h2d,p],{leg_str{:},'1d reference soln'});
+%   lh = legend([h2d,p],{leg_str{:},'1d reference soln'});
+%   set(lh,'AutoUpdate','off');  
+  hold off
+  
+  hold on
+  xm = (xref(1:end-1) + xref(2:end))/2;
+  pgrad = plot(xm(:),diff(qref(:))./diff(xref(:)),'r','linewidth',2);
+  axis([0,2.5,-2,6]);
+  
+  lh = legend([h2d,p,pgrad],{leg_str{:},'1d reference soln','Gradient'});
+  set(lh,'AutoUpdate','off');  
+    
   hold off;
 
 end
