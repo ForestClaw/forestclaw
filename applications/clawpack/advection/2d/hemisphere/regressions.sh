8fec3779421903aeae8693e7a49eb84beb778a4d
Scott Aiton <scott@aiton.net>
2021-06-22 12:20:13 -0600

    added echo statement for test

--- applications/clawpack/advection/2d/hemisphere/regressions.sh
+++ applications/clawpack/advection/2d/hemisphere/regressions.sh
@@ -6,11 +6,8 @@ application=$(pwd)/applications/clawpack/advection/2d/hemisphere/hemisphere
 cd $srcdir/applications/clawpack/advection/2d/hemisphere/
 
 # run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
+echo $FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS
 $FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=0 || exit 1
 $FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 --user:example=0 || exit 1
 $FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=1 || exit 1
 $FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 --user:example=1 || exit 1
-
-
-echo $FCLAW_MPI_RUN $FCLAW_MPI_TEST_FLAGS
-exit 1
