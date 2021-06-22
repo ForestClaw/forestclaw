2f46d43684afb4d0cdb7bab33069ad54a03c7907
Scott Aiton <scott@aiton.net>
2021-06-22 12:13:24 -0600

    fixed variable name

--- applications/clawpack/advection/2d/hemisphere/regressions.sh
+++ applications/clawpack/advection/2d/hemisphere/regressions.sh
@@ -6,10 +6,10 @@ application=$(pwd)/applications/clawpack/advection/2d/hemisphere/hemisphere
 cd $srcdir/applications/clawpack/advection/2d/hemisphere/
 
 # run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
-$FCLAW_MPI_RUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=0 || exit 1
-$FCLAW_MPI_RUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 --user:example=0 || exit 1
-$FCLAW_MPI_RUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=1 || exit 1
-$FCLAW_MPI_RUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 --user:example=1 || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=0 || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 --user:example=0 || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=1 || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 --user:example=1 || exit 1
 
 
 echo $FCLAW_MPI_RUN $FCLAW_MPI_TEST_FLAGS
