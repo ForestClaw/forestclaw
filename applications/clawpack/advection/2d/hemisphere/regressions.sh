ea02c934f7c4af57b23555458f3bb64db8dbbab2
Scott Aiton <scott@aiton.net>
2021-06-22 14:56:07 -0600

    added regressions.sh script for annulus

--- applications/clawpack/advection/2d/hemisphere/regressions.sh
+++ applications/clawpack/advection/2d/hemisphere/regressions.sh
@@ -6,7 +6,6 @@ application=$FCLAW_APPLICATIONS_BUILD_DIR/clawpack/advection/2d/hemisphere/hemis
 cd $FCLAW_APPLICATIONS_SRC_DIR/clawpack/advection/2d/hemisphere/
 
 # run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
-echo $FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS
 $FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=0 || exit 1
 $FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 --user:example=0 || exit 1
 $FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=1 || exit 1
