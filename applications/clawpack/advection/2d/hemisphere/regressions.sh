72541032ce025f641ecd4cd8767ed332da54a392
Scott Aiton <scott@aiton.net>
2021-06-22 12:06:21 -0600

    pass some variables to test scripts

--- applications/clawpack/advection/2d/hemisphere/regressions.sh
+++ applications/clawpack/advection/2d/hemisphere/regressions.sh
@@ -6,7 +6,11 @@ application=$(pwd)/applications/clawpack/advection/2d/hemisphere/hemisphere
 cd $srcdir/applications/clawpack/advection/2d/hemisphere/
 
 # run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
-$application -F regression.ini --user:claw-version=4 --user:example=0 || exit 1
-$application -F regression.ini --user:claw-version=5 --user:example=0 || exit 1
-$application -F regression.ini --user:claw-version=4 --user:example=1 || exit 1
-$application -F regression.ini --user:claw-version=5 --user:example=1 || exit 1
+$FCLAW_MPI_RUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=0 || exit 1
+$FCLAW_MPI_RUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 --user:example=0 || exit 1
+$FCLAW_MPI_RUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=1 || exit 1
+$FCLAW_MPI_RUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 --user:example=1 || exit 1
+
+
+echo $FCLAW_MPI_RUN $FCLAW_MPI_TEST_FLAGS
+exit 1
