aa044c39fefba587da517b6da1b779f572762db2
Scott Aiton <scott@aiton.net>
2021-06-22 10:13:52 -0600

    changed script to check for mpi

--- applications/clawpack/advection/2d/hemisphere/regressions.sh
+++ applications/clawpack/advection/2d/hemisphere/regressions.sh
@@ -5,8 +5,14 @@ application=$(pwd)/applications/clawpack/advection/2d/hemisphere/hemisphere
 # change to source dir for working directory
 cd $srcdir/applications/clawpack/advection/2d/hemisphere/
 
+# get runner
+if command -v mpirun
+then
+	runner="mpirun -n 2"
+fi
+
 # run programs, exit script with nonzero on failure
-mpirun -n 2 $application -F regression.ini --user:claw-version=4 --user:example=0 || exit 1
-mpirun -n 2 $application -F regression.ini --user:claw-version=5 --user:example=0 || exit 1
-mpirun -n 2 $application -F regression.ini --user:claw-version=4 --user:example=1 || exit 1
-mpirun -n 2 $application -F regression.ini --user:claw-version=5 --user:example=1 || exit 1
+$runner $application -F regression.ini --user:claw-version=4 --user:example=0 || exit 1
+$runner $application -F regression.ini --user:claw-version=5 --user:example=0 || exit 1
+$runner $application -F regression.ini --user:claw-version=4 --user:example=1 || exit 1
+$runner $application -F regression.ini --user:claw-version=5 --user:example=1 || exit 1
