643b2b5435985555ac083f277f27c6a2ad1312db
Scott Aiton <scott@aiton.net>
2021-06-22 16:51:27 -0600

    added regression.sh for gaussian and slotted_disk

---
+++ applications/clawpack/transport/gaussian/regressions.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# absolute path to application we are testing
+application=$FCLAW_APPLICATIONS_BUILD_DIR/clawpack/transport/gaussian/gaussian
+
+# change to source dir for working directory
+cd $FCLAW_APPLICATIONS_SRC_DIR/clawpack/transport/gaussian/
+
+# run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=0 || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 --user:example=1 || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 --user:example=0 || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 --user:example=1 || exit 1
