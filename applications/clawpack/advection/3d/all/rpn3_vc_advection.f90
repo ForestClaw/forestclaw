eda4f1faa2f00951623059688899fecd8d9401ee
Donna Calhoun <donnacalhoun@boisestate.edu>
2021-07-14 06:09:44 -0600

    (claw3/advection/swirl) Remove unused files

---
+++ applications/clawpack/advection/3d/all/rpn3_vc_advection.f90
@@ -0,0 +1,67 @@
+subroutine clawpack46_rpn3(ixyz,maxm,meqn,mwaves,maux,mbc,mx,ql,qr, & 
+    auxl,auxr,wave,s,amdq,apdq)
+! Riemann-solver for the advection equation
+!    q_t  +  u*q_x + v*q_y + w*q_z = 0
+! where u and v are a given velocity field.
+!
+! waves: 1
+! equations: 1
+! aux fields: 3
+!
+! Conserved quantities:
+!       1 q
+!
+! Auxiliary variables
+!       1 x_velocity
+!       2 y_velocity
+!       3 z_velocity
+!
+! The velocities are specified at the left/bottom/back cell face centers.
+!
+! Solve Riemann problems along one slice of data.
+! This data is along a slice in the x-direction if ixyz=1
+!                               the y-direction if ixyz=2.
+!                               the z-direction if ixyz=3.
+!
+! On input, ql contains the state vector at the left edge of each cell
+!           qr contains the state vector at the right edge of each cell
+!
+! On output, wave contains the waves, s the speeds,
+! and amdq, apdq the left-going and right-going flux differences,
+! respectively.  Note that in this advective form, the sum of
+! amdq and apdq is not equal to a difference of fluxes except in the
+! case of constant velocities.
+!
+! Note that the i'th Riemann problem has left state qr(i-1,:)
+!                                    and right state ql(i,:)
+! From the basic clawpack routines, this routine is called with ql = qr
+
+    implicit none
+
+    integer, intent(in) :: ixyz, maxm, meqn, mwaves, mbc, mx, maux
+
+    double precision, intent(in) :: ql, qr, auxl, auxr
+    double precision, intent(out) :: wave, s, amdq, apdq
+
+    dimension wave(meqn,mwaves,1-mbc:maxm+mbc)
+    dimension    s(mwaves,1-mbc:maxm+mbc)
+    dimension   ql(meqn,1-mbc:maxm+mbc)
+    dimension   qr(meqn,1-mbc:maxm+mbc)
+    dimension amdq(meqn,1-mbc:maxm+mbc)
+    dimension apdq(meqn,1-mbc:maxm+mbc)
+    dimension auxl(maux,1-mbc:maxm+mbc)
+    dimension auxr(maux,1-mbc:maxm+mbc)
+
+    integer :: i
+
+    ! Set wave, speed, and flux differences:
+    do i = 2-mbc, mx+mbc
+        wave(1,1,i) = ql(1,i) - qr(1,i-1)
+        s(1,i) = auxl(ixyz,i)
+        ! The flux difference df = s*wave all goes in the downwind direction:
+        amdq(1,i) = dmin1(auxl(ixyz,i), 0.d0) * wave(1,1,i)
+        apdq(1,i) = dmax1(auxl(ixyz,i), 0.d0) * wave(1,1,i)
+    end do
+
+    return
+end subroutine clawpack46_rpn3
---
+++ applications/clawpack/advection/3d/all/rpt3_vc_advection.f90
@@ -0,0 +1,90 @@
+subroutine clawpack46_rpt3(ixyz,icoor,imp,maxm,meqn,mwaves,maux,mbc, & 
+    mx,ql,qr,aux1,aux2,aux3,asdq,bmasdq,bpasdq)
+    ! Riemann solver in the transverse direction for the
+    ! advection equations.
+    !
+    ! On input,
+    !
+    !    ql,qr is the data along some one-dimensional slice, as in rpn3
+    !         This slice is
+    !             in the x-direction if ixyz=1,
+    !             in the y-direction if ixyz=2, or
+    !             in the z-direction if ixyz=3.
+    !    asdq is an array of flux differences (A^*\Dq).
+    !         asdq(i,:) is the flux difference propagating away from
+    !         the interface between cells i-1 and i.
+    !    Note that asdq represents B^*\Dq if ixyz=2 or C^*\Dq if ixyz=3.
+    !
+    !    ixyz indicates the direction of the original Riemann solve,
+    !         called the x-like direction in the table below:
+    !
+    !               x-like direction   y-like direction   z-like direction
+    !      ixyz=1:        x                  y                  z
+    !      ixyz=2:        y                  z                  x
+    !      ixyz=3:        z                  x                  y
+    !
+    !    icoor indicates direction in which the transverse solve should
+    !         be performed.
+    !      icoor=2: split in the y-like direction.
+    !      icoor=3: split in the z-like direction.
+    !
+    !    For example,
+    !      ixyz=1, icoor=2 means asdq=A^*\Dq, and should be split in y into
+    !                        bmasdq = B^-A^*\Dq,
+    !                        bpasdq = B^+A^*\Dq.
+    !
+    !      ixyz=2, icoor=2 means asdq=B^*\Dq, and should be split in z into
+    !                        bmasdq = C^-B^*\Dq,
+    !                        bpasdq = C^+B^*\Dq.
+    !
+    !    The parameter imp is generally needed only if aux
+    !    arrays are being used, in order to access the appropriate
+    !    variable coefficients:
+    !
+    !    imp = 1 if asdq = A^- \Dq,  the left-going flux difference
+    !          2 if asdq = A^+ \Dq, the right-going flux difference
+    !
+    !    aux2(:,:,2) is a 1d slice of the aux array along the row
+    !                 where the data ql, qr lie.   
+    !    aux1(:,:,2) and aux3(:,:,2) are neighboring rows in the 
+    !                 y-like direction
+    !    aux2(:,:,1) and aux2(:,:,3) are neighboring rows in the 
+    !                z-like direction
+
+    implicit none
+
+    integer, intent(in) :: ixyz, icoor, maxm, meqn, mwaves, mbc, mx, maux, imp
+    double precision, intent(in) :: ql, qr, asdq, aux1, aux2, aux3
+    double precision, intent(out) :: bmasdq, bpasdq
+
+    dimension     ql(meqn,1-mbc:maxm+mbc)
+    dimension     qr(meqn,1-mbc:maxm+mbc)
+    dimension   asdq(meqn,1-mbc:maxm+mbc)
+    dimension bmasdq(meqn,1-mbc:maxm+mbc)
+    dimension bpasdq(meqn,1-mbc:maxm+mbc)
+    dimension   aux1(maux,1-mbc:maxm+mbc,3)
+    dimension   aux2(maux,1-mbc:maxm+mbc,3)
+    dimension   aux3(maux,1-mbc:maxm+mbc,3)
+
+    integer :: iuvw, i, i1
+
+    ! set iuvw = 1 for u, 2 for v, 3 for w component of velocity
+    ! depending on transverse direction:
+    iuvw = ixyz + icoor - 1
+    if (iuvw.gt.3) iuvw = iuvw-3
+
+    do i=2-mbc,mx+mbc
+        i1 = i-2+imp    !#  =  i-1 for amdq,  i for apdq
+        if (icoor == 2) then  !! transverse dir. is y-like direction
+            bmasdq(1,i) = dmin1(aux2(iuvw,i1,2), 0.d0)*asdq(1,i)
+            bpasdq(1,i) = dmax1(aux3(iuvw,i1,2), 0.d0)*asdq(1,i)
+        else !! icoor == 3  !! transverse dir. is z-like direction
+            !! quanities split into cmasdq and cpasdq
+            bmasdq(1,i) = dmin1(aux2(iuvw,i1,2),0.d0)*asdq(1,i)
+            bpasdq(1,i) = dmax1(aux2(iuvw,i1,3),0.d0)*asdq(1,i)
+        endif
+    end do
+
+    return
+end subroutine clawpack46_rpt3
+
