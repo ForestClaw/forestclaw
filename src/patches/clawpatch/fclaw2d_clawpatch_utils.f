b575944a2c18385995b48ae3ccac7b04a302a8e8
Scott Aiton <scott@aiton.net>
2021-08-20 15:27:58 -0600

    (doc) fully doxumented clawpatch

--- src/patches/clawpatch/fclaw2d_clawpatch_utils.f
+++ src/patches/clawpatch/fclaw2d_clawpatch_utils.f
@@ -1,7 +1,13 @@
 c> @file
 c> Clawpatch utilities
 
-c> ????
+c -------------------------------------------------------
+c> @brief Slope limiting function
+c>
+c> @param[in] sl, sr the slope on the left and right sides
+c> @param[in] mth type of limiting to use
+c> @return the limited slope
+c -------------------------------------------------------
       double precision function 
      &      fclaw2d_clawpatch_compute_slopes(sl,sr,mth)
       implicit none
@@ -61,10 +67,10 @@ c         fclaw2d_clawpatch_compute_slopes = sc
 
 c     ---------------------------------------------------------------
 c>    @brief Determines the tranformation from a coarse patch to it's
-c> .  half-sized neighbor's coordinate system
+c>    half-sized neighbor's coordinate system
 c>
 c>    @param[in]  the pointer to the fclaw2d_patch_transform_data struct
-c>    @param[out] a the 2x2 tranform matrix for the i.j indexes of a patch
+c>    @param[out] a the 2x2 tranform matrix for the i,j indexes of a patch
 c>    @param[out] f the transform vector
 c     ---------------------------------------------------------------
       subroutine fclaw2d_clawpatch_build_transform(transform_ptr,a,f)
@@ -110,6 +116,13 @@ c     # calls to T.
 
       end
 
+c --------------------------------------------------------------------
+c> @brief checks if the index is in the range given
+c>
+c> @param[in] iff, jff the index to check
+c> @param[in] i2, j2 the list of indecies
+c> @return true if the index is valid
+c --------------------------------------------------------------------
       logical function fclaw2d_clawpatch_check_indices(iff,jff,i2,j2)
       implicit none
 
@@ -138,6 +151,14 @@ c     # calls to T.
 
       end
 
+c --------------------------------------------------------------------
+c> @brief checks if the index is valid for interpolation
+c>
+c> @param[in] i, j the idnex to check
+c> @param[in] mx, my the number of cells in the x and y directions
+c> @param[in] mbc the number of ghost cells
+c> @return true if the index is valid
+c --------------------------------------------------------------------
       logical function fclaw2d_clawpatch_is_valid_interp(i,j,mx,my,mbc)
       implicit none
       integer i,j,mx, my, mbc
@@ -151,6 +172,13 @@ c     # calls to T.
 
       end
 
+c --------------------------------------------------------------------
+c> @brief checks if the index is valid for averaging
+c>
+c> @param[in] i, j the idnex to check
+c> @param[in] mx, my the number of cells in the x and y directions
+c> @return true if the index is valid
+c --------------------------------------------------------------------
       logical function fclaw2d_clawpatch_is_valid_average(i,j,mx,my)
       implicit none
 
@@ -164,16 +192,18 @@ c     # calls to T.
 
       end
 
-c>    Initializes the area and edge length arrays for fclaw2d_clawpatch_registers
+c --------------------------------------------------------------------
+c> Initializes the area and edge length arrays for fclaw2d_clawpatch_registers
 c>
-c>    @param[in]  mx, my number of cells in the x and y directions
-c>    @param[in]  mbc number of ghost cells
-c>    @param[in]  dx, dy spacings in the x and y direcitons
-c>    @param[in]  area area of each cell
-c>    @param[in]  edgelengths edge lengts of each cell along the x and y directions
-c>    @param[out] area0, area1, area2, area3 areas of cells along the edges
-c>    @param[out] el0, el1, el2, el3 edges lengths of the cells along the dges
-c>    @param[in]  manifold 1 if using manifold
+c> @param[in]  mx, my number of cells in the x and y directions
+c> @param[in]  mbc number of ghost cells
+c> @param[in]  dx, dy spacings in the x and y direcitons
+c> @param[in]  area area of each cell
+c> @param[in]  edgelengths edge lengts of each cell along the x and y directions
+c> @param[out] area0, area1, area2, area3 areas of cells along the edges
+c> @param[out] el0, el1, el2, el3 edges lengths of the cells along the dges
+c> @param[in]  manifold 1 if using manifold
+c --------------------------------------------------------------------
       subroutine clawpatch_time_sync_setup(mx,my,mbc,dx,dy,
      &                                     area,edgelengths,
      &                                     area0,area1,area2,area3,
