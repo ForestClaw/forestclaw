d502aacb132c82f4f6ec585defa4840188bf17b6
Donna Calhoun <donnacalhoun@boisestate.edu>
2021-10-12 21:11:30 -0600

    (clawpatch) Replace 'class' with struct

--- src/patches/clawpatch/fclaw2d_clawpatch.hpp
+++ src/patches/clawpatch/fclaw2d_clawpatch.hpp
@@ -34,7 +34,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 struct fclaw2d_patch;
 struct fclaw2d_global;
-class  fclaw2d_metric_patch_t;
+struct  fclaw2d_metric_patch_t;
 
 /**
  * @brief Stores data for each patch
