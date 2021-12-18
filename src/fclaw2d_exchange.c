391c9dcd61dbd3419cfda11ca81a4194f931aeb7
Donna Calhoun <donnacalhoun@boisestate.edu>
2021-12-17 19:06:21 -0700

    Fix typo in comment

--- src/fclaw2d_exchange.c
+++ src/fclaw2d_exchange.c
@@ -99,7 +99,7 @@ void build_remote_ghost_patches(fclaw2d_global_t* glob)
         fclaw2d_patch_remote_ghost_build(glob,ghost_patch,blockno,
                                          patchno, build_mode);
     }
-    fclaw_infof("[%d] Done building remove ghost patches : %d\n",
+    fclaw_infof("[%d] Done building remote ghost patches : %d\n",
                             domain->mpirank,domain->num_ghost_patches);
 }
 
