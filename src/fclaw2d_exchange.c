05dbd9f758401b4e7592b4363efb733006d895b8
Donna Calhoun <donnacalhoun@boisestate.edu>
2021-12-17 18:55:28 -0700

    WIP : Debugging MPI exchange for 3d

--- src/fclaw2d_exchange.c
+++ src/fclaw2d_exchange.c
@@ -99,6 +99,8 @@ void build_remote_ghost_patches(fclaw2d_global_t* glob)
         fclaw2d_patch_remote_ghost_build(glob,ghost_patch,blockno,
                                          patchno, build_mode);
     }
+    fclaw_infof("[%d] Done building remove ghost patches : %d\n",
+                            domain->mpirank,domain->num_ghost_patches);
 }
 
 static
