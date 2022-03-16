f08cb36e1adf8531fca443f37b3be4618c0fc807
KYANJO <brian.kyanjo@aims.ac.rw>
2022-03-16 17:06:43 -0600

    Revert accidentally pushed changes

--- applications/geoclaw/bowl_slosh/regressions.sh
+++ applications/geoclaw/bowl_slosh/regressions.sh
@@ -3,8 +3,7 @@
 application=$FCLAW_APPLICATIONS_BUILD_DIR/geoclaw/bowl_slosh/bowl_slosh
 
 # change to source dir for working directory
-cd $FCLAW_APPLICATIONS_SRC_DIR/geoclaw/bowl_slosh/
+cd $FCLAW_APPLICATIONS_SRC_DIR/geoclaw/bowl_slosh/regression
 
 # run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
-$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 || exit 1
-$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini || exit 1
