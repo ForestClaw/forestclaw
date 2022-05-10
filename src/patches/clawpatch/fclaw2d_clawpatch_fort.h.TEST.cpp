83038365ccb1936c8ddc2fd2a3e747d0a7fe3839
Scott Aiton <scott@aiton.net>
2022-05-10 13:18:18 -0600

    add 3d exceeds threshold test

--- src/patches/clawpatch/fclaw2d_clawpatch_fort.h.TEST.cpp
+++ src/patches/clawpatch/fclaw2d_clawpatch_fort.h.TEST.cpp
@@ -27,26 +27,33 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <fclaw2d_forestclaw.h>
 #include <fclaw2d_global.h>
+
 #include <fclaw2d_clawpatch.h>
 #include <fclaw2d_clawpatch_options.h>
 #include <fclaw2d_clawpatch_fort.h>
 
+#include <fclaw3dx_clawpatch.h>
+#include <fclaw3dx_clawpatch_options.h>
+#include <fclaw3dx_clawpatch_fort.h>
+
 namespace{
     struct exceeds_test_parameters
     {
-        int *blockno          = (int*)   1;
-        double *qval          = (double*)2;
-        double* qmin          = (double*)3;
-        double *qmax          = (double*)4;
-        double *quad          = (double*)5;
-        double *dx            = (double*)6;
-        double *dy            = (double*)7;
-        double *xc            = (double*)8;
-        double *yc            = (double*)9;
-        double *tag_threshold = (double*)10;
-        int *init_flag        = (int*)   11;
-        int *is_ghost         = (int*)   12;
-        int return_value      =          13;
+        int *blockno          = (int*)    1;
+        double *qval          = (double*) 2;
+        double* qmin          = (double*) 3;
+        double *qmax          = (double*) 4;
+        double *quad          = (double*) 5;
+        double *dx            = (double*) 6;
+        double *dy            = (double*) 7;
+        double *dz            = (double*) 8;
+        double *xc            = (double*) 9;
+        double *yc            = (double*)10;
+        double *zc            = (double*)11;
+        double *tag_threshold = (double*)12;
+        int *init_flag        = (int*)   13;
+        int *is_ghost         = (int*)   14;
+        int return_value      =          15;
     };
 
     exceeds_test_parameters global_exceeds_test_parameters;
@@ -108,6 +115,75 @@ TEST_CASE("FCLAW2D_CLAWPATCH_EXCEEDS_THRESHOLD calls user function")
                                                   params.tag_threshold,
                                                   params.init_flag,
                                                   params.is_ghost);
+    fclaw2d_global_unset_global();
+
+    CHECK_EQ(ret, params.return_value);
+
+    fclaw2d_global_destroy(glob);
+}
+TEST_CASE("FCLAW3DX_CLAWPATCH_EXCEEDS_THRESHOLD calls user function")
+{
+    global_exceeds_test_parameters = exceeds_test_parameters();
+    exceeds_test_parameters& params = global_exceeds_test_parameters;
+
+	fclaw2d_global_t* glob = fclaw2d_global_new();
+
+    fclaw2d_vtables_initialize(glob);
+    fclaw3dx_clawpatch_vtable_initialize(glob, 4);
+
+    fclaw3dx_clawpatch_vt(glob)->fort_user_exceeds_threshold = 
+        [](const int *blockno,
+           const double *qval, 
+           const double* qmin, 
+           const double *qmax,
+           const double quad[], 
+           const double *dx, 
+           const double *dy, 
+           const double *dz, 
+           const double *xc, 
+           const double *yc, 
+           const double *zc, 
+           const double *tag_threshold,
+           const int *init_flag,
+           const int *is_ghost)
+        {
+            exceeds_test_parameters& params = global_exceeds_test_parameters;
+            CHECK_EQ(blockno, params.blockno);
+            CHECK_EQ(qval, params.qval);
+            CHECK_EQ(qmin, params.qmin);
+            CHECK_EQ(qmax, params.qmax);
+            CHECK_EQ(quad, params.quad);
+            CHECK_EQ(dx, params.dx);
+            CHECK_EQ(dy, params.dy);
+            CHECK_EQ(dz, params.dz);
+            CHECK_EQ(xc, params.xc);
+            CHECK_EQ(yc, params.yc);
+            CHECK_EQ(zc, params.zc);
+            CHECK_EQ(tag_threshold, params.tag_threshold);
+            CHECK_EQ(init_flag, params.init_flag);
+            CHECK_EQ(is_ghost, params.is_ghost);
+            return params.return_value;
+        };
+
+	fclaw3dx_clawpatch_options_t opts;
+    opts.refinement_criteria = FCLAW_REFINE_CRITERIA_USER;
+    fclaw3dx_clawpatch_options_store(glob, &opts);
+
+    fclaw2d_global_set_global(glob);
+    int ret = FCLAW3DX_CLAWPATCH_EXCEEDS_THRESHOLD(params.blockno,
+                                                   params.qval,
+                                                   params.qmin,
+                                                   params.qmax,
+                                                   params.quad,
+                                                   params.dx,
+                                                   params.dy,
+                                                   params.dz,
+                                                   params.xc,
+                                                   params.yc,
+                                                   params.zc,
+                                                   params.tag_threshold,
+                                                   params.init_flag,
+                                                   params.is_ghost);
     fclaw2d_global_unset_global();
 
     CHECK_EQ(ret, params.return_value);
