fca9f70289ed79a62efcf1a99693f690447c4436
Scott Aiton <scott@aiton.net>
2022-06-08 14:00:11 -0600

    store cudaclaw options in pointer map

--- src/solvers/fc2d_cudaclaw/fc2d_cudaclaw_options.c
+++ src/solvers/fc2d_cudaclaw/fc2d_cudaclaw_options.c
@@ -28,9 +28,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <fclaw2d_clawpatch_options.h>
 #include <fclaw2d_global.h>
 #include <fclaw_options.h>
-#include <fclaw_package.h>
-
-static int s_cudaclaw_options_package_id = -1;
+#include <fclaw_pointer_map.h>
 
 static void*
 cudaclaw_register (fc2d_cudaclaw_options_t* clawopt, sc_options_t * opt)
@@ -223,12 +221,14 @@ fc2d_cudaclaw_options_t*  fc2d_cudaclaw_options_register (fclaw_app_t * app,
 
 fc2d_cudaclaw_options_t* fc2d_cudaclaw_get_options(fclaw2d_global_t *glob)
 {
-    int id = s_cudaclaw_options_package_id;
-    return (fc2d_cudaclaw_options_t*) fclaw_package_get_options(glob,id);
+	fc2d_cudaclaw_options_t* clawopt = (fc2d_cudaclaw_options_t*) 
+	   							fclaw_pointer_map_get(glob->options, "fc2d_cudaclaw");
+	FCLAW_ASSERT(clawopt != NULL);
+	return clawopt;
 }
 
 void fc2d_cudaclaw_options_store (fclaw2d_global_t* glob, fc2d_cudaclaw_options_t* clawopt)
 {
-    int id = fclaw_package_container_add_pkg(glob,clawopt);
-    s_cudaclaw_options_package_id = id;
+	FCLAW_ASSERT(fclaw_pointer_map_get(glob->options,"fc2d_cudaclaw") == NULL);
+	fclaw_pointer_map_insert(glob->options, "fc2d_cudaclaw", clawopt, NULL);
 }
--- src/solvers/fc2d_cudaclaw/fc2d_cudaclaw_options.h.TEST.cpp
+++ src/solvers/fc2d_cudaclaw/fc2d_cudaclaw_options.h.TEST.cpp
@@ -26,26 +26,26 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <fclaw2d_global.h>
 #include <fclaw2d_options.h>
 #include <fclaw2d_clawpatch_options.h>
-#include <fc2d_geoclaw.h>
-#include <fc2d_geoclaw_options.h>
+#include <fc2d_cudaclaw.h>
+#include <fc2d_cudaclaw_options.h>
 #include <fclaw2d_forestclaw.h>
 #include <test/doctest.h>
 
-TEST_CASE("fc2d_geoclaw_options can store options in two seperate globs")
+TEST_CASE("fc2d_cudaclaw_options can store options in two seperate globs")
 {
 	fclaw2d_global_t* glob1 = fclaw2d_global_new();
 	fclaw2d_global_t* glob2 = fclaw2d_global_new();
 
-	fc2d_geoclaw_options_t* opts1 = FCLAW_ALLOC_ZERO(fc2d_geoclaw_options_t,1);
-	fc2d_geoclaw_options_t* opts2 = FCLAW_ALLOC_ZERO(fc2d_geoclaw_options_t,1);
+	fc2d_cudaclaw_options_t* opts1 = FCLAW_ALLOC_ZERO(fc2d_cudaclaw_options_t,1);
+	fc2d_cudaclaw_options_t* opts2 = FCLAW_ALLOC_ZERO(fc2d_cudaclaw_options_t,1);
 
-	fc2d_geoclaw_options_store(glob1, opts1);
+	fc2d_cudaclaw_options_store(glob1, opts1);
 	/* glob1 has one package glob2 has two */
 	fclaw2d_options_store(glob2, FCLAW_ALLOC_ZERO(fclaw_options_t,1));
-	fc2d_geoclaw_options_store(glob2, opts2);
+	fc2d_cudaclaw_options_store(glob2, opts2);
 
-	CHECK_EQ(fc2d_geoclaw_get_options(glob1), opts1);
-	CHECK_EQ(fc2d_geoclaw_get_options(glob2), opts2);
+	CHECK_EQ(fc2d_cudaclaw_get_options(glob1), opts1);
+	CHECK_EQ(fc2d_cudaclaw_get_options(glob2), opts2);
 
 	fclaw2d_global_destroy(glob1);
 	fclaw2d_global_destroy(glob2);
@@ -53,29 +53,29 @@ TEST_CASE("fc2d_geoclaw_options can store options in two seperate globs")
 
 #ifdef FCLAW_ENABLE_DEBUG
 
-TEST_CASE("fc2d_geoclaw_get_options fails if not intialized")
+TEST_CASE("fc2d_cudaclaw_get_options fails if not intialized")
 {
 	fclaw2d_global_t* glob1 = fclaw2d_global_new();
 	fclaw2d_global_t* glob2 = fclaw2d_global_new();
 
-	CHECK_THROWS(fc2d_geoclaw_vt(glob1));
+	CHECK_THROWS(fc2d_cudaclaw_vt(glob1));
 
-	CHECK_THROWS(fc2d_geoclaw_vt(glob2));
+	CHECK_THROWS(fc2d_cudaclaw_vt(glob2));
 
 	fclaw2d_global_destroy(glob1);
 	fclaw2d_global_destroy(glob2);
 }
 
-TEST_CASE("fc2d_geoclaw_options_store fails if called twice on a glob")
+TEST_CASE("fc2d_cudaclaw_options_store fails if called twice on a glob")
 {
 	fclaw2d_global_t* glob1 = fclaw2d_global_new();
 	fclaw2d_global_t* glob2 = fclaw2d_global_new();
 
-	fc2d_geoclaw_options_store(glob1, FCLAW_ALLOC_ZERO(fc2d_geoclaw_options_t,1));
-	CHECK_THROWS(fc2d_geoclaw_options_store(glob1, FCLAW_ALLOC_ZERO(fc2d_geoclaw_options_t,1)));
+	fc2d_cudaclaw_options_store(glob1, FCLAW_ALLOC_ZERO(fc2d_cudaclaw_options_t,1));
+	CHECK_THROWS(fc2d_cudaclaw_options_store(glob1, FCLAW_ALLOC_ZERO(fc2d_cudaclaw_options_t,1)));
 
-	fc2d_geoclaw_options_store(glob2, FCLAW_ALLOC_ZERO(fc2d_geoclaw_options_t,1));
-	CHECK_THROWS(fc2d_geoclaw_options_store(glob2, FCLAW_ALLOC_ZERO(fc2d_geoclaw_options_t,1)));
+	fc2d_cudaclaw_options_store(glob2, FCLAW_ALLOC_ZERO(fc2d_cudaclaw_options_t,1));
+	CHECK_THROWS(fc2d_cudaclaw_options_store(glob2, FCLAW_ALLOC_ZERO(fc2d_cudaclaw_options_t,1)));
 
 	fclaw2d_global_destroy(glob1);
 	fclaw2d_global_destroy(glob2);
