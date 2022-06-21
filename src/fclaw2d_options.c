18469a69d11a78ac447db32f0a0a10227a4bc697
Scott Aiton <scott@aiton.net>
2022-06-21 10:37:19 -0600

    update copyright

--- src/fclaw2d_options.c
+++ src/fclaw2d_options.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2012-2021 Carsten Burstedde, Donna Calhoun
+Copyright (c) 2012-2022 Carsten Burstedde, Donna Calhoun, Scott Aiton
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
