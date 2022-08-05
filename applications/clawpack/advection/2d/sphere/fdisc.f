7e7c02ee3169c62ab4bde023d29d65b9f0da3b4b
donnaaboise <donna.calhoun@gmail.com>
2022-08-04 17:33:49 -0700

    (clawpack/advection) WIP : More testing

--- applications/clawpack/advection/2d/sphere/fdisc.f
+++ applications/clawpack/advection/2d/sphere/fdisc.f
@@ -1,23 +1,33 @@
       double precision function  fdisc(blockno,xc,yc)
       implicit none
 
-      double precision xc,yc, xp, yp, zp
+      double precision xc,yc
       integer blockno
       integer*8 cont, fclaw_map_get_context
 
       double precision pi, pi2
       common /compi/ pi, pi2
 
+      double precision phi0, phi, xp, yp, zp, rp
+
       cont = fclaw_map_get_context()
 
       call fclaw2d_map_c2m(cont,
      &      blockno,xc,yc,xp,yp,zp)
 
-c     #  Make a beach ball.
-      if (xp .ne. 0) then
-         fdisc = yp/xp
-      else
-         fdisc = 1
-      endif
+!!c     #  Make a beach ball.
+!!      if (xp .ne. 0) then
+!!         fdisc = yp/xp
+!!      else
+!!         fdisc = 1
+!!      endif
+
+
+      rp = sqrt(xp**2 + yp**2 + zp**2)
+
+      phi = asin(zp/rp)
+
+      phi0 = pi/4.d0
+      fdisc = abs(phi - phi0) - pi/32.0
 
       end
