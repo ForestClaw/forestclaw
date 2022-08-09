0920242d0edcaeaa507b8844871c27efa2b6b778
donnaaboise <donna.calhoun@gmail.com>
2022-08-08 17:30:22 -0700

    (clawpack) Update list of 3d applications

--- applications/clawpack/claw3.cmake
+++ applications/clawpack/claw3.cmake
@@ -8,5 +8,6 @@ add_subdirectory(clawpack/advection/3d/swirl)
 add_subdirectory(clawpack/advection/3d/filament)
 add_subdirectory(clawpack/advection/3d/sphere)
 add_subdirectory(clawpack/advection/3d/disk)
+add_subdirectory(clawpack/advection/3d/latlong)
 
 
