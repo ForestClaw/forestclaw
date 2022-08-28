ea19bd97ca8cc4e18d8cae4a3866cb79e709c6fd
donnaaboise <donna.calhoun@gmail.com>
2022-08-27 21:26:34 -0600

    (advection/3d/latlong) Update regression testing

--- applications/clawpack/advection/3d/latlong/regressions.sh
+++ applications/clawpack/advection/3d/latlong/regressions.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # absolute path to application we are testing
-application=$FCLAW_APPLICATIONS_BUILD_DIR/clawpack/advection/2d/latlong/latlong
+application=$FCLAW_APPLICATIONS_BUILD_DIR/clawpack/advection/3d/latlong/latlong
 
 # change to source dir for working directory
-cd $FCLAW_APPLICATIONS_SRC_DIR/clawpack/advection/2d/latlong/
+cd $FCLAW_APPLICATIONS_SRC_DIR/clawpack/advection/3d/latlong/
 
 # run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
 $FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 || exit 1
-$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 || exit 1
