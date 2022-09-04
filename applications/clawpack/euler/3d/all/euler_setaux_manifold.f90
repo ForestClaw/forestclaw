4261d5f95f31b1b878913e4e33e5e2ea233fea92
donnaaboise <donna.calhoun@gmail.com>
2022-09-04 17:57:59 -0600

    (euler/3d) WIP : Add setaux for mapped Euler

---
+++ applications/clawpack/euler/3d/all/euler_setaux_manifold.f90
@@ -0,0 +1,51 @@
+subroutine overpressure_setaux_manifold(mbc,mx,my,mz, mcapa, & 
+               xlower,ylower,zlower,dx,dy,dz,maux, & 
+               aux,blockno, xrot, yrot, zrot, volume, & 
+               faceareas)
+    implicit none
+
+    integer mbc, mx, my, mz, maux, mcapa
+    integer blockno
+    double precision dx,dy, dz, xlower, ylower, zlower
+    double precision  aux(1-mbc:mx+mbc,1-mbc:my+mbc, 1-mbc:mz+mbc,maux)
+
+    double precision xrot(-mbc:mx+mbc+1,-mbc:my+mbc+1,-mbc:mz+mbc+2,3,3)
+    double precision yrot(-mbc:mx+mbc+1,-mbc:my+mbc+1,-mbc:mz+mbc+2,3,3)
+    double precision zrot(-mbc:mx+mbc+1,-mbc:my+mbc+1,-mbc:mz+mbc+2,3,3)
+
+    double precision volume(-mbc:mx+mbc+1,-mbc:my+mbc+1,-mbc:mz+mbc)
+    double precision faceareas(-mbc:mx+mbc+1,-mbc:my+mbc+1, -mbc:mz+mbc+2,3)
+
+    integer i,j, k, ii,jj,count
+    double precision dxdydz, dxdy, dxdz,dydz
+
+    dxdydz = dx*dy*dz
+    dxdy = dx*dy
+    dxdz = dx*dz
+    dydz = dy*dz
+
+    do i = 1-mbc,mx+mbc
+        do j = 1-mbc,my+mbc
+            do k = 1-mbc,mz+mbc
+                aux(i,j,k,mcapa) = volume(i,j,k)/dxdydz
+
+                aux(i,j,k,mcapa+1) = faceareas(i,j,k,1)/dydz
+                aux(i,j,k,mcapa+2) = faceareas(i,j,k,2)/dxdz
+                aux(i,j,k,mcapa+3) = faceareas(i,j,k,3)/dxdy
+
+                count = 1
+                do ii = 1,3
+                    do jj = 1,3
+                        !! Store by row
+                        aux(i,j,k,mcapa+3+ 0+count) = xrot(i,j,k,ii,jj)
+                        aux(i,j,k,mcapa+3+ 9+count) = yrot(i,j,k,ii,jj)
+                        aux(i,j,k,mcapa+3+18+count) = zrot(i,j,k,ii,jj)
+                        count = count + 1
+                    end do
+                end do
+            end do
+        end do
+    end do
+
+end subroutine overpressure_setaux_manifold
+
---
+++ applications/clawpack/euler/3d/all/euler_user.h
@@ -0,0 +1,140 @@
+/*
+Copyright (c) 2012-2021 Carsten Burstedde, Donna Calhoun
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef EULER_USER_3D_H
+#define EULER_USER_3D_H
+
+#include <fclaw2d_include_all.h>
+
+
+// #include <fclaw3dx_clawpatch_pillow.h>
+
+/* Headers for both Clawpack 4.6 and  Clawpack 5.0 */
+#include <fclaw3dx_clawpatch.h>
+#include <fclaw3dx_clawpatch_options.h>
+#include <fclaw3dx_clawpatch_fort.h>
+
+
+/* Clawpack 4.6 headers */  
+#include <fc3d_clawpack46.h>  
+#include <fc3d_clawpack46_options.h>
+#include <fc3d_clawpack46_fort.h>  
+#include <fc3d_clawpack46_user_fort.h>  
+#include <fclaw3dx_clawpatch46_fort.h>
+
+#if 0
+/* Clawpack 5.0 headers */
+#include <fc2d_clawpack5.h>
+#include <fc2d_clawpack5_options.h>
+#include <fc2d_clawpack5_fort.h>
+#include <clawpack5_user_fort.h>
+#include <fclaw3dx_clawpatch5_fort.h>
+#endif
+
+
+/* Headers for common FORTRAN files */
+//#include "advection_user_fort3.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#if 0
+/* Fix syntax */
+#endif
+
+/* --------------------------------- Square mappings ---------------------------------- */
+
+fclaw2d_map_context_t* fclaw2d_map_new_identity(fclaw2d_map_context_t *brick);
+
+fclaw2d_map_context_t* fclaw2d_map_new_cart(fclaw2d_map_context_t* brick,
+                                            const double scale[],
+                                            const double shift[]);
+  
+fclaw2d_map_context_t* fclaw2d_map_new_fivepatch(const double scale[],
+                                                 const double shift[],
+                                                 const double alpha);
+
+fclaw2d_map_context_t* fclaw2d_map_new_fivepatch5(const double scale[],
+                                                  const double shift[],
+                                                  const double alpha);
+
+fclaw2d_map_context_t* fclaw2d_map_new_squareddisk(const double scale[],
+                                                   const double shift[],
+                                                   const double alpha);
+  
+fclaw2d_map_context_t* fclaw2d_map_new_bilinear(fclaw2d_map_context_t *brick,
+                                                const double scale[],
+                                                const double shift[],
+                                                const double center[]);
+
+/* --------------------------------- Sphere mappings ---------------------------------- */
+
+fclaw2d_map_context_t* fclaw2d_map_new_pillowsphere(const double scale[],
+                                                    const double rotate[]);
+
+fclaw2d_map_context_t * fclaw2d_map_new_cubedsphere (const double scale[],
+                                                     const double rotate[]);
+
+fclaw2d_map_context_t * fclaw2d_map_new_pillowsphere (const double scale[],
+                                                      const double rotate[]);
+
+/* ---------------------------------- Disk mappings ----------------------------------- */
+
+fclaw2d_map_context_t* fclaw2d_map_new_pillowdisk(const double scale[],
+                                                  const double shift[],
+                                                  const double rotate[]);
+
+fclaw2d_map_context_t* fclaw2d_map_new_pillowdisk5(const double scale[],
+                                                   const double shift[],
+                                                   const double rotate[],
+                                                   const double alpha);
+
+
+/* --------------------------------- Hemisphere mappings ------------------------------ */
+
+fclaw2d_map_context_t* fclaw2d_map_new_pillowsphere5(const double scale[],
+                                                     const double rotate[],
+                                                     const double alpha);
+
+/* --------------------------------- Latlong mapping ---------------------------------- */
+
+
+fclaw2d_map_context_t *
+    fclaw2d_map_new_latlong (fclaw2d_map_context_t* brick,
+                             const double scale[],
+                             const double rotate[],
+                             const double lat[],
+                             const double longitude[],
+                             const int a, const int b);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
