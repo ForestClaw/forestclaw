c1b506ba43af8b0ab771e08affca3edd62569043
Scott Aiton <scott@aiton.net>
2022-11-08 17:47:57 -0700

    (heat_phasefield) update

--- applications/elliptic/heat_phasefield/phasefield/phasefield.cpp
+++ applications/elliptic/heat_phasefield/phasefield/phasefield.cpp
@@ -57,7 +57,7 @@ fclaw2d_domain_t* phasefield_create_domain(sc_MPI_Comm mpicomm, fclaw_options_t*
 
     /* Map unit square to disk using mapc2m_disk.f */
     conn = p4est_connectivity_new_brick(mi,mj,a,b);
-    brick = fclaw2d_map_new_brick(conn,mi,mj);
+    brick = fclaw2d_map_new_brick_conn (conn,mi,mj);
     cont = fclaw2d_map_new_nomap_brick(brick);
 
     domain = fclaw2d_domain_new_conn_map (mpicomm, fclaw_opt->minlevel, conn, cont);
