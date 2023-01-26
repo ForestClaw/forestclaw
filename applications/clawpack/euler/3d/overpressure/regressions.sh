193713d10ec7bce01687da32fd37b32f0223f77f
donnaaboise <donna.calhoun@gmail.com>
2023-01-26 11:16:10 -0700

    (euler/3d/overpressure) Fix regression test file

--- applications/clawpack/euler/3d/overpressure/regressions.sh
+++ applications/clawpack/euler/3d/overpressure/regressions.sh
@@ -7,11 +7,13 @@ cd $FCLAW_APPLICATIONS_SRC_DIR/clawpack/euler/3d/overpressure/
 
 # run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
 
-$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application mpirun -n 4 overpressure -F cart.ini --tfinal=0.02 --nout=1 --output=F
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F cart.ini --tfinal=0.02 --nout=1 --output=F
 
-$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application mpirun -n 4 overpressure -F cart_bump.ini --tfinal=0.02 --nout=1 --output=F
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F cart_bump.ini --tfinal=0.02 --nout=1 --output=F
 
-$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application mpirun -n 4 overpressure -F nomap.ini --tfinal=0.02 --nout=1 --output=F
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F nomap.ini --tfinal=0.02 --nout=1 --output=F
 
-$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application mpirun -n 4 overpressure -F latlong.ini --tfinal=0.01 --nout=1 --output=F --clawpatch:mz=32
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F latlong.ini --tfinal=0.01 --nout=1 --output=F --clawpatch:mz=32
 
+
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F cubed_sphere.ini --tfinal=0.01 --nout=1 --output=F
