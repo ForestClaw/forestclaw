6728285dc4bbeb60b7f5d906e1aae8d79bf0ff1a
Scott Aiton <scott@aiton.net>
2023-03-06 15:49:45 -0700

    handle csv files for multiple test cases in a regressions.sh file

--- applications/clawpack/transport/2d/torus/regressions.sh
+++ applications/clawpack/transport/2d/torus/regressions.sh
@@ -6,7 +6,7 @@ application=$FCLAW_APPLICATIONS_BUILD_DIR/clawpack/transport/2d/torus/torus_tran
 cd $FCLAW_APPLICATIONS_SRC_DIR/clawpack/transport/2d/torus/
 
 # run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
-$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4 || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=4  --regression-check=regression_values_0.csv || exit 1
 
 # Version 5 not yet implemented.
-$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5 || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:claw-version=5  --regression-check=regression_values_1.csv || exit 1
--- applications/geoclaw/bowl_slosh/regressions.sh
+++ applications/geoclaw/bowl_slosh/regressions.sh
@@ -6,4 +6,4 @@ application=$FCLAW_APPLICATIONS_BUILD_DIR/geoclaw/bowl_slosh/bowl_slosh
 cd $FCLAW_APPLICATIONS_SRC_DIR/geoclaw/bowl_slosh/regression
 
 # run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
-$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini  --regression-check=regression_values_0.csv || exit 1
