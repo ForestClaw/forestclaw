a22632ce68d15d32c7f0e0025651053f28dde1a6
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2023-06-21 10:11:59 +0200

    Feature 3D: begin with empty p8/swirl example

---
+++ applications/clawpack/advection/p8/swirl/swirl.cpp
@@ -0,0 +1,226 @@
+/*
+  Copyright (c) 2012-2022 Carsten Burstedde, Donna Calhoun, Scott Aiton
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifdef P8HACK
+#include "swirl_user.h"
+#endif
+
+#include <fclaw2d_defs.h>
+
+#ifdef P8HACK
+
+static
+fclaw2d_domain_t* create_domain(sc_MPI_Comm mpicomm, 
+                                fclaw_options_t* fclaw_opt, 
+                                user_options_t* user,
+                                fclaw3dx_clawpatch_options_t* clawpatch_opt,
+                                fc3d_clawpack46_options_t *claw3_opt)
+{
+    /* Mapped, multi-block domain */
+    p4est_connectivity_t     *conn = NULL;
+    fclaw2d_domain_t         *domain;
+    fclaw2d_map_context_t    *cont = NULL, *brick = NULL;
+
+    int mi = fclaw_opt->mi;
+    int mj = fclaw_opt->mj;
+    int a = fclaw_opt->periodic_x;
+    int b = fclaw_opt->periodic_y;
+
+    int mx = clawpatch_opt->mx;
+    int minlevel = fclaw_opt->minlevel;
+    int check = mi*mx*pow_int(2,minlevel);
+
+    switch (user->example) 
+    {
+    case 0:
+        FCLAW_ASSERT(claw3_opt->mcapa == 0);
+        FCLAW_ASSERT(fclaw_opt->manifold == 0);
+        /* Size is set by [ax,bx] x [ay, by], set in .ini file */
+        conn = p4est_connectivity_new_unitsquare();
+        cont = fclaw2d_map_new_nomap();
+        break;
+
+    case 1:
+        /* Square brick domain */
+        FCLAW_ASSERT(claw3_opt->mcapa != 0);
+        FCLAW_ASSERT(fclaw_opt->manifold != 0);
+        FCLAW_ASSERT(clawpatch_opt->maux == 4);
+        conn = p4est_connectivity_new_brick(mi,mj,a,b);
+        brick = fclaw2d_map_new_brick_conn (conn,mi,mj);
+        /* Square in [-1,1]x[-1,1], scaled/shifted to [0,1]x[0,1] */
+        cont = fclaw2d_map_new_cart(brick,
+                                    fclaw_opt->scale,
+                                    fclaw_opt->shift);
+
+        break;
+    case 2:
+        FCLAW_ASSERT(fclaw_opt->manifold != 0);
+        FCLAW_ASSERT(claw3_opt->mcapa != 0);
+        FCLAW_ASSERT(clawpatch_opt->maux == 4);
+        if (check < 32)
+        {
+            printf("mi*pow_int(mx,minlevel) = %d\n",check);
+            fclaw_global_essentialf("The five patch mapping requires mi*mx*2^minlevel > 32\n");
+            exit(0);
+
+        }
+        /* Five patch square domain */
+        conn = p4est_connectivity_new_disk (0, 0);
+        cont = fclaw2d_map_new_fivepatch (fclaw_opt->scale,
+                                          fclaw_opt->shift,
+                                          user->alpha);
+
+        break;
+
+    case 3:
+        /* bilinear square domain : maps to [-1,1]x[-1,1] */
+        FCLAW_ASSERT(fclaw_opt->manifold != 0);
+        FCLAW_ASSERT(claw3_opt->mcapa != 0);
+        FCLAW_ASSERT(clawpatch_opt->maux == 4);
+        FCLAW_ASSERT(mi == 2 && mj == 2);
+        conn = p4est_connectivity_new_brick(mi,mj,a,b);
+        brick = fclaw2d_map_new_brick_conn (conn,mi,mj);
+        cont = fclaw2d_map_new_bilinear (brick, 
+                                         fclaw_opt->scale,
+                                         fclaw_opt->shift, 
+                                         user->center);
+        break;
+
+    default:
+        SC_ABORT_NOT_REACHED ();
+    }
+
+    if (user->example > 0)
+    {
+        swirl_map_extrude(cont,user->maxelev);        
+    }
+
+    domain = fclaw2d_domain_new_conn_map (mpicomm, fclaw_opt->minlevel, conn, cont);
+    fclaw2d_domain_list_levels(domain, FCLAW_VERBOSITY_ESSENTIAL);
+    fclaw2d_domain_list_neighbors(domain, FCLAW_VERBOSITY_DEBUG);  
+    return domain;
+}
+
+static
+void run_program(fclaw2d_global_t* glob)
+{
+    const user_options_t           *user_opt;
+
+    /* ---------------------------------------------------------------
+       Set domain data.
+       --------------------------------------------------------------- */
+    fclaw2d_domain_data_new(glob->domain);
+
+    user_opt = swirl_get_options(glob);
+
+    /* Initialize virtual table for ForestClaw */
+    fclaw2d_vtables_initialize(glob);
+
+    if (user_opt->claw_version == 4)
+    {
+        fc3d_clawpack46_solver_initialize(glob);
+    }
+    else if (user_opt->claw_version == 5)
+    {
+        printf("swirl.cpp : Example not implemented for Claw version 5.\n");
+        exit(0);
+    }
+
+    swirl_link_solvers(glob);
+
+    /* ---------------------------------------------------------------
+       Run
+       --------------------------------------------------------------- */
+    fclaw2d_initialize(glob);
+    fclaw2d_run(glob);
+    fclaw2d_finalize(glob);
+}
+
+#endif
+
+int
+main (int argc, char **argv)
+{
+#ifdef P8HACK
+    fclaw_app_t *app;
+    int first_arg;
+    fclaw_exit_type_t vexit;
+
+    /* Options */
+    sc_options_t                 *options;
+    user_options_t               *user_opt;
+    fclaw_options_t              *fclaw_opt;
+    fclaw3dx_clawpatch_options_t *clawpatch_opt;
+    fc3d_clawpack46_options_t    *claw46_opt;
+
+    fclaw2d_global_t            *glob;
+    fclaw2d_domain_t            *domain;
+    sc_MPI_Comm mpicomm;
+
+    int retval;
+
+    /* Initialize application */
+    app = fclaw_app_new (&argc, &argv, NULL);
+
+    /* Create new options packages */
+    fclaw_opt =                   fclaw_options_register(app,  NULL,       "fclaw_options.ini");
+    clawpatch_opt =  fclaw3dx_clawpatch_options_register(app, "clawpatch", "fclaw_options.ini");
+    claw46_opt =        fc3d_clawpack46_options_register(app, "claw3",     "fclaw_options.ini");
+    user_opt =                    swirl_options_register(app,              "fclaw_options.ini");  
+
+    /* Read configuration file(s) and command line, and process options */
+    options = fclaw_app_get_options (app);
+    retval = fclaw_options_read_from_file(options);
+    vexit =  fclaw_app_options_parse (app, &first_arg,"fclaw_options.ini.used");
+
+    /* Run the program */
+    if (!retval & !vexit)
+    {
+        /* Options have been checked and are valid */
+
+        mpicomm = fclaw_app_get_mpi_size_rank (app, NULL, NULL);
+        domain = create_domain(mpicomm, fclaw_opt, user_opt, clawpatch_opt,
+                               claw46_opt);
+    
+        /* Create global structure which stores the domain, timers, etc */
+        glob = fclaw2d_global_new();
+        fclaw2d_global_store_domain(glob, domain);
+
+        /* Store option packages in glob */
+        fclaw2d_options_store           (glob, fclaw_opt);
+        fclaw3dx_clawpatch_options_store(glob, clawpatch_opt);
+        fc3d_clawpack46_options_store   (glob, claw46_opt);
+        swirl_options_store             (glob, user_opt);
+
+        run_program(glob);
+
+        fclaw2d_global_destroy(glob);        
+    }
+    
+    fclaw_app_destroy (app);
+
+#endif
+    return 0;
+}
