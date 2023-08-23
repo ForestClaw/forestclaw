523559d4a679d8f3ae8cbbea5f1ba0aa444ea9ee
Scott Aiton <scott@aiton.net>
2023-08-22 23:28:09 -0600

    update how regressions are registered in cmake

---
+++ applications/cmake/Modules/add_regressions.cmake
@@ -0,0 +1,45 @@
+function(add_regressions TARGET)
+  if(NOT _WORKING_DIRECTORY)
+    set(_WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
+  endif()
+  if(NOT _TEST_LIST)
+    set(_TEST_LIST ${TARGET}_TESTS)
+  endif()
+
+  ## Generate a unique name based on the extra arguments
+  string(SHA1 args_hash "${TARGET} ${CMAKE_CURRENT_BINARY_DIR}")
+  string(SUBSTRING ${args_hash} 0 7 args_hash)
+
+  # Define rule to generate test list for aforementioned test executable
+  set(ctest_include_file "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}_include-${args_hash}.cmake")
+  file(WRITE "${ctest_include_file}"
+    "include(\"${PROJECT_SOURCE_DIR}/cmake/Modules/discover_regressions.cmake\")\n"
+    "add_regressions(\"${CMAKE_CURRENT_SOURCE_DIR}/${TARGET}\" \"${CMAKE_CURRENT_SOURCE_DIR}\" \"${CMAKE_CURRENT_BINARY_DIR}\")\n"
+  )
+
+  if(NOT CMAKE_VERSION VERSION_LESS 3.10)
+    # Add discovered tests to directory TEST_INCLUDE_FILES
+    set_property(DIRECTORY
+      APPEND PROPERTY TEST_INCLUDE_FILES "${ctest_include_file}"
+    )
+  else()
+    # Add discovered tests as directory TEST_INCLUDE_FILE if possible
+    get_property(test_include_file_set DIRECTORY PROPERTY TEST_INCLUDE_FILE SET)
+    if(NOT ${test_include_file_set})
+      set_property(DIRECTORY
+        PROPERTY TEST_INCLUDE_FILE "${ctest_include_file}"
+      )
+    else()
+      message(FATAL_ERROR
+        "Cannot set more than one TEST_INCLUDE_FILE"
+      )
+    endif()
+  endif()
+
+endfunction()
+
+###############################################################################
+
+set(_DISCOVER_REGRESSIONS_SCRIPT
+  ${CMAKE_CURRENT_LIST_DIR}/discover_regressions.cmake
+)
---
+++ applications/cmake/Modules/discover_regressions.cmake
@@ -0,0 +1,100 @@
+function(read_file_lines filepath out_list)
+    # Check if the file exists
+    if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${filepath})
+        message(FATAL_ERROR "File not found: ${filepath}")
+    endif()
+
+    # Read the file content
+    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/${filepath} content)
+
+    # Split the content by newline
+    string(REPLACE "\n" ";" lines "${content}")
+
+    set(non_empty_lines "")
+
+    # Loop through each line
+    foreach(line ${lines})
+        # Trim whitespace from the beginning and end of the line
+        string(STRIP "${line}" trimmed_line)
+
+        # Check if the line is not empty and doesn't start with #
+        if(NOT "${trimmed_line}" STREQUAL "" AND NOT "${trimmed_line}" MATCHES "^#.*")
+            # remove everything including / before adding to the list
+            list(APPEND non_empty_lines "${trimmed_line}")
+        endif()
+    endforeach()
+
+    # Set the out_list to the filtered lines
+    set(${out_list} "${non_empty_lines}" PARENT_SCOPE)
+endfunction()
+
+function(add_regressions filename source_dir build_dir)
+    read_file_lines(${filename} tests) 
+
+    set(working_directory "${source_dir}")
+    # Loop through each test
+    foreach(test IN LISTS tests)
+        list(GET argv 0 executable)
+        if(executable STREQUAL "cd")
+            list(GET argv 1 directory)
+
+            cmake_path(APPEND working_directory "${directory}")
+            cmake_path(SET working_directory NORMALIZE "${working_directory}")
+
+            message(STATUS "${working_directory}")
+        else()
+            string(REGEX REPLACE "^.*applications/" "" relative_source_directory "${working_directory}")
+            set(test_name "${relative_source_directory}: ${test}")
+
+            string(REGEX REPLACE "^.*/" "" test "${test}")
+            string(REGEX REPLACE "[ \t].*" "" executable "${test}")
+            string(REGEX REPLACE "^${executable}" "" args "${test}")
+            cmake_path(APPEND build_dir "${executable}" OUTPUT_VARIABLE executable)
+
+            # Initialize an empty list to hold the final split values
+            set(argv "")
+
+            # Initialize a variable to hold the current token
+            set(current_token "")
+
+            # Initialize a variable to indicate if we are inside quotes
+            set(in_quotes FALSE)
+
+            # Get length of the string
+            string(LENGTH ${args} args_length)
+
+            # Loop through each character in the string
+            foreach(index RANGE 0 ${args_length})
+                # Use string(SUBSTRING) to get a single character from the string
+                string(SUBSTRING ${args} ${index} 1 char)
+              if(char STREQUAL "\"" OR char STREQUAL "'")
+                # Toggle the in-quotes state
+                set(in_quotes NOT ${in_quotes})
+              elseif((char STREQUAL " " OR char STREQUAL "\t") AND NOT ${in_quotes})
+                # If the character is a space or a tab and we are not in quotes,
+                # then this is a delimiter.
+                # Add the current token to the list and clear it
+                if (NOT "${current_token}" STREQUAL "")
+                  list(APPEND argv "${current_token}")
+                endif()
+                set(current_token "")
+              else()
+                # Otherwise, append the character to the current token
+                set(current_token "${current_token}${char}")
+              endif()
+            endforeach()
+
+            # Append the last token to the list
+            if (NOT "${current_token}" STREQUAL "")
+              list(APPEND argv "${current_token}")
+            endif()
+
+            add_test(${test_name} ${TEST_RUNNER} ${executable} ${argv})
+
+            set_tests_properties(${test_name} PROPERTIES WORKING_DIRECTORY ${working_directory})
+
+        endif()
+
+    endforeach()
+
+endfunction()
