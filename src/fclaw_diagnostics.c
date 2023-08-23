33a693eefa7d2355d5b3fa0205983019db7ed399
Scott Aiton <scott@aiton.net>
2023-08-23 11:55:22 -0600

    add more diagnostics_accumulator casts

--- src/fclaw_diagnostics.c
+++ src/fclaw_diagnostics.c
@@ -194,7 +194,8 @@ void fclaw_diagnostics_gather(fclaw_global_t *glob,
 
 void fclaw_diagnostics_reset(fclaw_global_t *glob)
 {
-    fclaw_diagnostics_accumulator_t *acc = fclaw_global_get_attribute(glob, "acc");
+    fclaw_diagnostics_accumulator_t *acc = 
+            (fclaw_diagnostics_acumulator_t*) fclaw_global_get_attribute(glob, "acc");
     const fclaw_options_t *fclaw_opt = fclaw_get_options(glob);
     fclaw_diagnostics_vtable_t *diag_vt = fclaw_diagnostics_vt(glob);
 
@@ -220,7 +221,8 @@ void fclaw_diagnostics_reset(fclaw_global_t *glob)
 
 void fclaw_diagnostics_finalize(fclaw_global_t *glob)
 {
-    fclaw_diagnostics_accumulator_t *acc = fclaw_global_get_attribute(glob, "acc");
+    fclaw_diagnostics_accumulator_t *acc =
+            (fclaw_diagnostics_acumulator_t*) fclaw_global_get_attribute(glob, "acc");
     const fclaw_options_t *fclaw_opt = fclaw_get_options(glob);
     fclaw_diagnostics_vtable_t *diag_vt = fclaw_diagnostics_vt(glob);
 
