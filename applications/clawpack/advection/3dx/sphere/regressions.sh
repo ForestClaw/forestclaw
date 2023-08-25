82f6a4307b282b6b38ab8cbc928cc0ba83b9a0a7
Scott Aiton <scott@aiton.net>
2023-08-25 09:43:43 -0600

    move advection/3d to advection/3dx

--- applications/clawpack/advection/3dx/sphere/regressions.sh
+++ applications/clawpack/advection/3dx/sphere/regressions.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # absolute path to application we are testing
-application=$FCLAW_APPLICATIONS_BUILD_DIR/clawpack/advection/3d/sphere/sphere
+application=$FCLAW_APPLICATIONS_BUILD_DIR/clawpack/advection/3dx/sphere/sphere
 
 # change to source dir for working directory
-cd $FCLAW_APPLICATIONS_SRC_DIR/clawpack/advection/3d/sphere/
+cd $FCLAW_APPLICATIONS_SRC_DIR/clawpack/advection/3dx/sphere/
 
 # run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
 $FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:example=1  --regression-check=regression_values_0.csv || exit 1
--- applications/clawpack/claw3.cmake
+++ applications/clawpack/claw3.cmake
@@ -4,12 +4,12 @@
 
 
 # Scalar advection
-add_subdirectory(clawpack/advection/3d/swirl)
+add_subdirectory(clawpack/advection/3dx/swirl)
 add_subdirectory(clawpack/advection/p8/swirl)
-add_subdirectory(clawpack/advection/3d/filament)
-add_subdirectory(clawpack/advection/3d/sphere)
-add_subdirectory(clawpack/advection/3d/disk)
-add_subdirectory(clawpack/advection/3d/latlong)
+add_subdirectory(clawpack/advection/3dx/filament)
+add_subdirectory(clawpack/advection/3dx/sphere)
+add_subdirectory(clawpack/advection/3dx/disk)
+add_subdirectory(clawpack/advection/3dx/latlong)
 
 # Euler equations
 add_subdirectory(clawpack/euler/3d/overpressure)
