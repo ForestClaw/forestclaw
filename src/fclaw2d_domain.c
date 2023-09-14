de099a61a2f6827c83b44cc880307c5cd80c1a3f
Scott Aiton <scott@aiton.net>
2023-09-13 20:04:38 -0600

    write dim_ind domain constructor

--- src/fclaw2d_domain.c
+++ src/fclaw2d_domain.c
@@ -53,8 +53,111 @@ fclaw2d_domain_t* fclaw_domain_get_2d_domain(const fclaw_domain_t* domain)
     return wrap->domain;
 }
 
+static
+void copy_patch(fclaw_patch_t* patch, fclaw2d_patch_t* patch_2d)
+{
+    patch->dim = FCLAW2D_SPACEDIM;
+    patch->xlower = patch_2d->xlower;
+    patch->xupper = patch_2d->xupper;
+    patch->ylower = patch_2d->ylower;
+    patch->yupper = patch_2d->yupper;
+#ifndef P4_TO_P8
+    patch->zlower = 0;
+    patch->zupper = 1;
+#else
+    patch->zlower = patch_2d->zlower;
+    patch->zupper = patch_2d->zupper;
+#endif
+
+
+    patch->level = patch_2d->level;
+    patch->wrapped_patch = patch_2d;
+    patch_2d->user = patch;
+}
+
+static
+void copy_block(fclaw_block_t* block, fclaw2d_block_t* block_2d)
+{
+    block->dim = FCLAW2D_SPACEDIM;
+    block->num_patches = block_2d->num_patches;
+    block->num_patches_before = block_2d->num_patches_before;
+    block->num_exchange_patches = block_2d->num_exchange_patches;
+
+    block->xlower = block_2d->xlower;
+    block->xupper = block_2d->xupper;
+    block->ylower = block_2d->ylower;
+    block->yupper = block_2d->yupper;
+#ifndef P4_TO_P8
+    block->zlower = 0;
+    block->zupper = 1;
+#else
+    block->zlower = block_2d->zlower;
+    block->zupper = block_2d->zupper;
+#endif
+
+    for(int i=0; i < 3*FCLAW2D_NUMCORNERS; ++i)
+    {
+        block->vertices[i] = block_2d->vertices[i];
+    }
+
+    for(int i=0; i < FCLAW2D_NUMFACES; ++i)
+    {
+        block->is_boundary[i] = block_2d->is_boundary[i];
+    }
+
+    block->minlevel = block_2d->minlevel;
+    block->maxlevel = block_2d->maxlevel;
+
+    block->patches = FCLAW_ALLOC(fclaw_patch_t, block->num_patches);
+    for(int patchno = 0; patchno < block->num_patches; patchno++)
+    {
+        copy_patch(&block->patches[patchno], &block_2d->patches[patchno]);
+    }
+}
+
 fclaw_domain_t* fclaw_domain_wrap_2d(fclaw2d_domain_t* domain_2d)
 {
     fclaw_domain_t* domain = FCLAW_ALLOC_ZERO(fclaw_domain_t, 1);
+    domain->dim = FCLAW2D_SPACEDIM;
+
+    domain->mpicomm = domain_2d->mpicomm;
+    domain->mpisize = domain_2d->mpisize;
+    domain->mpirank = domain_2d->mpirank;
+    domain->possible_maxlevel = domain_2d->possible_maxlevel;
+
+    domain->local_num_patches = domain_2d->local_num_patches;
+    domain->local_minlevel = domain_2d->local_minlevel;
+    domain->local_maxlevel = domain_2d->local_maxlevel;
+    domain->global_num_patches = domain_2d->global_num_patches;
+    domain->global_num_patches_before = domain_2d->global_num_patches_before;
+    domain->global_minlevel = domain_2d->global_minlevel;
+    domain->global_maxlevel = domain_2d->global_maxlevel;
+
+    domain->num_blocks = domain_2d->num_blocks;
+
+    domain->blocks = FCLAW_ALLOC_ZERO(fclaw_block_t, domain->num_blocks);
+
+    for(int blockno = 0; blockno < domain->num_blocks; ++blockno)
+    {
+        copy_block(&domain->blocks[blockno], &domain_2d->blocks[blockno]);
+    }
+
+    domain->num_exchange_patches = domain_2d->num_exchange_patches;
+
+    domain->num_ghost_patches = domain_2d->num_ghost_patches;
+    domain->ghost_patches = FCLAW_ALLOC_ZERO(fclaw_patch_t, domain->num_ghost_patches);
+    for(int patchno = 0; patchno < domain->num_ghost_patches; patchno++)
+    {
+        copy_patch(&domain->ghost_patches[patchno], &domain_2d->ghost_patches[patchno]);
+    }
+
+    domain->attributes = domain_2d->attributes;
+
+    fclaw2d_domain_wrap_t* wrap = FCLAW_ALLOC_ZERO(fclaw2d_domain_wrap_t, 1);
+    wrap->domain = domain_2d;
+    domain->wrapped_domain = wrap;
+
+    domain_2d->user = domain;
+
     return domain;
 }
