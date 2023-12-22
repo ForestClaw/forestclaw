24c34a9b7d850f08a0c7c1f10c6e47044997eb47
scivision <scivision@users.noreply.github.com>
2023-12-22 13:17:35 -0500

    FetchContent Git Submodules for P4EST and SC

--- cmake/GitSubmodule.cmake
+++ cmake/GitSubmodule.cmake
@@ -1,10 +1,11 @@
+include_guard()
+
 find_package(Git REQUIRED)
 
 
 function(git_submodule dir)
 # get/update Git submodule directory to CMake, assuming the
 # Git submodule directory is a CMake project.
-
 if(NOT EXISTS ${dir}/CMakeLists.txt)
   execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
