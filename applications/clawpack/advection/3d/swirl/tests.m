d4ce643deb525892cc967f073171c8dc45ab4f08
Scott Aiton <scott@aiton.net>
2024-01-21 19:35:48 -0700

    remove files

--- applications/clawpack/advection/3d/swirl/tests.m
+++
@@ -1,6 +0,0 @@
-function tests(filename)
-    patches = FCReadAscii.readFile(filename);
-    mesh = FCMesh(patches);
-    geom = FCMeshGeometry(mesh);
-    trisurf(geom.T, geom.x, geom.y, geom.z, "EdgeColor", 'none')
-end
