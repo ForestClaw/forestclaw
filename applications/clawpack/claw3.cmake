e1941e5161b528604c9c3ddbc5e6d2bc527fc257
Scott Aiton <scott@aiton.net>
2024-01-23 12:18:01 -0700

    remove current p8est example

--- applications/clawpack/claw3.cmake
+++ applications/clawpack/claw3.cmake
@@ -5,7 +5,6 @@
 
 # Scalar advection
 add_subdirectory(clawpack/advection/3dx/swirl)
-add_subdirectory(clawpack/advection/p8/swirl)
 add_subdirectory(clawpack/advection/3dx/filament)
 add_subdirectory(clawpack/advection/3dx/sphere)
 add_subdirectory(clawpack/advection/3dx/disk)
