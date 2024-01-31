1cb829b993fffa8a15ae88cadab63785bea7bbdd
Scott Aiton <scott@aiton.net>
2024-01-31 16:25:26 -0700

    add regression for non-mapped case

--- applications/clawpack/advection/3d/swirl/regressions.sh
+++ applications/clawpack/advection/3d/swirl/regressions.sh
@@ -6,7 +6,7 @@ application=$FCLAW_APPLICATIONS_BUILD_DIR/clawpack/advection/3d/swirl/swirl
 cd $FCLAW_APPLICATIONS_SRC_DIR/clawpack/advection/3d/swirl/
 
 # run programs, exit script with nonzero on failure (or else script will exit with value of last program run)
-#$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:example=0  --regression-check=regression_values_0.csv || exit 1
+$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression.ini --user:example=0  --regression-check=regression_values_0.csv || exit 1
 
 #$FCLAW_MPIRUN $FCLAW_MPI_TEST_FLAGS $application -F regression_map.ini --user:example=1  --regression-check=regression_values_1.csv || exit 1
 
