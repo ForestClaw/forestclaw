0ffc6ad82de165033ad51eeaea9052f7c00d5ee5
Scott Aiton <scott@aiton.net>
2025-01-13 18:59:59 +0000

    fix poisson_output

--- applications/elliptic/3d/poisson/fortran/poisson_output.f90
+++ applications/elliptic/3d/poisson/fortran/poisson_output.f90
@@ -51,13 +51,13 @@ subroutine poisson_fort_output_ascii(matname1, &
     matunit1 = 10
     open(matunit1,file=matname1,position='append');
 
-    call fclaw3d_clawpatch_fort_write_grid_header(matunit1, & 
+    call fclaw3d_clawpatch46_fort_write_grid_header(matunit1, & 
            mx,my,mz,xlower,ylower,zlower, dx,dy,dz,patch_num,level, & 
            blockno,mpirank)
 
     if (mfields .gt. 5) then
         write(6,'(A,A,A,I5,A)')     & 
-             'Warning (fclaw2d_fort_write_grid_header.f) ',  & 
+             'Warning (fclaw3d_fort_write_grid_header.f) ',  & 
              ': meqn > 5; change format statement 120.',     & 
              '(meqn = ',mfields,')'
         stop
